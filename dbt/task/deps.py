451c496b0d80d9f7d793278715131572eaf95f43
Drew Banin <drew@fishtownanalytics.com>
2016-11-28 23:57:35 -0500

    try normpath

--- dbt/task/deps.py
+++ dbt/task/deps.py
@@ -17,6 +17,7 @@ class DepsTask:
         self.project = project
 
     def __checkout_branch(self, branch, full_path):
+        full_path = os.path.normpath(full_path)
         print("  checking out branch {}".format(branch))
         print("DEBUG: cwd = {}, run from = {}".format(os.getcwd(), full_path))
         cwd = os.getcwd()
