1e541af06e000b25100b53b1a1b3d46059819d2f
Drew Banin <drew@fishtownanalytics.com>
2016-11-30 10:26:53 -0500

    grant login to noaccess user

--- test/setup_db.sh
+++ test/setup_db.sh
@@ -5,4 +5,5 @@ psql -c "ALTER ROLE root WITH LOGIN;" -U postgres
 psql -c "GRANT CREATE, CONNECT ON DATABASE dbt TO root;" -U postgres
 
 psql -c "CREATE ROLE noaccess WITH UNENCRYPTED PASSWORD 'password' NOSUPERUSER;" -U postgres;
+psql -c "ALTER ROLE noaccess WITH LOGIN;" -U postgres
 psql -c "GRANT CREATE, CONNECT ON DATABASE dbt TO noaccess;" -U postgres;
