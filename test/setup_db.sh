f5787e6806cd372b75b16e177e92a9274cbd9f3f
Drew Banin <drew@fishtownanalytics.com>
2016-11-30 10:23:14 -0500

    how sql

--- test/setup_db.sh
+++ test/setup_db.sh
@@ -1,8 +1,8 @@
 
 createdb dbt
-psql -c "CREATE ROLE IF NOT EXISTS root WITH UNENCRYPTED PASSWORD 'password';" -U postgres
+psql -c "CREATE ROLE root WITH UNENCRYPTED PASSWORD 'password';" -U postgres
 psql -c "ALTER ROLE root WITH LOGIN;" -U postgres
 psql -c "GRANT CREATE, CONNECT ON DATABASE dbt TO root;" -U postgres
 
-psql -c "CREATE USER IF NOT EXISTS noaccess WITH UNENCRYPTED PASSWORD 'password' NOSUPERUSER;" -U postgres;
+psql -c "CREATE ROLE noaccess WITH UNENCRYPTED PASSWORD 'password' NOSUPERUSER;" -U postgres;
 psql -c "GRANT CREATE, CONNECT ON DATABASE dbt TO noaccess;" -U postgres;
