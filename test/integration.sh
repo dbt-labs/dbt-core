e848e59ccc78e5ad4cac7a574499f433c55184c9
Connor McArthur <cmcarthur@rjmetrics.com>
2017-01-06 15:39:22 -0500

    remove now-unused test scripts

--- test/integration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pip install tox
-
-cd /usr/src/app
-tox -e integration-py27,integration-py35
--- test/unit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pip install tox
-
-cd /usr/src/app
-time tox -e unit-py27,unit-py35,pep8
