0a1170eb15e4b4f68c65591c117c15fa484be59e
Drew Banin <drew@fishtownanalytics.com>
2017-03-12 21:03:04 -0400

    typo

--- dbt/runner.py
+++ dbt/runner.py
@@ -130,7 +130,7 @@ def print_result_line(result, schema_name, index, total):
     if node.get('resource_type') == NodeType.Model:
         print_model_result_line(result, schema_name, index, total)
     elif node.get('resource_type') == NodeType.Test:
-        (result, schema_name, index, total)
+        print_test_result_line(result, schema_name, index, total)
     elif node.get('resource_type') == NodeType.Archive:
         print_archive_result_line(result, index, total)
 
