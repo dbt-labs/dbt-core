9849fa08e71be50c6299eb50c3e7b870a4ecde4c
Drew Banin <drew@fishtownanalytics.com>
2017-06-15 13:09:46 -0400

    better bq validation

--- dbt/contracts/connection.py
+++ dbt/contracts/connection.py
@@ -35,7 +35,7 @@ snowflake_credentials_contract = Schema({
 
 bigquery_auth_methods = ['oauth', 'service-account', 'service-account-json']
 bigquery_credentials_contract = Schema({
-    Required('method'): basestring,
+    Required('method'): Any(*bigquery_auth_methods ),
     Required('project'): basestring,
     Required('schema'): basestring,
     Optional('keyfile'): basestring,
