62ac30a3d25fb3c71705559095acdcc0d9afb012
Connor McArthur <cmcarthur@rjmetrics.com>
2017-07-13 16:31:01 -0400

    init: remove .git directory after clone

--- dbt/clients/git.py
+++ dbt/clients/git.py
@@ -1,47 +1,34 @@
-from dbt.clients.system import run_cmd
+import os.path
+
+from dbt.clients.system import run_cmd, rmdir
 from dbt.logger import GLOBAL_LOGGER as logger
-import dbt.exceptions
 
 
-def clone(repo, cwd, dirname=None):
+def clone(repo, cwd, dirname=None, remove_git_dir=False):
     clone_cmd = ['git', 'clone', '--depth', '1', repo]
 
     if dirname is not None:
         clone_cmd.append(dirname)
 
-    return run_cmd(cwd, clone_cmd)
+    result = run_cmd(cwd, clone_cmd)
+
+    if remove_git_dir:
+        rmdir(os.path.join(dirname, '.git'))
+
+    return result
 
 
-def list_tags(cwd):
-    out, err = run_cmd(cwd, ['git', 'tag', '--list'])
-    tags = set(out.decode('utf-8').strip().split("\n"))
-    return tags
-
-
-def checkout(cwd, repo, branch=None):
+def checkout(cwd, branch=None):
     if branch is None:
         branch = 'master'
+
+    remote_branch = 'origin/{}'.format(branch)
 
     logger.info('  Checking out branch {}.'.format(branch))
 
     run_cmd(cwd, ['git', 'remote', 'set-branches', 'origin', branch])
-    run_cmd(cwd, ['git', 'fetch', '--tags', '--depth', '1', 'origin', branch])
-
-    tags = list_tags(cwd)
-
-    # Prefer tags to branches if one exists
-    if branch in tags:
-        spec = 'tags/{}'.format(branch)
-    else:
-        spec = 'origin/{}'.format(branch)
-
-    out, err = run_cmd(cwd, ['git', 'reset', '--hard', spec])
-    stderr = err.decode('utf-8').strip()
-
-    if stderr.startswith('fatal:'):
-        dbt.exceptions.bad_package_spec(repo, branch, stderr)
-    else:
-        return out, err
+    run_cmd(cwd, ['git', 'fetch', '--depth', '1', 'origin', branch])
+    run_cmd(cwd, ['git', 'reset', '--hard', remote_branch])
 
 
 def get_current_sha(cwd):
