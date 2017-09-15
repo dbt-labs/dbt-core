664ae71b4e76e747d3edfac49e817a25fb037939
Drew Banin <drew@fishtownanalytics.com>
2017-09-14 23:35:36 -0400

    provide schema to create table for bq

--- dbt/include/global_project/macros/adapters/bigquery.sql
+++ dbt/include/global_project/macros/adapters/bigquery.sql
@@ -1,4 +1,4 @@
 
 {% macro bigquery__create_table_as(temporary, identifier, sql) -%}
-    {{ adapter.execute_model({"name": identifier, "injected_sql": sql}, 'table') }}
+    {{ adapter.execute_model({"name": identifier, "injected_sql": sql, "schema": schema}, 'table') }}
 {% endmacro %}
