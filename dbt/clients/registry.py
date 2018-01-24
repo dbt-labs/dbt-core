b168d1be0961d8f50d2a954ce63b0f4f26d2b8ee
Buck Ryan <buck.ryan2@gmail.com>
2018-01-24 12:58:16 -0500

    Refactor registry client for cleanliness and better error handling

--- dbt/clients/registry.py
+++ dbt/clients/registry.py
@@ -26,28 +26,30 @@ def _wrap_exceptions(fn):
 
 
 @_wrap_exceptions
+def _get(path, registry_base_url=None):
+    url = _get_url(path, registry_base_url)
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return resp.json()
+
+
 def index(registry_base_url=None):
-    return requests.get(
-        _get_url('api/v1/index.json',
-                 registry_base_url)).json()
+    return _get('api/v1/index.json', registry_base_url)
 
 
-@_wrap_exceptions
+def packages(registry_base_url=None):
+    return requests.get(r'https://tinyurl.com/ydg3z87m').json()
+    # return _get('api/v1/packages.json', registry_base_url)
+
+
 def package(name, registry_base_url=None):
-    return requests.get(
-        _get_url('api/v1/{}.json'.format(name),
-                 registry_base_url)).json()
+    return _get('api/v1/{}.json'.format(name), registry_base_url)
 
 
-@_wrap_exceptions
 def package_version(name, version, registry_base_url=None):
-    url = _get_url('api/v1/{}/{}.json'.format(name, version))
-    response = requests.get(url, registry_base_url)
-
-    return response.json()
+    return _get('api/v1/{}/{}.json'.format(name, version), registry_base_url)
 
 
 def get_available_versions(name):
     response = package(name)
-
-    return list(response['versions'].keys())
+    return list(response['versions'])
