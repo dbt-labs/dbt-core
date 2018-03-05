f5a61cd2404f67006804e3fdf788b2f80fb00d95
Connor McArthur <connormcarthur11@gmail.com>
2018-03-05 09:46:54 -0500

    use fishtown's snowplow collector (#682)
    automatic commit by git-black, original commits:
      600c5edaed19b4a8c8051b14eea26a0edcebdcd6

--- core/dbt/tracking.py
+++ core/dbt/tracking.py
@@ -34,9 +34,9 @@ COLLECTOR_URL = "fishtownanalytics.sinter-collect.com"
 COLLECTOR_PROTOCOL = "https"
 
 INVOCATION_SPEC = "iglu:com.dbt/invocation/jsonschema/1-0-2"
-PLATFORM_SPEC = 'iglu:com.dbt/platform/jsonschema/1-0-0'
+PLATFORM_SPEC = "iglu:com.dbt/platform/jsonschema/1-0-0"
 RUN_MODEL_SPEC = 'iglu:com.dbt/run_model/jsonschema/1-0-1'
-INVOCATION_ENV_SPEC = 'iglu:com.dbt/invocation_env/jsonschema/1-0-0'
+INVOCATION_ENV_SPEC = "iglu:com.dbt/invocation_env/jsonschema/1-0-0"
 PACKAGE_INSTALL_SPEC = 'iglu:com.dbt/package_install/jsonschema/1-0-0'
 RPC_REQUEST_SPEC = 'iglu:com.dbt/rpc_request/jsonschema/1-0-1'
 DEPRECATION_WARN_SPEC = 'iglu:com.dbt/deprecation_warn/jsonschema/1-0-0'
