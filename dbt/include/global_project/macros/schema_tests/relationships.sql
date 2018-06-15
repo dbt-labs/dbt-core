b8bfc2a70450fdf13569b07de52d1afc77207947
Michael Dunn <michael.dunn@realself.com>
2018-06-15 09:26:49 -0700

    CHange relationships schema test from not in to more efficient left outer join where null

--- dbt/include/global_project/macros/schema_tests/relationships.sql
+++ dbt/include/global_project/macros/schema_tests/relationships.sql
@@ -1,18 +1,15 @@
 
 {% macro test_relationships(model, field, to, from) %}
 
-
 select count(*)
 from (
-
     select
-        {{ from }} as id
-
-    from {{ model }}
-    where {{ from }} is not null
-      and {{ from }} not in (select {{ field }}
-                             from {{ to }})
-
+        t1.{{ from }}
+    from {{ model }} t1
+    left outer join {{ to }} t2
+        on  t1.{{ from }} = t2.{{ field }}
+    where t1.{{ from }} is not null
+        and	t2.{{ field }} is null
 ) validation_errors
 
 {% endmacro %}
