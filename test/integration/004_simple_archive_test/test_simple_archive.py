3ffca9c758c45949a2c319df0abacfc7570fa3da
Drew Banin <drew@fishtownanalytics.com>
2018-07-16 23:11:33 -0400

    extra test for nested/repeated records

--- test/integration/004_simple_archive_test/test_simple_archive.py
+++ test/integration/004_simple_archive_test/test_simple_archive.py
@@ -70,7 +70,7 @@ class TestSimpleArchive(DBTIntegrationTest):
         self.assertTablesEqual("ARCHIVE_EXPECTED", "archive_actual")
 
 
-class TestSimpleArchive(DBTIntegrationTest):
+class TestSimpleArchiveBigquery(DBTIntegrationTest):
 
     @property
     def schema(self):
@@ -82,8 +82,6 @@ class TestSimpleArchive(DBTIntegrationTest):
 
     @property
     def project_config(self):
-        source_table = 'seed'
-
         return {
             "archive": [
                 {
@@ -119,3 +117,38 @@ class TestSimpleArchive(DBTIntegrationTest):
 
         self.assertTablesEqual("archive_expected", "archive_actual")
 
+
+    @attr(type='bigquery')
+    def test__bigquery__archive_with_new_field(self):
+        self.use_default_project()
+        self.use_profile('bigquery')
+
+        self.run_sql_file("test/integration/004_simple_archive_test/seed_bq.sql")
+
+        self.run_dbt(["archive"])
+
+        self.assertTablesEqual("archive_expected", "archive_actual")
+
+        self.run_sql_file("test/integration/004_simple_archive_test/invalidate_bigquery.sql")
+        self.run_sql_file("test/integration/004_simple_archive_test/update_bq.sql")
+
+        # This adds new fields to the source table, and updates the expected archive output accordingly
+        self.run_sql_file("test/integration/004_simple_archive_test/add_column_to_source_bq.sql")
+
+        self.run_dbt(["archive"])
+
+        # A more thorough test would assert that archived == expected, but BigQuery does not support the
+        # "EXCEPT DISTINCT" operator on nested fields! Instead, just check that schemas are congruent.
+
+        expected_cols = self.adapter.get_columns_in_table(self._profile, self.project_config, self.unique_schema(), 'archive_expected')
+        archived_cols = self.adapter.get_columns_in_table(self._profile, self.project_config, self.unique_schema(), 'archive_actual')
+
+        self.assertTrue(len(expected_cols) > 0, "source table does not exist -- bad test")
+        self.assertEqual(len(expected_cols), len(archived_cols), "actual and expected column lengths are different")
+
+        for (expected_col, actual_col) in zip(expected_cols, archived_cols):
+            self.assertTrue(expected_col.name is not None)
+            self.assertTrue(expected_col.data_type is not None)
+
+            self.assertEqual(expected_col.name, actual_col.name, "names are different")
+            self.assertEqual(expected_col.data_type, actual_col.data_type, "data types are different")
