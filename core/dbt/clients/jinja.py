184a9ab646d3ded2ce7a25a6fc7903251f4499ce
Jacob Beck <jake@fishtownanalytics.com>
2018-10-23 07:38:51 -0600

    add a lot of handling for edge cases around captured undefined values
    automatic commit by git-black, original commits:
      5ef8df2faed1b37968d68ddf0b0a39afba0f6185

--- core/dbt/clients/jinja.py
+++ core/dbt/clients/jinja.py
@@ -425,8 +425,7 @@ def create_undefined(node=None):
         def __getattr__(self, name):
             if name == "name" or _is_dunder_name(name):
                 raise AttributeError(
-                    "'{}' object has no attribute '{}'"
-                    .format(type(self).__name__, name)
+                    "'{}' object has no attribute '{}'".format(type(self).__name__, name)
                 )
 
             self.name = name
