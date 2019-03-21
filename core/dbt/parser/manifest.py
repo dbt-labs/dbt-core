f0a2a65725811383e6e6d16a5e128f981ce89f95
Jacob Beck <jake@fishtownanalytics.com>
2019-03-21 08:42:40 -0600

    flake8, pep8, unit tests
    automatic commit by git-black, original commits:
      5e8ab9ce4a8ac905d66bf85414a0be121fc58dbd
      af8622e8fffbbf6d2022fb1e40f9b07ce2bb12b7

--- core/dbt/parser/manifest.py
+++ core/dbt/parser/manifest.py
@@ -950,9 +950,7 @@ def _check_resource_uniqueness(
 
         existing_node = names_resources.get(name)
         if existing_node is not None:
-            dbt.exceptions.raise_duplicate_resource_name(
-                existing_node, node
-            )
+            dbt.exceptions.raise_duplicate_resource_name(existing_node, node)
 
         existing_alias = alias_resources.get(full_node_name)
         if existing_alias is not None:
