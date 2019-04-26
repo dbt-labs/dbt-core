02161b83aa68fa47e0ee597df425a37a48c7aeab
Jacob Beck <jake@fishtownanalytics.com>
2019-04-26 11:16:05 -0600

    Fix postgres text handling
    Fix incorrect maximum text field length on redshift
    On postgres, pass through text without size information to fix archival of long text fields
    Add a test that makes sure postgres archives work with very long text entries
    Fix tests
    Add a redshift test
    
    
    automatic commit by git-black, original commits:
      7df6e0dc6840f95af430cbfb580c26244d621641

--- plugins/postgres/dbt/adapters/postgres/relation.py
+++ plugins/postgres/dbt/adapters/postgres/relation.py
@@ -27,6 +27,6 @@ class PostgresColumn(Column):
     @property
     def data_type(self):
         # on postgres, do not convert 'text' to 'varchar()'
-        if self.dtype.lower() == 'text':
+        if self.dtype.lower() == "text":
             return self.dtype
         return super().data_type
