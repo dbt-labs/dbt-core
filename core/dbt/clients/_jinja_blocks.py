3c066d7370e3bfd2d5ab2ab56f6b8ce2f09e55ea
Jacob Beck <jake@fishtownanalytics.com>
2020-01-24 13:08:53 -0700

    PR feedback: lines not chars
    automatic commit by git-black, original commits:
      de5ff68943c10ce3078d38cc7af106696ca25ea2

--- core/dbt/clients/_jinja_blocks.py
+++ core/dbt/clients/_jinja_blocks.py
@@ -365,11 +365,12 @@ class BlockIterator:
                 self.current = None
 
         if self.current:
-            linecount = self.data[:self.current.end].count('\n') + 1
+            linecount = self.data[: self.current.end].count("\n") + 1
             dbt.exceptions.raise_compiler_error(
                 'Reached EOF without finding a close tag for '
-                '{} (searched from line {})'
-            ).format(self.current.block_type_name, linecount))
+                    "Reached EOF without finding a close tag for " "{} (searched from line {})"
+                ).format(self.current.block_type_name, linecount)
+            )
 
         if collect_raw_data:
             raw_data = self.data[self.last_position :]
