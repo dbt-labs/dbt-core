carlineng/dbt

Add documentation for macros/analyses
Refactored parsing a bit to support the idea of:
 - macro patches (no columns)
 - node patches that don't get tests (analyses)

core/dbt/contracts/graph/unparsed.py
@@ -7,7 +7,7 @@ from hologram.helpers import StrEnum, ExtensibleJsonSchemaMixin
 
 from dataclasses import dataclass, field
 from datetime import timedelta
-from typing import Optional, List, Union, Dict, Any
+from typing import Optional, List, Union, Dict, Any, Sequence
 
 
 @dataclass
@@ -55,11 +55,15 @@ class UnparsedRunHook(UnparsedNode):
 
 
 @dataclass
-class NamedTested(JsonSchemaMixin, Replaceable):
+class HasDocs(JsonSchemaMixin, Replaceable):
     name: str
     description: str = ''
     meta: Dict[str, Any] = field(default_factory=dict)
     data_type: Optional[str] = None
+
+
+@dataclass
+class HasTests(HasDocs):
     tests: Optional[List[Union[Dict[str, Any], str]]] = None
 
     def __post_init__(self):
@@ -68,13 +72,13 @@ class NamedTested(JsonSchemaMixin, Replaceable):
 
 
 @dataclass
-class ColumnDescription(JsonSchemaMixin, Replaceable):
-    columns: List[NamedTested] = field(default_factory=list)
+class HasColumnDocs(JsonSchemaMixin, Replaceable):
+    columns: Sequence[HasDocs] = field(default_factory=list)
 
 
 @dataclass
-class NodeDescription(NamedTested):
-    pass
+class HasColumnTests(HasColumnDocs):
+    columns: Sequence[HasTests] = field(default_factory=list)
 
 
 @dataclass
@@ -85,9 +89,19 @@ class HasYamlMetadata(JsonSchemaMixin):
 
 
 @dataclass
-class UnparsedNodeUpdate(ColumnDescription, NodeDescription, HasYamlMetadata):
+class UnparsedAnalysisUpdate(HasColumnDocs, HasDocs, HasYamlMetadata):
+    pass
+
+
+@dataclass
+class UnparsedNodeUpdate(HasColumnTests, HasTests, HasYamlMetadata):
     def __post_init__(self):
-        NodeDescription.__post_init__(self)
+        HasTests.__post_init__(self)
+
+
+@dataclass
+class UnparsedMacroUpdate(HasDocs, HasYamlMetadata):
+    pass
 
 
 class TimePeriod(StrEnum):
@@ -196,7 +210,7 @@ class Quoting(JsonSchemaMixin, Mergeable):
 
 
 @dataclass
-class UnparsedSourceTableDefinition(ColumnDescription, NodeDescription):
+class UnparsedSourceTableDefinition(HasColumnTests, HasTests):
     loaded_at_field: Optional[str] = None
     identifier: Optional[str] = None
     quoting: Quoting = field(default_factory=Quoting)
@@ -208,7 +222,7 @@ class UnparsedSourceTableDefinition(ColumnDescription, NodeDescription):
     )
 
     def __post_init__(self):
-        NodeDescription.__post_init__(self)
+        HasTests.__post_init__(self)
 
 
 @dataclass

core/dbt/parser/schema_test_builders.py
@@ -1,12 +1,15 @@
 import hashlib
 import re
 from dataclasses import dataclass
-from typing import Generic, TypeVar, Dict, Any, Tuple, Optional, List, Union
+from typing import (
+    Generic, TypeVar, Dict, Any, Tuple, Optional, List, Union, Sequence
+)
 
 from dbt.clients.jinja import get_rendered
 from dbt.contracts.graph.unparsed import (
     UnparsedNodeUpdate, UnparsedSourceDefinition,
-    UnparsedSourceTableDefinition, NamedTested
+    UnparsedSourceTableDefinition, HasTests, UnparsedMacroUpdate,
+    UnparsedAnalysisUpdate
 )
 from dbt.exceptions import raise_compiler_error
 from dbt.parser.search import FileBlock
@@ -79,7 +82,7 @@ class SourceTarget:
         return '{0.name}_{1.name}'.format(self.source, self.table)
 
     @property
-    def columns(self) -> List[NamedTested]:
+    def columns(self) -> Sequence[HasTests]:
         if self.table.columns is None:
             return []
         else:
@@ -93,10 +96,22 @@ class SourceTarget:
             return self.table.tests
 
 
-NodeTarget = UnparsedNodeUpdate
+Testable = TypeVar('Testable', SourceTarget, UnparsedNodeUpdate)
 
+ColumnTarget = TypeVar(
+    'ColumnTarget',
+    SourceTarget,
+    UnparsedNodeUpdate,
+    UnparsedAnalysisUpdate,
+)
 
-Target = TypeVar('Target', NodeTarget, SourceTarget)
+Target = TypeVar(
+    'Target',
+    SourceTarget,
+    UnparsedNodeUpdate,
+    UnparsedMacroUpdate,
+    UnparsedAnalysisUpdate,
+)
 
 
 @dataclass
@@ -109,17 +124,11 @@ class TargetBlock(YamlBlock, Generic[Target]):
 
     @property
     def columns(self):
-        if self.target.columns is None:
-            return []
-        else:
-            return self.target.columns
+        return []
 
     @property
     def tests(self) -> List[Union[Dict[str, Any], str]]:
-        if self.target.tests is None:
-            return []
-        else:
-            return self.target.tests
+        return []
 
     @classmethod
     def from_yaml_block(
@@ -133,13 +142,43 @@ class TargetBlock(YamlBlock, Generic[Target]):
 
 
 @dataclass
-class SchemaTestBlock(TargetBlock):
+class TargetColumnsBlock(TargetBlock[ColumnTarget], Generic[ColumnTarget]):
+    @property
+    def columns(self):
+        if self.target.columns is None:
+            return []
+        else:
+            return self.target.columns
+
+
+@dataclass
+class TestBlock(TargetColumnsBlock[Testable], Generic[Testable]):
+    @property
+    def tests(self) -> List[Union[Dict[str, Any], str]]:
+        if self.target.tests is None:
+            return []
+        else:
+            return self.target.tests
+
+    @classmethod
+    def from_yaml_block(
+        cls, src: YamlBlock, target: Testable
+    ) -> 'TestBlock[Testable]':
+        return cls(
+            file=src.file,
+            data=src.data,
+            target=target,
+        )
+
+
+@dataclass
+class SchemaTestBlock(TestBlock[Testable], Generic[Testable]):
     test: Dict[str, Any]
     column_name: Optional[str]
 
     @classmethod
-    def from_target_block(
-        cls, src: TargetBlock, test: Dict[str, Any], column_name: Optional[str]
+    def from_test_block(
+        cls, src: TestBlock, test: Dict[str, Any], column_name: Optional[str]
     ) -> 'SchemaTestBlock':
         return cls(
             file=src.file,
@@ -150,7 +189,7 @@ class SchemaTestBlock(TargetBlock):
         )
 
 
-class TestBuilder(Generic[Target]):
+class TestBuilder(Generic[Testable]):
     """An object to hold assorted test settings and perform basic parsing
 
     Test names have the following pattern:
@@ -170,7 +209,7 @@ class TestBuilder(Generic[Target]):
     def __init__(
         self,
         test: Dict[str, Any],
-        target: Target,
+        target: Testable,
         package_name: str,
         render_ctx: Dict[str, Any],
         column_name: str = None,
@@ -178,7 +217,7 @@ class TestBuilder(Generic[Target]):
         test_name, test_args = self.extract_test_args(test, column_name)
         self.args: Dict[str, Any] = test_args
         self.package_name: str = package_name
-        self.target: Target = target
+        self.target: Testable = target
 
         match = self.TEST_NAME_PATTERN.match(test_name)
         if match is None:
@@ -257,7 +296,7 @@ class TestBuilder(Generic[Target]):
         return macro_name
 
     def describe_test_target(self) -> str:
-        if isinstance(self.target, NodeTarget):
+        if isinstance(self.target, UnparsedNodeUpdate):
             fmt = "model('{0}')"
         elif isinstance(self.target, SourceTarget):
             fmt = "source('{0.source}', '{0.table}')"
@@ -268,7 +307,7 @@ class TestBuilder(Generic[Target]):
         raise NotImplementedError('describe_test_target not implemented!')
 
     def get_test_name(self) -> Tuple[str, str]:
-        if isinstance(self.target, NodeTarget):
+        if isinstance(self.target, UnparsedNodeUpdate):
             name = self.name
         elif isinstance(self.target, SourceTarget):
             name = 'source_' + self.name
@@ -290,7 +329,7 @@ class TestBuilder(Generic[Target]):
         )
 
     def build_model_str(self):
-        if isinstance(self.target, NodeTarget):
+        if isinstance(self.target, UnparsedNodeUpdate):
             fmt = "ref('{0.name}')"
         elif isinstance(self.target, SourceTarget):
             fmt = "source('{0.source.name}', '{0.table.name}')"

dbt-labs/dbt-core

core/dbt/contracts/graph/unparsed.py
@@ -7,7 +7,7 @@ from hologram.helpers import StrEnum, ExtensibleJsonSchemaMixin
 
 from dataclasses import dataclass, field
 from datetime import timedelta
-from typing import Optional, List, Union, Dict, Any
+from typing import Optional, List, Union, Dict, Any, Sequence
 
 
 @dataclass
@@ -59,12 +59,19 @@ class UnparsedRunHook(UnparsedNode):
 
 
 @dataclass
-class NamedTested(JsonSchemaMixin, Replaceable):
+class HasDocs(JsonSchemaMixin, Replaceable):
     name: str
     description: str = ''
     meta: Dict[str, Any] = field(default_factory=dict)
     data_type: Optional[str] = None
-    tests: Optional[List[Union[Dict[str, Any], str]]] = None
+
+
+TestDef = Union[Dict[str, Any], str]
+
+
+@dataclass
+class HasTests(HasDocs):
+    tests: Optional[List[TestDef]] = None
 
     def __post_init__(self):
         if self.tests is None:
@@ -72,18 +79,18 @@ class NamedTested(JsonSchemaMixin, Replaceable):
 
 
 @dataclass
-class UnparsedColumn(NamedTested):
+class UnparsedColumn(HasTests):
     tags: List[str] = field(default_factory=list)
 
 
 @dataclass
-class ColumnDescription(JsonSchemaMixin, Replaceable):
-    columns: List[UnparsedColumn] = field(default_factory=list)
+class HasColumnDocs(JsonSchemaMixin, Replaceable):
+    columns: Sequence[HasDocs] = field(default_factory=list)
 
 
 @dataclass
-class NodeDescription(NamedTested):
-    pass
+class HasColumnTests(HasColumnDocs):
+    columns: Sequence[UnparsedColumn] = field(default_factory=list)
 
 
 @dataclass
@@ -94,9 +101,18 @@ class HasYamlMetadata(JsonSchemaMixin):
 
 
 @dataclass
-class UnparsedNodeUpdate(ColumnDescription, NodeDescription, HasYamlMetadata):
-    def __post_init__(self):
-        NodeDescription.__post_init__(self)
+class UnparsedAnalysisUpdate(HasColumnDocs, HasDocs, HasYamlMetadata):
+    pass
+
+
+@dataclass
+class UnparsedNodeUpdate(HasColumnTests, HasTests, HasYamlMetadata):
+    pass
+
+
+@dataclass
+class UnparsedMacroUpdate(HasDocs, HasYamlMetadata):
+    pass
 
 
 class TimePeriod(StrEnum):
@@ -205,7 +221,7 @@ class Quoting(JsonSchemaMixin, Mergeable):
 
 
 @dataclass
-class UnparsedSourceTableDefinition(ColumnDescription, NodeDescription):
+class UnparsedSourceTableDefinition(HasColumnTests, HasTests):
     loaded_at_field: Optional[str] = None
     identifier: Optional[str] = None
     quoting: Quoting = field(default_factory=Quoting)
@@ -216,9 +232,6 @@ class UnparsedSourceTableDefinition(ColumnDescription, NodeDescription):
         default_factory=ExternalTable
     )
     tags: List[str] = field(default_factory=list)
-
-    def __post_init__(self):
-        NodeDescription.__post_init__(self)
 
 
 @dataclass

core/dbt/parser/schema_test_builders.py
@@ -1,12 +1,15 @@
 import hashlib
 import re
 from dataclasses import dataclass
-from typing import Generic, TypeVar, Dict, Any, Tuple, Optional, List, Union
+from typing import (
+    Generic, TypeVar, Dict, Any, Tuple, Optional, List, Sequence
+)
 
 from dbt.clients.jinja import get_rendered
 from dbt.contracts.graph.unparsed import (
     UnparsedNodeUpdate, UnparsedSourceDefinition,
-    UnparsedSourceTableDefinition, UnparsedColumn
+    UnparsedSourceTableDefinition, UnparsedColumn, UnparsedMacroUpdate,
+    UnparsedAnalysisUpdate, TestDef
 )
 from dbt.exceptions import raise_compiler_error
 from dbt.parser.search import FileBlock
@@ -79,24 +82,36 @@ class SourceTarget:
         return '{0.name}_{1.name}'.format(self.source, self.table)
 
     @property
-    def columns(self) -> List[UnparsedColumn]:
+    def columns(self) -> Sequence[UnparsedColumn]:
         if self.table.columns is None:
             return []
         else:
             return self.table.columns
 
     @property
-    def tests(self) -> List[Union[Dict[str, Any], str]]:
+    def tests(self) -> List[TestDef]:
         if self.table.tests is None:
             return []
         else:
             return self.table.tests
 
 
-NodeTarget = UnparsedNodeUpdate
+Testable = TypeVar('Testable', SourceTarget, UnparsedNodeUpdate)
 
+ColumnTarget = TypeVar(
+    'ColumnTarget',
+    SourceTarget,
+    UnparsedNodeUpdate,
+    UnparsedAnalysisUpdate,
+)
 
-Target = TypeVar('Target', NodeTarget, SourceTarget)
+Target = TypeVar(
+    'Target',
+    SourceTarget,
+    UnparsedNodeUpdate,
+    UnparsedMacroUpdate,
+    UnparsedAnalysisUpdate,
+)
 
 
 @dataclass
@@ -109,17 +124,11 @@ class TargetBlock(YamlBlock, Generic[Target]):
 
     @property
     def columns(self):
-        if self.target.columns is None:
-            return []
-        else:
-            return self.target.columns
+        return []
 
     @property
-    def tests(self) -> List[Union[Dict[str, Any], str]]:
-        if self.target.tests is None:
-            return []
-        else:
-            return self.target.tests
+    def tests(self) -> List[TestDef]:
+        return []
 
     @classmethod
     def from_yaml_block(
@@ -133,15 +142,45 @@ class TargetBlock(YamlBlock, Generic[Target]):
 
 
 @dataclass
-class SchemaTestBlock(TargetBlock):
+class TargetColumnsBlock(TargetBlock[ColumnTarget], Generic[ColumnTarget]):
+    @property
+    def columns(self):
+        if self.target.columns is None:
+            return []
+        else:
+            return self.target.columns
+
+
+@dataclass
+class TestBlock(TargetColumnsBlock[Testable], Generic[Testable]):
+    @property
+    def tests(self) -> List[TestDef]:
+        if self.target.tests is None:
+            return []
+        else:
+            return self.target.tests
+
+    @classmethod
+    def from_yaml_block(
+        cls, src: YamlBlock, target: Testable
+    ) -> 'TestBlock[Testable]':
+        return cls(
+            file=src.file,
+            data=src.data,
+            target=target,
+        )
+
+
+@dataclass
+class SchemaTestBlock(TestBlock[Testable], Generic[Testable]):
     test: Dict[str, Any]
     column_name: Optional[str]
     tags: List[str]
 
     @classmethod
-    def from_target_block(
+    def from_test_block(
         cls,
-        src: TargetBlock,
+        src: TestBlock,
         test: Dict[str, Any],
         column_name: Optional[str],
         tags: List[str],
@@ -156,7 +195,7 @@ class SchemaTestBlock(TargetBlock):
         )
 
 
-class TestBuilder(Generic[Target]):
+class TestBuilder(Generic[Testable]):
     """An object to hold assorted test settings and perform basic parsing
 
     Test names have the following pattern:
@@ -174,7 +213,7 @@ class TestBuilder(Generic[Target]):
     def __init__(
         self,
         test: Dict[str, Any],
-        target: Target,
+        target: Testable,
         package_name: str,
         render_ctx: Dict[str, Any],
         column_name: str = None,
@@ -182,7 +221,7 @@ class TestBuilder(Generic[Target]):
         test_name, test_args = self.extract_test_args(test, column_name)
         self.args: Dict[str, Any] = test_args
         self.package_name: str = package_name
-        self.target: Target = target
+        self.target: Testable = target
 
         match = self.TEST_NAME_PATTERN.match(test_name)
         if match is None:
@@ -277,7 +316,7 @@ class TestBuilder(Generic[Target]):
         return macro_name
 
     def describe_test_target(self) -> str:
-        if isinstance(self.target, NodeTarget):
+        if isinstance(self.target, UnparsedNodeUpdate):
             fmt = "model('{0}')"
         elif isinstance(self.target, SourceTarget):
             fmt = "source('{0.source}', '{0.table}')"
@@ -288,7 +327,7 @@ class TestBuilder(Generic[Target]):
         raise NotImplementedError('describe_test_target not implemented!')
 
     def get_test_name(self) -> Tuple[str, str]:
-        if isinstance(self.target, NodeTarget):
+        if isinstance(self.target, UnparsedNodeUpdate):
             name = self.name
         elif isinstance(self.target, SourceTarget):
             name = 'source_' + self.name
@@ -310,7 +349,7 @@ class TestBuilder(Generic[Target]):
         )
 
     def build_model_str(self):
-        if isinstance(self.target, NodeTarget):
+        if isinstance(self.target, UnparsedNodeUpdate):
             fmt = "ref('{0.name}')"
         elif isinstance(self.target, SourceTarget):
             fmt = "source('{0.source.name}', '{0.table.name}')"