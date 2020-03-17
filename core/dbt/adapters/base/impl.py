90b2849b5bb0b922b31e1776df031c732276761c
Jacob Beck <jake@fishtownanalytics.com>
2020-03-17 12:34:09 -0600

    Fix issue with table databases/schemas/names not being interpreted as strings if they look like numbers
    automatic commit by git-black, original commits:
      3a77626cd728f047a0c3a096a750fe90af467d6e

--- core/dbt/adapters/base/impl.py
+++ core/dbt/adapters/base/impl.py
@@ -925,7 +925,7 @@ class BaseAdapter(metaclass=AdapterMeta):
         table = table_from_rows(
             table.rows,
             table.column_names,
-            text_only_columns=['table_database', 'table_schema', 'table_name']
+            text_only_columns=["table_database", "table_schema", "table_name"],
         )
         return table.where(_catalog_filter_schemas(manifest))
 
