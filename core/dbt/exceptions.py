de5a91ce29d4737ba4f606d5b4272a50167a262c
Jacob Beck <jake@fishtownanalytics.com>
2020-04-30 14:44:49 -0600

    fix the ambiguous alias exception to respect databases
    The alias name check is now tied to the behavior of adapter.Relation.create_from(...)
    Plugins that override the `include` of their relations will use whatever they render to for the check
    the actual exception-raising code gets the name that was compared instead of generating its own
    Finally, I added a reasonable fallback behavior since this method was exposed to the context
    
    
    automatic commit by git-black, original commits:
      20c5c4c3dd304f6fe68613b5c679d745e5c0e074

--- core/dbt/exceptions.py
+++ core/dbt/exceptions.py
@@ -873,8 +873,8 @@ def raise_ambiguous_alias(node_1, node_2, duped_name=None):
     raise_compiler_error(
         'dbt found two resources with the database representation "{}".\ndbt '
         "cannot create two resources with identical database representations. "
-        'To fix this,\nchange the configuration of one of these resources:'
-        '\n- {} ({})\n- {} ({})'.format(
+        "To fix this,\nchange the configuration of one of these resources:"
+        "\n- {} ({})\n- {} ({})".format(
             duped_name,
             node_1.unique_id,
             node_1.original_file_path,
