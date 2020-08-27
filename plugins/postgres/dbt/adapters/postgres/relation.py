9965fd200a2f500c7c1b60b6019fcdd358dd8df2
Gerda Shank <gerda@fishtownanalytics.com>
2020-08-27 16:35:35 -0400

    Tweak error message, reformat for flake8
    automatic commit by git-black, original commits:
      5830f5590ed185116dd4807f6351ad3afeb0dd5d

--- plugins/postgres/dbt/adapters/postgres/relation.py
+++ plugins/postgres/dbt/adapters/postgres/relation.py
@@ -9,8 +9,11 @@ class PostgresRelation(BaseRelation):
     def __post_init__(self):
         # Check for length of Postgres table/view names.
         # Check self.type to exclude test relation identifiers
-        if (self.identifier is not None and self.type is not None and
-                len(self.identifier) > self.relation_max_name_length()):
+        if (
+            self.identifier is not None
+            and self.type is not None
+            and len(self.identifier) > self.relation_max_name_length()
+        ):
             raise RuntimeException(
                 f"Relation name '{self.identifier}' "
                 f"is longer than {self.relation_max_name_length()} characters"
