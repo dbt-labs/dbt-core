96eb4ac528de1379f70a47297ab8e94e8f274c4b
Teddy Crepineau <teddy.crepineau@gmail.com>
2021-04-18 23:52:31 +0200

    fixed typo in comment

--- test/unit/test_registry_get_request_timeout.py
+++ test/unit/test_registry_get_request_timeout.py
@@ -5,5 +5,5 @@ from dbt.clients.registry import _get
 
 class testRegistryGetRequestTimeout(unittest.TestCase):
     def test_registry_request_timeout(self):
-        # using non routable IP to test time out logic in the _get function
+        # using non routable IP to test timeout logic in the _get function
         self.assertRaises(Timeout, _get, '', 'http://10.255.255.1')
