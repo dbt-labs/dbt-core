c28493cd193f2c0d510d07bbe8541b88ba6b63d2
Teddy Crepineau <teddy.crepineau@gmail.com>
2021-05-01 13:35:05 +0200

    updated comment in test_registry_get_request_exception.py to reflect new approach

--- test/unit/test_registry_get_request_exception.py
+++ test/unit/test_registry_get_request_exception.py
@@ -5,5 +5,5 @@ from dbt.clients.registry import _get
 
 class testRegistryGetRequestException(unittest.TestCase):
     def test_registry_request_error_catching(self):
-        # using closed port to test timeout logic in the _get function
+        # using non routable IP to test connection error logic in the _get function
         self.assertRaises(RegistryException, _get, '', 'http://10.255.255.1')
