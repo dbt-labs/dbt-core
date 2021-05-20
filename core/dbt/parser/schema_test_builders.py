d571db411906abfad79558b3b4e6e05da117f0da
Kyle Wigley <kyle@fishtownanalytics.com>
2021-05-20 15:18:44 -0400

    respond to some feedback

--- core/dbt/parser/schema_test_builders.py
+++ core/dbt/parser/schema_test_builders.py
@@ -299,7 +299,7 @@ class TestBuilder(Generic[Testable]):
     def where(self) -> Optional[str]:
         return self.modifiers.get('where')
 
-    def limit(self) -> Optional[str]:
+    def limit(self) -> Optional[int]:
         return self.modifiers.get('limit')
 
     def warn_if(self) -> Optional[str]:
