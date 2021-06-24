cf5c68323a54269953e7606dcfad38e4be5261cf
Daniele Frigo <daniele.frigo@gmail.com>
2021-06-24 09:49:54 +0200

    table and view materializations should rename from old_relation to manage changes from view to table and reverse

--- core/dbt/include/global_project/macros/materializations/table/table.sql
+++ core/dbt/include/global_project/macros/materializations/table/table.sql
@@ -42,7 +42,7 @@
 
   -- cleanup
   {% if old_relation is not none %}
-      {{ adapter.rename_relation(target_relation, backup_relation) }}
+      {{ adapter.rename_relation(old_relation, backup_relation) }}
   {% endif %}
 
   {{ adapter.rename_relation(intermediate_relation, target_relation) }}
