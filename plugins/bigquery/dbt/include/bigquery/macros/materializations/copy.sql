02fbe5806b654bd4691366080b70589b59bc8ada
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2021-07-21 16:51:52 +0300

    Change BigQuery copy materialization
    Change BigQuery copy materialization macros to copy data from several sources into single target

--- plugins/bigquery/dbt/include/bigquery/macros/materializations/copy.sql
+++ plugins/bigquery/dbt/include/bigquery/macros/materializations/copy.sql
@@ -3,31 +3,24 @@
   {# Setup #}
   {{ run_hooks(pre_hooks) }}
 
-  {# there should be exactly one ref or exactly one source #}
   {% set destination = this.incorporate(type='table') %}
 
-  {% set dependency_type = none %}
-  {% if (model.refs | length) == 1 and (model.sources | length) == 0 %}
-    {% set dependency_type = 'ref' %}
-  {% elif (model.refs | length) == 0 and (model.sources | length) == 1 %}
-    {% set dependency_type = 'source' %}
-  {% else %}
-    {% set msg %}
-        Expected exactly one ref or exactly one source, instead got {{ model.refs | length }} models and {{ model.sources | length }} sources.
-    {% endset %}
-    {% do exceptions.raise_compiler_error(msg) %}
-  {% endif %}
 
-  {% if dependency_type == 'ref' %}
-    {% set src =  ref(*model.refs[0]) %}
-  {% else %}
-    {% set src =  source(*model.sources[0]) %}
-  {% endif %}
+  {# Cycle over ref() and source() to create source tables array #}
+  {% set src = [] %}
+  {% for ref_table in model.refs %}
+    {{ src.append(ref(*ref_table)) }}
+  {% endfor %}
 
+  {% for src_table in model.sources %}
+    {{ src.append(source(*src_table)) }}
+  {% endfor %}
+
+  {# Call adapter's copy_table function #}
   {%- set result_str = adapter.copy_table(
       src,
       destination,
-      config.get('copy_materialization', 'table')) -%}
+      config.get('copy_materialization', default = 'table')) -%}
 
   {{ store_result('main', response=result_str) }}
 
