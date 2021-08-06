dde0c6eb2f325c3267a45484f8874306252f9710
Sung Won Chung <sungwonchung3@gmail.com>
2021-08-06 09:50:36 -0500

    add current sanitized label

--- plugins/bigquery/dbt/adapters/bigquery/connections.py
+++ plugins/bigquery/dbt/adapters/bigquery/connections.py
@@ -605,7 +605,7 @@ def _sanitize_label(value: str) -> str:
     value_length = len(value)
     if value_length > _VALIDATE_LABEL_LENGTH_LIMIT:
         error_msg = (
-            f"Current label length {value_length} is greater than length limit: {_VALIDATE_LABEL_LENGTH_LIMIT}"
+            f"Current label length {value_length} is greater than length limit: {_VALIDATE_LABEL_LENGTH_LIMIT} | Current sanitized label: {value}"
         )
         raise Exception(error_msg)
     else:
