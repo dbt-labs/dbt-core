d28ef22b473b0b44022528dcc5ac41713eaa42a1
Jeremy Cohen <jeremy@fishtownanalytics.com>
2021-08-10 08:54:04 -0400

    Fix flake8

--- core/dbt/adapters/base/impl.py
+++ core/dbt/adapters/base/impl.py
@@ -31,7 +31,6 @@ from dbt.contracts.graph.compiled import (
 from dbt.contracts.graph.manifest import Manifest, MacroManifest
 from dbt.contracts.graph.parsed import ParsedSeedNode
 from dbt.exceptions import warn_or_error
-from dbt.node_types import NodeType
 from dbt.logger import GLOBAL_LOGGER as logger
 from dbt.utils import filter_null_values, executor
 
