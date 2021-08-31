0933bdeda8f9299cadcd80129d6a9c35bf1f1736
Andreas Törnström <andreas.andersson@aw.com>
2021-08-31 07:59:32 +0200

    3682 Tested to add default kms to dataset and accidently pushed the changes

--- plugins/bigquery/dbt/adapters/bigquery/connections.py
+++ plugins/bigquery/dbt/adapters/bigquery/connections.py
@@ -525,11 +525,10 @@ class BigQueryConnectionManager(BaseConnectionManager):
         self._retry_and_handle(
             msg='drop dataset', conn=conn, fn=fn)
 
-    def create_dataset(self, database, schema, opts={}):
+    def create_dataset(self, database, schema):
         conn = self.get_thread_connection()
         client = conn.handle
         dataset = self.dataset(database, schema, conn)
-        dataset.default_encryption_configuration = opts['kms_key_name']
 
         def fn():
             return client.create_dataset(dataset, exists_ok=True)
