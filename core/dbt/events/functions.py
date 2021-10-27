fe66c7f2d3b00cb57f52f82ba74331fa59cc8840
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-10-27 16:42:32 -0400

    revert flake8 command change

--- core/dbt/events/functions.py
+++ core/dbt/events/functions.py
@@ -1,7 +1,7 @@
 
 import dbt.logger as logger  # type: ignore # TODO eventually remove dependency on this logger
 from dbt.events.history import EVENT_HISTORY
-from dbt.events.types import *  # noqa: F403
+from dbt.events.types import CliEventABC, Event
 
 
 # top-level method for accessing the new eventing system
