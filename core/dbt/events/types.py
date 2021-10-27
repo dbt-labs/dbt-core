b130e5ba298970b3509b1db0230a1d2e146ce3e2
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-10-27 16:41:17 -0400

    OO style

--- core/dbt/events/types.py
+++ core/dbt/events/types.py
@@ -1,92 +1,93 @@
-
-from typing import NamedTuple, Union
+from abc import ABCMeta, abstractmethod
 
 
 # The following classes represent the data necessary to describe a
 # particular event to both human readable logs, and machine reliable
-# event streams. The transformation to these forms will live in outside
-# functions.
+# event streams. classes extend superclasses that indicate what
+# destinations they are intended for, which mypy uses to enforce
+# that the necessary methods are defined.
+
+
+# top-level superclass for all events
+class Event():
+    pass
+
+
+class CliEventABC(Event, metaclass=ABCMeta):
+    # Solely the human readable message. Timestamps and formatting will be added by the logger
+    @abstractmethod
+    def cli_msg(self) -> str:
+        raise Exception("cli_msg not implemented for cli event")
+
+
+# base class used for type-level membership checking only
+class ParsingProgressBase():
+    pass
+
+
+class ParsingStart(CliEventABC, ParsingProgressBase):
+    def cli_msg(self) -> str:
+        return "Start parsing."
+
+
+class ParsingCompiling(CliEventABC, ParsingProgressBase):
+    def cli_msg(self) -> str:
+        return "Compiling."
+
+
+class ParsingWritingManifest(CliEventABC, ParsingProgressBase):
+    def cli_msg(self) -> str:
+        return "Writing manifest."
+
+
+class ParsingDone(CliEventABC, ParsingProgressBase):
+    def cli_msg(self) -> str:
+        return "Done."
+
+
+# base class used for type-level membership checking only
+class ManifestProgressBase():
+    pass
+
+
+class ManifestDependenciesLoaded(CliEventABC, ManifestProgressBase):
+    def cli_msg(self) -> str:
+        return "Dependencies loaded"
+
+
+class ManifestLoaderCreated(CliEventABC, ManifestProgressBase):
+    def cli_msg(self) -> str:
+        return "ManifestLoader created"
+
+
+class ManifestLoaded(CliEventABC, ManifestProgressBase):
+    def cli_msg(self) -> str:
+        return "Manifest loaded"
+
+
+class ManifestChecked(CliEventABC, ManifestProgressBase):
+    def cli_msg(self) -> str:
+        return "Manifest checked"
+
+
+class ManifestFlatGraphBuilt(CliEventABC, ManifestProgressBase):
+    def cli_msg(self) -> str:
+        return "Flat graph built"
+
+
+# since mypy doesn't run on every file we need to suggest to mypy that every
+# class gets instantiated. But we don't actually want to run this code.
+# making the conditional `if False` causes mypy to skip it as dead code so
+# we need to skirt around that by computing something it doesn't check statically.
 #
-# Until we drop support for Python 3.6 we must use NamedTuples over
-# frozen dataclasses.
-
-
-# base class used for type-level membership checking only
-class ParsingProgressBase(NamedTuple):
-    pass
-
-
-class ParsingStart(ParsingProgressBase):
-    pass
-
-
-class ParsingCompiling(ParsingProgressBase):
-    pass
-
-
-class ParsingWritingManifest(ParsingProgressBase):
-    pass
-
-
-class ParsingDone(ParsingProgressBase):
-    pass
-
-
-# using a union instead of inheritance means that this set cannot
-# be extended outside this file, and thus mypy can do exhaustiveness
-# checks for us.
-
-# type for parsing progress events
-ParsingProgress = Union[
-    ParsingStart,
-    ParsingCompiling,
-    ParsingWritingManifest,
-    ParsingDone
-]
-
-
-# base class used for type-level membership checking only
-class ManifestProgressBase(NamedTuple):
-    pass
-
-
-class ManifestDependenciesLoaded(ManifestProgressBase):
-    pass
-
-
-class ManifestLoaderCreated(ManifestProgressBase):
-    pass
-
-
-class ManifestLoaded(ManifestProgressBase):
-    pass
-
-
-class ManifestChecked(ManifestProgressBase):
-    pass
-
-
-class ManifestFlatGraphBuilt(ManifestProgressBase):
-    pass
-
-
-# type for manifest loading progress events
-ManifestProgress = Union[
-    ManifestDependenciesLoaded,
-    ManifestLoaderCreated,
-    ManifestLoaded,
-    ManifestChecked,
-    ManifestFlatGraphBuilt
-]
-
-# top-level event type for all events that go to the CLI
-CliEvent = Union[
-    ParsingProgress,
-    ManifestProgress
-]
-
-# top-level event type for all events
-Event = Union[
-    ParsingProgress,
-    ManifestProgress
-]
+# TODO remove these lines once we run mypy everywhere.
+if 1 == 0:
+    ParsingStart()
+    ParsingCompiling()
+    ParsingWritingManifest()
+    ParsingDone()
+    ManifestDependenciesLoaded()
+    ManifestLoaderCreated()
+    ManifestLoaded()
+    ManifestChecked()
+    ManifestFlatGraphBuilt()
