57ef51d7924f45cb5d780b8e481d73780d8ed567
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-10-28 11:32:36 -0400

    fix silly bug

--- core/dbt/events/functions.py
+++ core/dbt/events/functions.py
@@ -11,16 +11,16 @@ from dbt.events.types import CliEventABC, Event
 def fire_event(e: Event) -> None:
     EVENT_HISTORY.append(e)
     if isinstance(e, CliEventABC):
-        if e.level_tag == 'test':
+        if e.level_tag() == 'test':
             # TODO after implmenting #3977 send to new test level
             logger.GLOBAL_LOGGER.debug(logger.timestamped_line(e.cli_msg()))
-        elif e.level_tag == 'debug':
+        elif e.level_tag() == 'debug':
             logger.GLOBAL_LOGGER.debug(logger.timestamped_line(e.cli_msg()))
-        elif e.level_tag == 'info':
+        elif e.level_tag() == 'info':
             logger.GLOBAL_LOGGER.info(logger.timestamped_line(e.cli_msg()))
-        elif e.level_tag == 'warn':
+        elif e.level_tag() == 'warn':
             logger.GLOBAL_LOGGER.warning()(logger.timestamped_line(e.cli_msg()))
-        elif e.level_tag == 'error':
+        elif e.level_tag() == 'error':
             logger.GLOBAL_LOGGER.error(logger.timestamped_line(e.cli_msg()))
         else:
-            raise AssertionError("Event with unhandled level: {}".format(type(e).__name__))
+            raise AssertionError(f"Event type {type(e).__name__} has unhandled level: {e.level_tag()}")
