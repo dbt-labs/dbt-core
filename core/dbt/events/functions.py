b2116e868ebddb8e30a109e0175b2d80b453b5b9
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-11-04 12:10:57 -0400

    lazy logging in event module

--- core/dbt/events/functions.py
+++ core/dbt/events/functions.py
@@ -3,7 +3,7 @@ import dbt.logger as logger  # type: ignore # TODO eventually remove dependency 
 from dbt.events.history import EVENT_HISTORY
 from dbt.events.types import CliEventABC, Event, ShowException
 import os
-from typing import List
+from typing import Callable, List
 
 
 def env_secrets() -> List[str]:
@@ -22,6 +22,14 @@ def scrub_secrets(msg: str, secrets: List[str]) -> str:
     return scrubbed
 
 
+# this exists because some log messages are actually expensive to build.
+# for example, many debug messages call `dump_graph()` and we don't want to
+# do that in the event that those messages are never going to be sent to
+# the user because we are only logging info-level events.
+def lazy_log_msg(e: CliEventABC) -> Callable[[], str]:
+    return lambda: scrub_secrets(e.cli_msg(), env_secrets())
+
+
 # top-level method for accessing the new eventing system
 # this is where all the side effects happen branched by event type
 # (i.e. - mutating the event history, printing to stdout, logging
@@ -29,50 +37,50 @@ def scrub_secrets(msg: str, secrets: List[str]) -> str:
 def fire_event(e: Event) -> None:
     EVENT_HISTORY.append(e)
     if isinstance(e, CliEventABC):
-        msg = scrub_secrets(e.cli_msg(), env_secrets())
+        msg: Callable[[], str] = lazy_log_msg(e)
         if e.level_tag() == 'test' and not isinstance(e, ShowException):
             # TODO after implmenting #3977 send to new test level
-            logger.GLOBAL_LOGGER.debug(logger.timestamped_line(msg))
+            logger.GLOBAL_LOGGER.debug(logger.timestamped_line(msg()))
         elif e.level_tag() == 'test' and isinstance(e, ShowException):
             # TODO after implmenting #3977 send to new test level
             logger.GLOBAL_LOGGER.debug(
-                logger.timestamped_line(msg),
+                logger.timestamped_line(msg()),
                 exc_info=e.exc_info,
                 stack_info=e.stack_info,
                 extra=e.extra
             )
         elif e.level_tag() == 'debug' and not isinstance(e, ShowException):
-            logger.GLOBAL_LOGGER.debug(logger.timestamped_line(msg))
+            logger.GLOBAL_LOGGER.debug(logger.timestamped_line(msg()))
         elif e.level_tag() == 'debug' and isinstance(e, ShowException):
             logger.GLOBAL_LOGGER.debug(
-                logger.timestamped_line(msg),
+                logger.timestamped_line(msg()),
                 exc_info=e.exc_info,
                 stack_info=e.stack_info,
                 extra=e.extra
             )
         elif e.level_tag() == 'info' and not isinstance(e, ShowException):
-            logger.GLOBAL_LOGGER.info(logger.timestamped_line(msg))
+            logger.GLOBAL_LOGGER.info(logger.timestamped_line(msg()))
         elif e.level_tag() == 'info' and isinstance(e, ShowException):
             logger.GLOBAL_LOGGER.info(
-                logger.timestamped_line(msg),
+                logger.timestamped_line(msg()),
                 exc_info=e.exc_info,
                 stack_info=e.stack_info,
                 extra=e.extra
             )
         elif e.level_tag() == 'warn' and not isinstance(e, ShowException):
-            logger.GLOBAL_LOGGER.warning(logger.timestamped_line(msg))
+            logger.GLOBAL_LOGGER.warning(logger.timestamped_line(msg()))
         elif e.level_tag() == 'warn' and isinstance(e, ShowException):
             logger.GLOBAL_LOGGER.warning(
-                logger.timestamped_line(msg),
+                logger.timestamped_line(msg()),
                 exc_info=e.exc_info,
                 stack_info=e.stack_info,
                 extra=e.extra
             )
         elif e.level_tag() == 'error' and not isinstance(e, ShowException):
-            logger.GLOBAL_LOGGER.error(logger.timestamped_line(msg))
+            logger.GLOBAL_LOGGER.error(logger.timestamped_line(msg()))
         elif e.level_tag() == 'error' and isinstance(e, ShowException):
             logger.GLOBAL_LOGGER.error(
-                logger.timestamped_line(msg),
+                logger.timestamped_line(msg()),
                 exc_info=e.exc_info,
                 stack_info=e.stack_info,
                 extra=e.extra
