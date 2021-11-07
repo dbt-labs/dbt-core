99b40732713c1fe5b184a8e8c38c149cef8c7eb6
Jeremy Cohen <jeremy@dbtlabs.com>
2021-11-07 19:16:26 +0100

    Rm incremental_upsert, now unused

--- core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
@@ -63,12 +63,6 @@
 
 {%- endmacro %}
 
-{# here for backwards compatibility #}
-{% macro incremental_upsert(tmp_relation, target_relation, unique_key=none, statement_name="main") %}
-    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
-    {{ get_delete_insert_merge_sql(target_relation, tmp_relation, unique_key, dest_columns) }}
-{%- endmacro %}
-
 
 {% macro get_insert_overwrite_merge_sql(target, source, dest_columns, predicates, include_sql_header=false) -%}
   {{ adapter.dispatch('get_insert_overwrite_merge_sql', 'dbt')(target, source, dest_columns, predicates, include_sql_header) }}
