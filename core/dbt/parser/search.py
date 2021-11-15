7587e144518776c1ddc8cfd8643c92117fcd961d
Gerda Shank <gerda@fishtownanalytics.com>
2021-11-15 06:46:17 -0800

    [#3689] Fix filesystem searcher and tests that mock it (#4271)
    automatic commit by git-black, original commits:
      3d28b6704ce53446293fc0acecb0b57cd0f6f6ba

--- core/dbt/parser/search.py
+++ core/dbt/parser/search.py
@@ -66,14 +66,14 @@ def filesystem_search(project: Project, relative_dirs: List[str], extension: str
     root = project.project_root
     file_path_list = []
     for result in find_matching(root, relative_dirs, ext):
-        if 'searched_path' not in result or 'relative_path' not in result:
+        if "searched_path" not in result or "relative_path" not in result:
             raise InternalException(
                 'Invalid result from find_matching: {}'.format(result)
             )
         file_match = FilePath(
-            searched_path=result['searched_path'],
-            relative_path=result['relative_path'],
-            modification_time=result['modification_time'],
+            searched_path=result["searched_path"],
+            relative_path=result["relative_path"],
+            modification_time=result["modification_time"],
             project_root=root,
         )
         file_path_list.append(file_match)
