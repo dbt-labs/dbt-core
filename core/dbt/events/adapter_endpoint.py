09557f8613addd8f1d6aaa1f4426b513652bfd20
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-11-16 11:10:42 -0500

    use get for kwargs

--- core/dbt/events/adapter_endpoint.py
+++ core/dbt/events/adapter_endpoint.py
@@ -3,7 +3,6 @@ from dbt.events.functions import fire_event
 from dbt.events.types import (
     AdapterEventDebug, AdapterEventInfo, AdapterEventWarning, AdapterEventError
 )
-from typing import Any, Optional
 
 
 @dataclass
@@ -13,36 +12,36 @@ class AdapterLogger():
     def debug(self, *args, **kwargs):
         event = AdapterEventDebug(self.name, args, kwargs)
 
-        event.exc_info = or_none(kwargs, 'exc_info')
-        event.stack_info = or_none(kwargs, 'stack_info')
-        event.extra = or_none(kwargs, 'extra')
+        event.exc_info = kwargs.get('exc_info')
+        event.stack_info = kwargs.get('stack_info')
+        event.extra = kwargs.get('extra')
 
         fire_event(event)
 
     def info(self, *args, **kwargs):
         event = AdapterEventInfo(self.name, args, kwargs)
 
-        event.exc_info = or_none(kwargs, 'exc_info')
-        event.stack_info = or_none(kwargs, 'stack_info')
-        event.extra = or_none(kwargs, 'extra')
+        event.exc_info = kwargs.get('exc_info')
+        event.stack_info = kwargs.get('stack_info')
+        event.extra = kwargs.get('extra')
 
         fire_event(event)
 
     def warning(self, *args, **kwargs):
         event = AdapterEventWarning(self.name, args, kwargs)
 
-        event.exc_info = or_none(kwargs, 'exc_info')
-        event.stack_info = or_none(kwargs, 'stack_info')
-        event.extra = or_none(kwargs, 'extra')
+        event.exc_info = kwargs.get('exc_info')
+        event.stack_info = kwargs.get('stack_info')
+        event.extra = kwargs.get('extra')
 
         fire_event(event)
 
     def error(self, *args, **kwargs):
         event = AdapterEventError(self.name, args, kwargs)
 
-        event.exc_info = or_none(kwargs, 'exc_info')
-        event.stack_info = or_none(kwargs, 'stack_info')
-        event.extra = or_none(kwargs, 'extra')
+        event.exc_info = kwargs.get('exc_info')
+        event.stack_info = kwargs.get('stack_info')
+        event.extra = kwargs.get('extra')
 
         fire_event(event)
 
@@ -50,16 +49,9 @@ class AdapterLogger():
         event = AdapterEventError(self.name, args, kwargs)
 
         # defaulting exc_info=True if it is empty is what makes this method different
-        x = or_none(kwargs, 'exc_info')
+        x = kwargs.get('exc_info')
         event.exc_info = x if x else True
-        event.stack_info = or_none(kwargs, 'stack_info')
-        event.extra = or_none(kwargs, 'extra')
+        event.stack_info = kwargs.get('stack_info')
+        event.extra = kwargs.get('extra')
 
         fire_event(event)
-
-
-def or_none(x: dict, key: str) -> Optional[Any]:
-    try:
-        return x[key]
-    except KeyError:
-        return None
