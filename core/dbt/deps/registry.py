e82391d310d8997489bb8ca275fed39a76ebb58d
Joel Labes <joel.labes@dbtlabs.com>
2021-11-18 21:11:49 +1300

    Get prerelease packages when specifically requested (#4295)
    * Get prerelease packages when specifically required. Add test validating it works
    
    * Update CHANGELOG.md
    
    automatic commit by git-black, original commits:
      0aacd99168f3f23cc980d3250c0106406fc85844

--- core/dbt/deps/registry.py
+++ core/dbt/deps/registry.py
@@ -126,9 +126,7 @@ class RegistryUnpinnedPackage(RegistryPackageMixin, UnpinnedPackage[RegistryPinn
             raise DependencyException(new_msg) from e
 
         available = registry.get_available_versions(self.package)
-        prerelease_version_specified = any(
-            bool(version.prerelease) for version in self.versions
-        )
+        prerelease_version_specified = any(bool(version.prerelease) for version in self.versions)
         installable = semver.filter_installable(
             available,
             self.install_prerelease or prerelease_version_specified
