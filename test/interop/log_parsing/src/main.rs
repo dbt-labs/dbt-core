025305025546f7d13192d1a7a7ace805a446133d
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-12-01 12:51:05 -0500

    fmt

--- test/interop/log_parsing/src/main.rs
+++ test/interop/log_parsing/src/main.rs
@@ -1,11 +1,10 @@
-
 // main doesn't do anything. Just run the tests
 fn main() {}
 
 #[cfg(test)]
 mod tests {
     use chrono::NaiveDateTime;
-    use serde::{Serialize, Deserialize};
+    use serde::{Deserialize, Serialize};
 
     #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
     struct LogLine {
@@ -18,8 +17,8 @@ mod tests {
         level: String,
         invocation_id: String,
         thread_name: String,
-        data: serde_json::Value,  // TODO be more specific
-        node_info: serde_json::Value  // TODO be more specific
+        data: serde_json::Value,      // TODO be more specific
+        node_info: serde_json::Value, // TODO be more specific
     }
 
     // logs output timestamps like this: "2021-11-30T12:31:04.312814"
@@ -28,14 +27,11 @@ mod tests {
     // boilerplate-looking module.
     mod custom_date_format {
         use chrono::NaiveDateTime;
-        use serde::{self, Deserialize, Serializer, Deserializer};
+        use serde::{self, Deserialize, Deserializer, Serializer};
 
         const FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S%.6f";
 
-        pub fn serialize<S>(
-            date: &NaiveDateTime,
-            serializer: S,
-        ) -> Result<S::Ok, S::Error>
+        pub fn serialize<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
         where
             S: Serializer,
         {
@@ -43,9 +39,7 @@ mod tests {
             serializer.serialize_str(&s)
         }
 
-        pub fn deserialize<'de, D>(
-            deserializer: D,
-        ) -> Result<NaiveDateTime, D::Error>
+        pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
         where
             D: Deserializer<'de>,
         {
@@ -61,30 +55,33 @@ mod tests {
     }
 
     fn deserialized_input(log_lines: &[String]) -> serde_json::Result<Vec<LogLine>> {
-        log_lines.into_iter().map(|log_line| {
-            serde_json::from_str::<LogLine>(log_line)
-        }).collect()
+        log_lines
+            .into_iter()
+            .map(|log_line| serde_json::from_str::<LogLine>(log_line))
+            .collect()
     }
-    
-    fn deserialize_serialize_loop(log_lines: &[String]) -> serde_json::Result<Vec<(String, String)>> {
-        log_lines.into_iter().map(|log_line| {
-            serde_json::from_str::<LogLine>(log_line)
-                .and_then(|parsed| {
-                    serde_json::to_string(&parsed)
-                        .map(|json| (log_line.clone(), json))
+
+    fn deserialize_serialize_loop(
+        log_lines: &[String],
+    ) -> serde_json::Result<Vec<(String, String)>> {
+        log_lines
+            .into_iter()
+            .map(|log_line| {
+                serde_json::from_str::<LogLine>(log_line).and_then(|parsed| {
+                    serde_json::to_string(&parsed).map(|json| (log_line.clone(), json))
                 })
-        }).collect()
+            })
+            .collect()
     }
 
     // If this test breaks we have made a change that could break downstream consumers' apps
     // Either revert the change in dbt, or bump the log_version and add new docs to communicate the change
     #[test]
     fn test_expected_values() {
-        let log_lines = deserialized_input(&get_input())
-            .unwrap_or_else(|_| {
-                assert!(false, "input failed to deserialize");
-                vec![] // unreachable stub
-            });
+        let log_lines = deserialized_input(&get_input()).unwrap_or_else(|_| {
+            assert!(false, "input failed to deserialize");
+            vec![] // unreachable stub
+        });
 
         for log_line in log_lines {
             assert_eq!(
@@ -93,13 +90,17 @@ mod tests {
             );
 
             assert_eq!(
-                log_line.r#type, "log_line".to_owned(),
+                log_line.r#type,
+                "log_line".to_owned(),
                 "The type value has changed. If this is intentional, bump the log version"
             );
 
             assert!(
-                ["debug", "info", "warn", "error"].iter().any(|level| **level == log_line.level),
-                "log level had unexpected value {}", log_line.level
+                ["debug", "info", "warn", "error"]
+                    .iter()
+                    .any(|level| **level == log_line.level),
+                "log level had unexpected value {}",
+                log_line.level
             );
         }
     }
@@ -109,16 +110,15 @@ mod tests {
     // and add new docs to communicate the change
     #[test]
     fn deserialize_serialize_is_unchanged() {
-        let objects: Result<Vec<(serde_json::Value, serde_json::Value)>, serde_json::Error> = deserialize_serialize_loop(&get_input())
-            .and_then(|v| {
-                v.into_iter().map(|(s0, s1)| {
-                    serde_json::from_str::<serde_json::Value>(&s0)
-                        .and_then(|s0v| {
-                            serde_json::from_str::<serde_json::Value>(&s1)
-                                .map(|s1v| (s0v, s1v))
+        let objects: Result<Vec<(serde_json::Value, serde_json::Value)>, serde_json::Error> =
+            deserialize_serialize_loop(&get_input()).and_then(|v| {
+                v.into_iter()
+                    .map(|(s0, s1)| {
+                        serde_json::from_str::<serde_json::Value>(&s0).and_then(|s0v| {
+                            serde_json::from_str::<serde_json::Value>(&s1).map(|s1v| (s0v, s1v))
                         })
-                }).collect()
-                
+                    })
+                    .collect()
             });
 
         match objects {
@@ -126,22 +126,37 @@ mod tests {
             Ok(v) => {
                 for pair in v {
                     match pair {
-                        (serde_json::Value::Object(original), serde_json::Value::Object(looped)) => {
+                        (
+                            serde_json::Value::Object(original),
+                            serde_json::Value::Object(looped),
+                        ) => {
                             // looping through each key of each json value gives us meaningful failure messages
                             // instead of "this big string" != "this other big string"
                             for (key, value) in original.clone() {
                                 let looped_val = looped.get(&key);
-                                assert_eq!(looped_val, Some(&value), "original key value ({}, {}) expected in re-serialized result", key, value)
+                                assert_eq!(
+                                    looped_val,
+                                    Some(&value),
+                                    "original key value ({}, {}) expected in re-serialized result",
+                                    key,
+                                    value
+                                )
                             }
                             for (key, value) in looped.clone() {
                                 let original_val = original.get(&key);
-                                assert_eq!(original_val, Some(&value), "looped key value ({}, {}) not found in original result", key, value)
+                                assert_eq!(
+                                    original_val,
+                                    Some(&value),
+                                    "looped key value ({}, {}) not found in original result",
+                                    key,
+                                    value
+                                )
                             }
-                        },
-                        _ => assert!(false, "not comparing json objects")
+                        }
+                        _ => assert!(false, "not comparing json objects"),
                     }
                 }
-            },
+            }
         }
     }
 }
