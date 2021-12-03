f67cb2fbfba599c614e6d14e3947392000189131
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-12-02 20:00:17 -0500

    use 6 decimal places for fractional seconds

--- test/interop/log_parsing/src/main.rs
+++ test/interop/log_parsing/src/main.rs
@@ -55,6 +55,33 @@ mod tests {
         }
     }
 
+    // logs output timestamps like this: "2021-11-30T12:31:04.312814Z"
+    // which is so close to the default except for the decimal.
+    // this requires handling the date with "%Y-%m-%dT%H:%M:%S%.6f" which requires this
+    // boilerplate-looking module.
+    mod custom_date_format {
+        use chrono::NaiveDateTime;
+        use serde::{self, Deserialize, Deserializer, Serializer};
+
+        const FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S%.6fZ";
+
+        pub fn serialize<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let s = format!("{}", date.format(FORMAT));
+            serializer.serialize_str(&s)
+        }
+
+        pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let s = String::deserialize(deserializer)?;
+            NaiveDateTime::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+        }
+    }
+
     fn get_input() -> Vec<String> {
         let log_name = "dbt.log";
 
