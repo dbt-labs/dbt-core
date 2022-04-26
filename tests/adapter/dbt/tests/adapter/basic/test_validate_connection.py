36a1db69e5c1364eadb0e06b03fb597496cb7fd8
Chenyu Li <chenyu.li@dbtlabs.com>
2022-04-26 15:44:20 -0600

    add project artifact to avoid breaking other tests

--- tests/adapter/dbt/tests/adapter/basic/test_validate_connection.py
+++ tests/adapter/dbt/tests/adapter/basic/test_validate_connection.py
@@ -2,7 +2,7 @@ import dbt.task.debug
 
 
 class BaseValidateConnection:
-    def test_validate_connection(self, dbt_profile_data):
+    def test_validate_connection(self, project, dbt_profile_data):
 
         dbt.task.debug.DebugTask.validate_connection(
             dbt_profile_data["test"]["outputs"]["default"]
