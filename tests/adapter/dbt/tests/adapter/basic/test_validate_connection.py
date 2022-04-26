7d4655f99779b58a0478f1389a3c0aeae06cc437
Chenyu Li <chenyu.li@dbtlabs.com>
2022-04-26 15:44:59 -0600

    add comment

--- tests/adapter/dbt/tests/adapter/basic/test_validate_connection.py
+++ tests/adapter/dbt/tests/adapter/basic/test_validate_connection.py
@@ -2,6 +2,7 @@ import dbt.task.debug
 
 
 class BaseValidateConnection:
+    # project need to be here otherwise some other tests might break
     def test_validate_connection(self, project, dbt_profile_data):
 
         dbt.task.debug.DebugTask.validate_connection(
