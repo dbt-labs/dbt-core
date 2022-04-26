f68c5a8c2af57708bce907ab3f43b4dc7ef754ae
Mila Page <versusfacit@users.noreply.github.com>
2022-04-26 14:24:09 -0700

    Refactor test code to better use our pytest framework. Strengthen assertions.

--- tests/functional/custom_singular_tests/test_custom_singular_tests.py
+++ tests/functional/custom_singular_tests/test_custom_singular_tests.py
@@ -2,14 +2,13 @@ import pytest
 
 from pathlib import Path
 
-from dbt.config import RuntimeConfig
-from dbt.task.test import TestTask
-
-from dbt.tests.util import (
-    run_dbt,
-)
+from dbt.tests.util import run_dbt
 
 # from `test/integration/009_data_test`
+
+#
+# Models
+#
 
 models__table_copy = """
 {{
@@ -20,6 +19,10 @@ models__table_copy = """
 
 select * from {{ this.schema }}.seed
 """
+
+#
+# Tests
+#
 
 tests__fail_email_is_always_null = """
 select *
@@ -50,81 +53,53 @@ select 1 limit 0
 """
 
 
-class FakeArgs:
-    def __init__(self):
-        self.threads = 1
-        self.defer = False
-        self.full_refresh = False
-        self.models = None
-        self.select = None
-        self.exclude = None
-        self.single_threaded = False
-        self.selector_name = None
-        self.state = None
-        self.defer = None
-
-
-class ArgsForTest:
-    def __init__(self, kwargs):
-        self.which = "run"
-        self.single_threaded = False
-        self.profiles_dir = None
-        self.project_dir = None
-        self.__dict__.update(kwargs)
-
-
-class TestCustomGenericTests(object):
+class CustomSingularTestsBase(object):
     @pytest.fixture(scope="class", autouse=True)
     def setUp(self, project):
-        """Create table for ensuring seeds and models used in tests build correctly"""
+        """Create seed and downstream model tests are to be run on"""
         project.run_sql_file(project.test_data_dir / Path("seed_expected.sql"))
+
+        results = run_dbt()
+        assert len(results) == 1
 
     @pytest.fixture(scope="class")
     def models(self):
-        return {
-            "table_copy.sql": models__table_copy,
-        }
+        return {"table_copy.sql": models__table_copy}
 
+
+class TestPassingTests(CustomSingularTestsBase):
     @pytest.fixture(scope="class")
     def tests(self):
         return {
             "my_db.my_schema.table_copy.pass_id_not_null.sql": tests__dotted_path_pass_id_not_null,
             "tests__pass_id_not_null.sql": tests__pass_id_not_null,
             "tests__pass_no_ref.sql": tests__pass_no_ref,
+        }
+
+    def test_data_tests(self, project, tests):
+        test_results = run_dbt(["test"])
+        assert len(test_results) == len(tests)
+
+        for result in test_results:
+            assert result.status == "pass"
+            assert not result.skipped
+            assert result.failures == 0
+
+
+class TestFailingTests(CustomSingularTestsBase):
+    @pytest.fixture(scope="class")
+    def tests(self):
+        return {
             "tests__fail_email_is_always_null.sql": tests__fail_email_is_always_null,
             "tests__fail_no_ref.sql": tests__fail_no_ref,
         }
 
-    def run_data_validations(self, profiles_dir):
-        args = FakeArgs()
-
-        kwargs = {
-            "profile": None,
-            "profiles_dir": profiles_dir,
-            "target": None,
-        }
-
-        config = RuntimeConfig.from_args(ArgsForTest(kwargs))
-        test_task = TestTask(args, config)
-        return test_task.run()
-
-    def test_data_tests(self, project, tests, test_config):
-        results = run_dbt()
-        assert len(results) == 1
-        test_results = self.run_data_validations(project.profiles_dir)
+    def test_data_tests(self, project, tests):
+        """assert that all deliberately failing tests actually fail"""
+        test_results = run_dbt(["test"], expect_pass=False)
+        assert len(test_results) == len(tests)
 
         for result in test_results:
-            # assert that all deliberately failing tests actually fail
-            if "fail" in result.node.name:
-                assert result.status == "fail"
-                assert not result.skipped
-                assert result.failures > 0
-            # assert that actual tests pass
-            else:
-                assert result.status == "pass"
-                assert not result.skipped
-                assert result.failures == 0
-
-        # check that all tests were run
-        assert len(test_results) != 0
-        assert len(test_results) == len(tests)
+            assert result.status == "fail"
+            assert not result.skipped
+            assert result.failures > 0
