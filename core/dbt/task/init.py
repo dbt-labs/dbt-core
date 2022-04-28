b9d10e97ad06e510fa266f7db99fad8b50f8a6cb
Alex Rosenfeld <arosenfeld@seismic.com>
2022-04-28 07:09:18 -0400

    quoting consistency

--- core/dbt/task/init.py
+++ core/dbt/task/init.py
@@ -186,7 +186,7 @@ class InitTask(BaseTask):
         initial_target = profile_template.get("fixed", {})
         prompts = profile_template.get("prompts", {})
         target = self.generate_target_from_input(prompts, initial_target)
-        target_name = target.pop('target', "dev")
+        target_name = target.pop("target", "dev")
         profile = {"outputs": {target_name: target}, "target": target_name}
         self.write_profile(profile, profile_name)
 
