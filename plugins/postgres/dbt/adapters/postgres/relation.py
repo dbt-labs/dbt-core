0b09596b47eebc8a5f8ab5b40b9c63ede1f7b4fb
Leon Stefani <leon@brigad.co>
2022-05-23 10:35:15 +0200

    fix: correctly name varchar

--- plugins/postgres/dbt/adapters/postgres/relation.py
+++ plugins/postgres/dbt/adapters/postgres/relation.py
@@ -27,6 +27,8 @@ class PostgresColumn(Column):
     @property
     def data_type(self):
         # on postgres, do not convert 'text' or 'varchar' to 'varchar()'
-        if self.dtype.lower() == "text" or (self.dtype.lower() == "varchar" and self.char_size is None):
+        if self.dtype.lower() == "text" or (
+            self.dtype.lower() == "character varying" and self.char_size is None
+        ):
             return self.dtype
         return super().data_type
