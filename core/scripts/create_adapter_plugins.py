3790fe58bd79ac0a638c71bc821d33ce51e4d20c
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-06-16 11:11:31 -0500

    readding the original script file but changing its output ot be a print statement and leave comment that also points to the new scaffold

---
+++ core/scripts/create_adapter_plugins.py
@@ -0,0 +1,5 @@
+# The create_adapter_plugins script is being replaced by a new interactive cookiecutter scaffold
+# that can be found https://github.com/dbt-labs/dbt-database-adapter-scaffold
+print(
+    "To create a new adapter please visit https://github.com/dbt-labs/dbt-database-adapter-scaffold"
+)
