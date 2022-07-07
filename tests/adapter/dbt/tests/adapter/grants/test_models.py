ad8855614a13803c3b6e1f785400f4969780c400
Gerda Shank <gerda@dbtlabs.com>
2022-07-07 08:15:29 -0400

    use run_dbt_and_capture, change to assert for test_users

--- tests/adapter/dbt/tests/adapter/grants/test_models.py
+++ tests/adapter/dbt/tests/adapter/grants/test_models.py
@@ -2,6 +2,7 @@ import pytest
 import os
 from dbt.tests.util import (
     run_dbt,
+    run_dbt_and_capture,
     get_manifest,
     read_file,
     relation_from_name,
@@ -41,42 +42,38 @@ class TestModelGrants:
     @pytest.fixture(scope="class", autouse=True)
     def get_test_users(self, project):
         test_users = []
-        missing = []
         for env_var in TEST_USER_ENV_VARS:
             user_name = os.getenv(env_var)
-            if not user_name:
-                missing.append(env_var)
-            else:
+            if user_name:
                 test_users.append(user_name)
-        if missing:
-            pytest.skip(f"Test requires env vars with test users. Missing {', '.join(missing)}.")
         return test_users
 
     def test_basic(self, project, get_test_users, logs_dir):
+        # we want the test to fail, not silently skip
+        test_users = get_test_users
+        assert len(test_users) == 3
+
         # Tests a project with a single model, view materialization
-        results = run_dbt(["run"])
+        (results, log_output) = run_dbt_and_capture(["--debug", "run"])
         assert len(results) == 1
         manifest = get_manifest(project.project_root)
         model_id = "model.test.my_model"
         model = manifest.nodes[model_id]
-        expected = {"select": ["dbt_test_user_1"]}
+        expected = {"select": [test_users[0]]}
         assert model.config.grants == expected
         assert model.config.materialized == "view"
 
-        log_contents = read_file(logs_dir, "dbt.log")
         my_model_relation = relation_from_name(project.adapter, "my_model")
-        grant_log_line = f"grant select on table {my_model_relation} to dbt_test_user_1;"
-        assert grant_log_line in log_contents
+        grant_log_line = f"grant select on table {my_model_relation} to {test_users[0]};"
+        assert grant_log_line in log_output
 
         # Switch to a different user, still view materialization
-        rm_file(logs_dir, "dbt.log")
         write_file(user2_model_schema_yml, project.project_root, "models", "schema.yml")
-        results = run_dbt(["run"])
+        (results, log_output) = run_dbt_and_capture(["--debug", "run"])
         assert len(results) == 1
-        log_contents = read_file(logs_dir, "dbt.log")
-        print(log_contents)
-        grant_log_line = f"grant select on table {my_model_relation} to dbt_test_user_2;"
-        assert grant_log_line in log_contents
-        # Note: We are not revoking grants here, so there is no revoke in the log
+        log_output = read_file(logs_dir, "dbt.log")
+        grant_log_line = f"grant select on table {my_model_relation} to {test_users[1]};"
+        assert grant_log_line in log_output
+        assert "revoke select" not in log_output
 
 
