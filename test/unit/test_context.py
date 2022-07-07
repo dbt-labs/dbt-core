65cb03fd9af2e8c6cf4d793056e534ef68b50be8
Chenyu Li <chenyu.li@dbtlabs.com>
2022-07-07 15:06:41 -0700

    fix all tests

--- test/unit/test_context.py
+++ test/unit/test_context.py
@@ -238,7 +238,7 @@ REQUIRED_MACRO_KEYS = REQUIRED_QUERY_HEADER_KEYS | {
     "adapter_macro",
     "selected_resources",
 }
-REQUIRED_MODEL_KEYS = REQUIRED_MACRO_KEYS | {"this"}
+REQUIRED_MODEL_KEYS = REQUIRED_MACRO_KEYS | {"this", "compiled_code",}
 MAYBE_KEYS = frozenset({"debug"})
 
 
