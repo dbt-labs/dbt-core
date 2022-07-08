89eabc320d8db0836071d67656bab93cc2012789
Jeremy Cohen <jeremy@dbtlabs.com>
2022-07-08 18:36:10 +0200

    Refactor macros to return lists, fix test

--- core/dbt/include/global_project/macros/adapters/apply_grants.sql
+++ core/dbt/include/global_project/macros/adapters/apply_grants.sql
@@ -34,12 +34,17 @@
 {% endmacro %}
 
 {%- macro default__get_grant_sql(relation, grant_config) -%}
+    {%- set grant_statements = [] -%}
     {%- for privilege in grant_config.keys() %}
         {%- set grantees = grant_config[privilege] -%}
         {%- if grantees %}
-            grant {{ privilege }} on {{ relation }} to {{ grantees | join(', ') }};
+            {% set grant_sql -%}
+                grant {{ privilege }} on {{ relation }} to {{ grantees | join(', ') }}
+            {%- endset %}
+            {%- do grant_statements.append(grant_sql) -%}
         {% endif -%}
     {%- endfor -%}
+    {{ return(grant_statements) }}
 {%- endmacro %}
 
 {% macro get_revoke_sql(relation, grant_config) %}
@@ -47,13 +52,32 @@
 {% endmacro %}
 
 {% macro default__get_revoke_sql(relation, grant_config) %}
+    {%- set revoke_statements = [] -%}
     {%- for privilege in grant_config.keys() -%}
         {%- set grantees = grant_config[privilege] -%}
         {%- if grantees %}
-            revoke {{ privilege }} on {{ relation }} from {{ grantees | join(', ') }};
+            {% set revoke_sql -%}
+                revoke {{ privilege }} on {{ relation }} from {{ grantees | join(', ') }}
+            {%- endset %}
+            {%- do revoke_statements.append(revoke_sql) -%}
         {% endif -%}
     {%- endfor -%}
+    {{ return(revoke_statements) }}
 {%- endmacro -%}
+
+
+{% macro call_grant_revoke_statement_list(grant_and_revoke_statement_list) %}
+    {{ return(adapter.dispatch("call_grant_revoke_statement_list", "dbt")(grant_and_revoke_statement_list)) }}
+{% endmacro %}
+
+{% macro default__call_grant_revoke_statement_list(grant_and_revoke_statement_list) %}
+    {% call statement('grants') %}
+        {% for grant_or_revoke_statement in grant_and_revoke_statement_list %}
+            {{ grant_or_revoke_statement }};
+        {% endfor %}
+    {% endcall %}
+{% endmacro %}
+
 
 {% macro apply_grants(relation, grant_config, should_revoke) %}
     {{ return(adapter.dispatch("apply_grants", "dbt")(relation, grant_config, should_revoke)) }}
@@ -61,23 +85,25 @@
 
 {% macro default__apply_grants(relation, grant_config, should_revoke=True) %}
     {% if grant_config %}
-            {% if should_revoke %}
-                {% set current_grants_table = run_query(get_show_grant_sql(relation)) %}
-                {% set current_grants_dict = adapter.standardize_grants_dict(current_grants_table) %}
-                {% set needs_granting = diff_of_two_dicts(grant_config, current_grants_dict) %}
-                {% set needs_revoking = diff_of_two_dicts(current_grants_dict, grant_config) %}
-                {% if not (needs_granting or needs_revoking) %}
-                    {{ log('On ' ~ relation ~': All grants are in place, no revocation or granting needed.')}}
-                {% endif %}
-            {% else %}
-                {% set needs_revoking = {} %}
-                {% set needs_granting = grant_config %}
+        {% if should_revoke %}
+            {% set current_grants_table = run_query(get_show_grant_sql(relation)) %}
+            {% set current_grants_dict = adapter.standardize_grants_dict(current_grants_table) %}
+            {% set needs_granting = diff_of_two_dicts(grant_config, current_grants_dict) %}
+            {% set needs_revoking = diff_of_two_dicts(current_grants_dict, grant_config) %}
+            {% if not (needs_granting or needs_revoking) %}
+                {{ log('On ' ~ relation ~': All grants are in place, no revocation or granting needed.')}}
             {% endif %}
-            {% if needs_granting or needs_revoking %}
-                {% call statement('grants') %}
-                    {{ get_revoke_sql(relation, needs_revoking) }}
-                    {{ get_grant_sql(relation, needs_granting) }}
-                {% endcall %}
+        {% else %}
+            {% set needs_revoking = {} %}
+            {% set needs_granting = grant_config %}
+        {% endif %}
+        {% if needs_granting or needs_revoking %}
+            {% set revoke_statement_list = get_revoke_sql(relation, needs_revoking) %}
+            {% set grant_statement_list = get_grant_sql(relation, needs_granting) %}
+            {% set grant_and_revoke_statement_list = revoke_statement_list + grant_statement_list %}
+            {% if grant_and_revoke_statement_list %}
+                {{ call_grant_revoke_statement_list(grant_and_revoke_statement_list) }}
             {% endif %}
+        {% endif %}
     {% endif %}
 {% endmacro %}
