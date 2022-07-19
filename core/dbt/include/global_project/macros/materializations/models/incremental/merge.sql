ee002a961526068441c33596078d721cf086eefc
Gerda Shank <gerda@dbtlabs.com>
2022-07-19 12:25:05 -0400

    Uncomment sql_header and use config to get include_sql_header

--- core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
@@ -26,7 +26,7 @@
         {% do predicates.append('FALSE') %}
     {% endif %}
 
-    {# sql_header if sql_header is not none #}
+    {{ sql_header if sql_header is not none }}
 
     merge into {{ target }} as DBT_INTERNAL_DEST
         using {{ source }} as DBT_INTERNAL_SOURCE
@@ -94,8 +94,10 @@
     {%- set predicates = [] if predicates is none else [] + predicates -%}
     {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
     {%- set sql_header = config.get('sql_header', none) -%}
+    {# The following is only true in BigQuery #}
+    {%- set include_sql_header = config.get('include_sql_header', false) -%}
 
-    {{ sql_header if sql_header is not none }}
+    {{ sql_header if sql_header is not none and include_sql_header }}
 
     merge into {{ target }} as DBT_INTERNAL_DEST
         using {{ source }} as DBT_INTERNAL_SOURCE
