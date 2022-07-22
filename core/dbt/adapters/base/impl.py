168740bba9bb8d55fb89b5e5183d37b4c7423470
Chenyu Li <chenyu.li@dbtlabs.com>
2022-07-21 23:03:42 -0700

    add a decorator to log code execution

--- core/dbt/adapters/base/impl.py
+++ core/dbt/adapters/base/impl.py
@@ -2,6 +2,7 @@ import abc
 from concurrent.futures import as_completed, Future
 from contextlib import contextmanager
 from datetime import datetime
+import time
 from itertools import chain
 from typing import (
     Optional,
@@ -42,7 +43,7 @@ from dbt.contracts.graph.manifest import Manifest, MacroManifest
 from dbt.contracts.graph.parsed import ParsedSeedNode
 from dbt.exceptions import warn_or_error
 from dbt.events.functions import fire_event
-from dbt.events.types import CacheMiss, ListRelations
+from dbt.events.types import CacheMiss, ListRelations, CodeExecution, CodeExecutionStatus
 from dbt.utils import filter_null_values, executor
 
 from dbt.adapters.base.connections import Connection, AdapterResponse
@@ -1193,3 +1194,24 @@ def catch_as_completed(
             # exc is not None, derives from Exception, and isn't ctrl+c
             exceptions.append(exc)
     return merge_tables(tables), exceptions
+
+
+def log_code_execution(code_execution_function):
+    # decorator to log code and execution time
+    if code_execution_function.__name__ != "submit_python_job":
+        raise ValueError("this should be only used to log submit_python_job now")
+
+    def execution_with_log(*args):
+        self = args[0]
+        connection_name = self.connections.get_thread_connection().name
+        fire_event(CodeExecution(conn_name=connection_name, code_content=args[2]))
+        start_time = time.time()
+        response = code_execution_function(*args)
+        fire_event(
+            CodeExecutionStatus(
+                status=response._message, elapsed=round((time.time() - start_time), 2)
+            )
+        )
+        return response
+
+    return execution_with_log
