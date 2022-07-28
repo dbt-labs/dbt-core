0525b88ae75d4db99a0b39c7fdb95a84295216cc
Stu Kilgore <stuart.kilgore@gmail.com>
2022-07-28 09:42:28 -0500

    Add python incremental materialization test

--- tests/adapter/dbt/tests/adapter/python_model/test_python_model.py
+++ tests/adapter/dbt/tests/adapter/python_model/test_python_model.py
@@ -38,3 +38,66 @@ class BasePythonModelTests:
         # test command
         results = run_dbt(["run"])
         assert len(results) == 3
+
+
+m_1 = """
+{{config(materialized='table')}}
+select 1 as id union all
+select 2 as id union all
+select 3 as id union all
+select 4 as id union all
+select 5 as id
+"""
+
+incremental_python = """
+def model(dbt, session):
+    dbt.config(materialized="incremental", unique_key='id')
+    df = dbt.ref("m_1")
+    if dbt.is_incremental:
+        # incremental runs should only apply to
+        df = df.filter(df.id >= 5)
+    return df
+"""
+
+
+class BasePythonIncrementalTests:
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"models": {"+incremental_strategy": "merge"}}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"m_1.sql": m_1, "incremental.py": incremental_python}
+
+    def test_incremental(self, project):
+        # create m_1 and run incremental model the first time
+        run_dbt(["run"])
+        assert (
+            project.run_sql(
+                f"select count(*) from {project.database}.{project.test_schema}.incremental",
+                fetch="one",
+            )[0]
+            == 5
+        )
+        # running incremental model again will not cause any changes in the result model
+        run_dbt(["run", "-s", "incremental"])
+        assert (
+            project.run_sql(
+                f"select count(*) from {project.database}.{project.test_schema}.incremental",
+                fetch="one",
+            )[0]
+            == 5
+        )
+        # add 3 records with one supposed to be filtered out
+        project.run_sql(
+            f"insert into {project.database}.{project.test_schema}.m_1(id) values (0), (6), (7)"
+        )
+        # validate that incremental model would correctly add 2 valid records to result model
+        run_dbt(["run", "-s", "incremental"])
+        assert (
+            project.run_sql(
+                f"select count(*) from {project.database}.{project.test_schema}.incremental",
+                fetch="one",
+            )[0]
+            == 7
+        )
