bfbf4972b951a5589ae77b356b898e0727fbd713
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-13 23:49:23 -0500

    attempt to rewrite file for test in snowflake

--- tests/adapter/dbt/tests/adapter/concurrency/test_concurrency.py
+++ tests/adapter/dbt/tests/adapter/concurrency/test_concurrency.py
@@ -1,9 +1,11 @@
 import pytest
 from dbt.tests.util import (
-    run_dbt,
     check_relations_equal,
     check_table_does_not_exist,
+    rm_file,
+    run_dbt,
     run_dbt_and_capture,
+    write_file,
 )
 
 
@@ -309,7 +311,6 @@ class TestConcurenncy(BaseConcurrency):
         run_dbt(["seed", "--select", "seed"])
         results = run_dbt(["run"], expect_pass=False)
         assert len(results) == 7
-
         check_relations_equal(project.adapter, ["seed", "view_model"])
         check_relations_equal(project.adapter, ["seed", "dep"])
         check_relations_equal(project.adapter, ["seed", "table_a"])
@@ -317,11 +318,11 @@ class TestConcurenncy(BaseConcurrency):
         check_table_does_not_exist(project.adapter, "invalid")
         check_table_does_not_exist(project.adapter, "skip")
 
-        run_dbt(["seed", "--select", "update"])
+        rm_file(project.project_root, "seeds", "seed.csv")
+        write_file(seeds__update_csv, project.project_root, "seeds", "seed.csv")
 
         results, output = run_dbt_and_capture(["run"], expect_pass=False)
         assert len(results) == 7
-
         check_relations_equal(project.adapter, ["seed", "view_model"])
         check_relations_equal(project.adapter, ["seed", "dep"])
         check_relations_equal(project.adapter, ["seed", "table_a"])
