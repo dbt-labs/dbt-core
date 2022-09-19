cdf70810314df645e041c40e4f8547b66df10626
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-19 15:36:22 -0500

    init pr for 026 test conversion

---
+++ tests/functional/aliases/test_aliases.py
@@ -0,0 +1,271 @@
+import pytest
+from dbt.tests.fixtures.project import write_project_files
+
+
+models_dupe_custom_database__schema_yml = """
+version: 2
+models:
+- name: model_a
+  tests:
+  - expect_value:
+      field: tablename
+      value: duped_alias
+- name: model_b
+  tests:
+  - expect_value:
+      field: tablename
+      value: duped_alias
+
+"""
+
+models_dupe_custom_database__model_b_sql = """
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models_dupe_custom_database__model_a_sql = """
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models_dupe_custom_database__README_md = """
+these should succeed, as both models have the same alias,
+but they are configured to be built in _different_ schemas
+
+"""
+
+models_dupe__model_b_sql = """
+
+{{ config(alias='duped_alias') }}
+
+select 1 as id
+
+"""
+
+models_dupe__model_a_sql = """
+
+{{ config(alias='duped_alias') }}
+
+select 1 as id
+
+"""
+
+models_dupe__README_md = """
+these should fail because both models have the same alias
+and are configured to build in the same schema
+
+"""
+
+models__schema_yml = """
+version: 2
+models:
+- name: foo_alias
+  tests:
+  - expect_value:
+      field: tablename
+      value: foo
+- name: ref_foo_alias
+  tests:
+  - expect_value:
+      field: tablename
+      value: ref_foo_alias
+- name: alias_in_project
+  tests:
+  - expect_value:
+      field: tablename
+      value: project_alias
+- name: alias_in_project_with_override
+  tests:
+  - expect_value:
+      field: tablename
+      value: override_alias
+
+"""
+
+models__foo_alias_sql = """
+
+{{
+    config(
+        alias='foo',
+        materialized='table'
+    )
+}}
+
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models__alias_in_project_sql = """
+
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models__alias_in_project_with_override_sql = """
+
+{{ config(alias='override_alias') }}
+
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models__ref_foo_alias_sql = """
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with trigger_ref as (
+
+  -- we should still be able to ref a model by its filepath
+  select * from {{ ref('foo_alias') }}
+
+)
+
+-- this name should still be the filename
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+macros__cast_sql = """
+
+
+{% macro string_literal(s) -%}
+  {{ adapter.dispatch('string_literal', macro_namespace='test')(s) }}
+{%- endmacro %}
+
+{% macro default__string_literal(s) %}
+    '{{ s }}'::text
+{% endmacro %}
+
+"""
+
+macros__expect_value_sql = """
+
+-- cross-db compatible test, similar to accepted_values
+
+{% test expect_value(model, field, value) %}
+
+select *
+from {{ model }}
+where {{ field }} != '{{ value }}'
+
+{% endtest %}
+
+"""
+
+models_dupe_custom_schema__schema_yml = """
+version: 2
+models:
+- name: model_a
+  tests:
+  - expect_value:
+      field: tablename
+      value: duped_alias
+- name: model_b
+  tests:
+  - expect_value:
+      field: tablename
+      value: duped_alias
+- name: model_c
+  tests:
+  - expect_value:
+      field: tablename
+      value: duped_alias
+
+"""
+
+models_dupe_custom_schema__model_c_sql = """
+
+-- no custom schema for this model
+{{ config(alias='duped_alias') }}
+
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models_dupe_custom_schema__model_b_sql = """
+
+{{ config(alias='duped_alias', schema='schema_b') }}
+
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models_dupe_custom_schema__model_a_sql = """
+
+{{ config(alias='duped_alias', schema='schema_a') }}
+
+select {{ string_literal(this.name) }} as tablename
+
+"""
+
+models_dupe_custom_schema__README_md = """
+these should succeed, as both models have the same alias,
+but they are configured to be built in _different_ schemas
+
+"""
+
+
+@pytest.fixture(scope="class")
+def models_dupe_custom_database():
+    return {
+        "schema.yml": models_dupe_custom_database__schema_yml,
+        "model_b.sql": models_dupe_custom_database__model_b_sql,
+        "model_a.sql": models_dupe_custom_database__model_a_sql,
+        "README.md": models_dupe_custom_database__README_md,
+    }
+
+
+@pytest.fixture(scope="class")
+def models_dupe():
+    return {
+        "model_b.sql": models_dupe__model_b_sql,
+        "model_a.sql": models_dupe__model_a_sql,
+        "README.md": models_dupe__README_md,
+    }
+
+
+@pytest.fixture(scope="class")
+def models():
+    return {
+        "schema.yml": models__schema_yml,
+        "foo_alias.sql": models__foo_alias_sql,
+        "alias_in_project.sql": models__alias_in_project_sql,
+        "alias_in_project_with_override.sql": models__alias_in_project_with_override_sql,
+        "ref_foo_alias.sql": models__ref_foo_alias_sql,
+    }
+
+
+@pytest.fixture(scope="class")
+def macros():
+    return {"cast.sql": macros__cast_sql, "expect_value.sql": macros__expect_value_sql}
+
+
+@pytest.fixture(scope="class")
+def models_dupe_custom_schema():
+    return {
+        "schema.yml": models_dupe_custom_schema__schema_yml,
+        "model_c.sql": models_dupe_custom_schema__model_c_sql,
+        "model_b.sql": models_dupe_custom_schema__model_b_sql,
+        "model_a.sql": models_dupe_custom_schema__model_a_sql,
+        "README.md": models_dupe_custom_schema__README_md,
+    }
+
+
+@pytest.fixture(scope="class")
+def project_files(
+    project_root,
+    models_dupe_custom_database,
+    models_dupe,
+    models,
+    macros,
+    models_dupe_custom_schema,
+):
+    write_project_files(project_root, "models-dupe-custom-database", models_dupe_custom_database)
+    write_project_files(project_root, "models-dupe", models_dupe)
+    write_project_files(project_root, "models", models)
+    write_project_files(project_root, "macros", macros)
+    write_project_files(project_root, "models-dupe-custom-schema", models_dupe_custom_schema)
