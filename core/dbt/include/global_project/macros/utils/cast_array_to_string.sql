549c49fa4b29412ad43ef43608eeb271c0f872aa
Doug Beatty <doug.beatty@dbtlabs.com>
2022-09-21 15:58:39 -0600

    Remove `cast_array_to_string` macro

--- core/dbt/include/global_project/macros/utils/cast_array_to_string.sql
+++
@@ -1,7 +0,0 @@
-{% macro cast_array_to_string(array) %}
-  {{ adapter.dispatch('cast_array_to_string', 'dbt') (array) }}
-{% endmacro %}
-
-{% macro default__cast_array_to_string(array) %}
-    cast({{ array }} as {{ type_string() }})
-{% endmacro %}
--- plugins/postgres/dbt/include/postgres/macros/utils/cast_array_to_string.sql
+++
@@ -1,5 +0,0 @@
-{# when casting as array to string, postgres uses {} (ex: {1,2,3}) while other dbs use [] (ex: [1,2,3]) #}
-{% macro postgres__cast_array_to_string(array) %}
-    {%- set array_as_string -%}cast({{ array }} as {{ type_string() }}){%- endset -%}
-    {{ replace(replace(array_as_string,"'}'","']'"),"'{'","'['") }}
-{% endmacro %}
