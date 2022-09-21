2148abf88c15e937aad8b5e6914fc947182a9e7d
Doug Beatty <doug.beatty@dbtlabs.com>
2022-09-21 17:20:03 -0600

    Trivial direct test; array_concat/append test non-triviallly indirectly

--- tests/adapter/dbt/tests/adapter/utils/fixture_array_construct.py
+++ tests/adapter/dbt/tests/adapter/utils/fixture_array_construct.py
@@ -1,8 +1,8 @@
 # array_construct
 
 models__array_construct_expected_sql = """
-select 1 as id, array[1,2,3] as array_col union all
-select 2 as id, array[]::integer[] as array_col
+select 1 as id, {{ array_construct([1,2,3]) }} as array_col union all
+select 2 as id, {{ array_construct([]) }} as array_col
 """.lstrip()
 
 
