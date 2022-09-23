fb5e6ed6e842e2f8cdbe387fe86dd819a7927785
Rachel Daniel <rachel.daniel@fishtownanalytics.com>
2022-09-23 14:50:51 -0500

    Updates test to check for adapter connection call instead of compile and execute

--- test/unit/test_lib.py
+++ test/unit/test_lib.py
@@ -1,8 +1,17 @@
 import os
 import unittest
 from unittest import mock
+import dbt
+from dbt.contracts.files import FileHash
+from dbt.contracts.graph.manifest import Manifest
+from dbt.contracts.graph.model_config import NodeConfig
+from dbt.contracts.graph.parsed import ParsedModelNode
 from dbt.contracts.results import RunningStatus
-from dbt.lib import SqlCompileRunnerNoIntrospection
+from dbt.lib import SqlCompileRunnerNoIntrospection, compile_sql
+from dbt.adapters.snowflake import Plugin
+from dbt.node_types import NodeType
+
+from test.unit.utils import clear_plugin, config_from_parts_or_dicts, inject_adapter
 
 
 class TestContext():
@@ -17,26 +26,33 @@ class TestContext():
 class SqlCompileRunnerNoIntrospectionTest(unittest.TestCase):
     def setUp(self):
             self.ctx = TestContext()
-            self.manifest = {'mock':'data'}
+            self.manifest = {'mock':'manifest'}
+            self.adapter = Plugin.adapter({})
+            self.adapter.connection_for = mock.MagicMock()
+            inject_adapter(self.adapter, Plugin)
+
+    def tearDown(self):
+        clear_plugin(Plugin)
 
     def test__compile_and_execute__with_connection(self):
         """
         By default, env var for allowing introspection is true, and calling this
         method should defer to the parent method.
         """
-        with mock.patch('dbt.task.base.BaseRunner.compile_and_execute') as parent_compile:
-            runner = SqlCompileRunnerNoIntrospection(None, None, None, 1, 1)
+        with mock.patch('dbt.task.sql.GenericSqlRunner.compile') as mock_compile:
+            runner = SqlCompileRunnerNoIntrospection({}, self.adapter, None, 1, 1)
             runner.compile_and_execute(self.manifest, self.ctx)
-            parent_compile.assert_called_once_with(self.manifest, self.ctx)
+            mock_compile.assert_called_once_with(self.manifest)
+            self.adapter.connection_for.assert_called_once()
 
+    
     def test__compile_and_execute__without_connection(self):
         """
         This tests only that the proper compile_and_execute is called if introspection is disabled
         """
         with mock.patch.dict(os.environ, {"__DBT_ALLOW_INTROSPECTION": "0"}):
-            with mock.patch('dbt.task.base.BaseRunner.compile_and_execute') as mock_parent_compile:
-                with mock.patch('dbt.task.sql.GenericSqlRunner.compile') as mock_sql_compile:
-                    runner = SqlCompileRunnerNoIntrospection(None, None, None, 1, 1)
-                    runner.compile_and_execute(self.manifest, self.ctx)
-                    mock_parent_compile.assert_not_called()
-                    mock_sql_compile.assert_called_once_with(self.manifest)
+            with mock.patch('dbt.task.sql.GenericSqlRunner.compile') as mock_compile:
+                runner = SqlCompileRunnerNoIntrospection({}, self.adapter, None, 1, 1)
+                runner.compile_and_execute(self.manifest, self.ctx)
+                self.adapter.connection_for.assert_not_called()
+                mock_compile.assert_called_once_with(self.manifest)
