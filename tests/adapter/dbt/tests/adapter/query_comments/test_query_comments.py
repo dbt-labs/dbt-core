1e115e18cc0e556aa1a3775b6fe47558f33592b0
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-28 16:07:02 -0500

    init query_comment test conversion pr

---
+++ tests/adapter/dbt/tests/adapter/query_comments/test_query_comments.py
@@ -0,0 +1,183 @@
+import pytest
+import io
+import json
+import re
+from dbt.exceptions import RuntimeException
+from dbt.version import __version__ as dbt_version
+from dbt.logger import log_manager
+from dbt.tests.util import run_dbt_and_capture
+from dbt.tests.adapter.query_comments.fixtures import MODELS__X_SQL
+
+
+class BaseDefaultQueryComments:
+    def matches_comment(self, msg) -> bool:
+        if not msg.startswith("/* "):
+            return False
+        # our blob is the first line of the query comments, minus the comment
+        json_str = msg.split("\n")[0][3:-3]
+        data = json.loads(json_str)
+        return (
+            data["app"] == "dbt"
+            and data["dbt_version"] == dbt_version
+            and data["node_id"] == "model.test.x"
+        )
+
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"config-version": 2, "name": "query_comment", "macro-paths": ["macros"]}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "x.sql": MODELS__X_SQL,
+        }
+
+    @pytest.fixture(scope="class", autouse=True)
+    def setUp(self, project):
+        self.initial_stdout = log_manager.stdout
+        self.initial_stderr = log_manager.stderr
+        self.stringbuf = io.StringIO()
+        log_manager.set_output_stream(self.stringbuf)
+
+    def run_get_json(self, expect_pass=True):
+        res, raw_logs = run_dbt_and_capture(
+            ["--debug", "--log-format=json", "run"], expect_pass=expect_pass
+        )
+
+        parsed_logs = []
+        for line in raw_logs.split("\n"):
+            try:
+                log = json.loads(line)
+            except ValueError:
+                continue
+
+            parsed_logs.append(log)
+
+        # empty lists evaluate as False
+        assert len(parsed_logs) > 0
+        return parsed_logs
+
+    def query_comment(self, model_name, log):
+        # N.B: a temporary string replacement regex to strip the HH:MM:SS from the log line if present.
+        # TODO: make this go away when structured logging is stable
+        log_msg = re.sub(r"(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d \| )", "", log["msg"])
+        prefix = "On {}: ".format(model_name)
+        if log_msg.startswith(prefix):
+            msg = log_msg[len(prefix) :]
+            if msg in {"COMMIT", "BEGIN", "ROLLBACK"}:
+                return None
+            return msg
+        return None
+
+    def run_assert_comments(self):
+        logs = self.run_get_json()
+
+        seen = False
+        for log in logs:
+            msg = self.query_comment("model.test.x", log)
+            if msg is not None and self.matches_comment(msg):
+                seen = True
+
+        for log in logs:
+            if seen:
+                "Never saw a matching log message! Logs:\n{}".format("\n".join(log["msg"]))
+
+    def test_comments(self, project):
+        self.run_assert_comments()
+
+
+# Base setup to be inherited #
+class BaseQueryComments(BaseDefaultQueryComments):
+    def project_config(self):
+        cfg = super().project_config
+        cfg.update({"query-comment": "dbt\nrules!\n"})
+        return cfg
+
+    def matches_comment(self, msg) -> bool:
+        return msg.startswith("/* dbt\nrules! */\n")
+
+
+class BaseMacroQueryComments(BaseDefaultQueryComments):
+    def project_config(self):
+        cfg = super().project_config
+        cfg.update({"query-comment": "{{ query_header_no_args() }}"})
+        return cfg
+
+    def matches_comment(self, msg) -> bool:
+        start_with = "/* dbt macros\nare pretty cool */\n"
+        return msg.startswith(start_with)
+
+
+class BaseMacroArgsQueryComments(BaseDefaultQueryComments):
+    def project_config(self):
+        cfg = super().project_config
+        cfg.update(
+            {"query-comment": "{{ return(ordered_to_json(query_header_args(target.name))) }}"}
+        )
+        return cfg
+
+    def matches_comment(self, msg) -> bool:
+        expected_dct = {
+            "app": "dbt++",
+            "dbt_version": dbt_version,
+            "macro_version": "0.1.0",
+            "message": "blah: default2",
+        }
+        expected = "/* {} */\n".format(json.dumps(expected_dct, sort_keys=True))
+        return msg.startswith(expected)
+
+
+class BaseMacroInvalidQueryComments(BaseDefaultQueryComments):
+    def project_config(self):
+        cfg = super().project_config
+        cfg.update({"query-comment": "{{ invalid_query_header() }}"})
+        return cfg
+
+    def run_assert_comments(self):
+        with pytest.raises(RuntimeException):
+            self.run_get_json(expect_pass=False)
+
+
+class BaseNullQueryComments(BaseDefaultQueryComments):
+    def project_config(self):
+        cfg = super().project_config
+        cfg.update({"query-comment": ""})
+        return cfg
+
+    def matches_comment(self, msg) -> bool:
+        return not ("/*" in msg or "*/" in msg)
+
+
+class BaseEmptyQueryComments(BaseDefaultQueryComments):
+    def project_config(self):
+        cfg = super().project_config
+        cfg.update({"query-comment": ""})
+        return cfg
+
+    def matches_comment(self, msg) -> bool:
+        return not ("/*" in msg or "*/" in msg)
+
+
+# Tests #
+class TestQueryComments(BaseQueryComments):
+    pass
+
+
+class TestMacroQueryComments(BaseMacroQueryComments):
+    pass
+
+
+class TestMacroArgsQueryComments(BaseMacroArgsQueryComments):
+    pass
+
+
+class TestMacroInvalidQueryComments(BaseMacroInvalidQueryComments):
+    pass
+
+
+class TestNullQueryComments(BaseNullQueryComments):
+    pass
+
+
+class TestEmptyQueryComments(BaseEmptyQueryComments):
+    pass
