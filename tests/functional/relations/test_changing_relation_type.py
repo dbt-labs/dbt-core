669f642ed786b7ae7a454deb7096b0ddb363ecf3
Colin <colin.rogers@dbtlabs.com>
2022-10-12 10:54:59 -0700

    formatting fixes

--- tests/functional/relations/test_changing_relation_type.py
+++ tests/functional/relations/test_changing_relation_type.py
@@ -15,24 +15,25 @@ select '{{ var("materialized") }}' as materialization
 {% endif %}
 """
 
+
 class BaseChangeRelationTypeValidator:
     @pytest.fixture(scope="class")
     def models(self):
         return {
             "model_mc_modelface.sql": _DEFAULT_CHANGE_RELATION_TYPE_MODEL
         }
-    
+
     def _run_and_check(self, materialization):
         results = run_dbt(["run", '--vars', f'materialized: {materialization}'])
         assert results[0].node.config.materialized == materialization
         assert len(results) == 1
-
 
     def test_changing_materialization_changes_relation_type(self, project):
         self._run_and_check('view')
         self._run_and_check('table')
         self._run_and_check('view')
         self._run_and_check('incremental')
-    
+
+
 class TestChangeRelationTypes(BaseChangeRelationTypeValidator):
     pass
