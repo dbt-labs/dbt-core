c35b1885ceadcedab4907d35291258963475d9cf
Michelle Ark <michelle.ark@dbtlabs.com>
2022-10-20 15:41:43 -0400

    use packages fixture, clean up test names

--- tests/functional/exit_codes/test_exit_codes.py
+++ tests/functional/exit_codes/test_exit_codes.py
@@ -4,8 +4,7 @@ import dbt.exceptions
 from dbt.tests.util import (
     check_table_does_exist,
     check_table_does_not_exist,
-    run_dbt,
-    write_config_file
+    run_dbt
 )
 from tests.functional.exit_codes.fixtures import (
     BaseConfigProject,
@@ -21,35 +20,35 @@ class TestExitCodes(BaseConfigProject):
     def snapshots(self):
         return {"g.sql": snapshots_good_sql}
 
-    def test_postgres_exit_code_run_succeed(self, project):
+    def test_exit_code_run_succeed(self, project):
         results = run_dbt(['run', '--model', 'good'])
         assert len(results) == 1
         check_table_does_exist(project.adapter, 'good')
 
-    def test_postgres_exit_code_run_fail(self, project):
+    def test_exit_code_run_fail(self, project):
         results = run_dbt(['run', '--model', 'bad'], expect_pass=False)
         assert len(results) == 1
         check_table_does_not_exist(project.adapter, 'bad')
 
-    def test_postgres_schema_test_pass(self, project):
+    def test_schema_test_pass(self, project):
         results = run_dbt(['run', '--model', 'good'])
         assert len(results) == 1
 
         results = run_dbt(['test', '--model', 'good'])
         assert len(results) == 1
 
-    def test_postgres_schema_test_fail(self, project):
+    def test_schema_test_fail(self, project):
         results = run_dbt(['run', '--model', 'dupe'])
         assert len(results) == 1
 
         results = run_dbt(['test', '--model', 'dupe'], expect_pass=False)
         assert len(results) == 1
 
-    def test_postgres_compile(self, project):
+    def test_compile(self, project):
         results = run_dbt(['compile'])
         assert len(results) == 7
 
-    def test_postgres_snapshot_pass(self, project):
+    def test_snapshot_pass(self, project):
         results = run_dbt(['snapshot'])
         assert len(results) == 1
         check_table_does_exist(project.adapter, 'good_snapshot')
@@ -60,7 +59,7 @@ class TestExitCodesSnapshotFail(BaseConfigProject):
     def snapshots(self):
         return {"b.sql": snapshots_bad_sql}
 
-    def test_postgres_snapshot_fail(self, project):
+    def test_snapshot_fail(self, project):
         results = run_dbt(['run', '--model', 'good'])
         assert len(results) == 1
 
@@ -71,7 +70,7 @@ class TestExitCodesSnapshotFail(BaseConfigProject):
 
 class TestExitCodesDeps:
     @pytest.fixture(scope="class")
-    def packages_config(self):
+    def packages(self):
         return {
             "packages": [
                 {
@@ -81,16 +80,14 @@ class TestExitCodesDeps:
             ]
         }
 
-    def test_postgres_deps(self, project, packages_config):
-        write_config_file(packages_config, project.project_root, "packages.yml")
-
+    def test_deps(self, project):
         results = run_dbt(['deps'])
         assert results is None
 
 
 class TestExitCodesDepsFail:
     @pytest.fixture(scope="class")
-    def packages_config(self):
+    def packages(self):
         return {
             "packages": [
                 {
@@ -100,9 +97,7 @@ class TestExitCodesDepsFail:
             ]
         }
 
-    def test_postgres_deps_fail(self, project, packages_config):
-        write_config_file(packages_config, project.project_root, "packages.yml")
-
+    def test_deps_fail(self, project):
         with pytest.raises(dbt.exceptions.InternalException) as exc:
             run_dbt(['deps'])
         expected_msg = "Error checking out spec='bad-branch'"
@@ -114,7 +109,7 @@ class TestExitCodesSeed:
     def seeds(self):
         return {"good.csv": data_seed_good_csv}
 
-    def test_postgres_seed(self, project):
+    def test_seed(self, project):
         results = run_dbt(['seed'])
         assert len(results) == 1
 
@@ -124,5 +119,5 @@ class TestExitCodesSeedFail:
     def seeds(self):
         return {"bad.csv": data_seed_bad_csv}
 
-    def test_postgres_seed(self, project):
+    def test_seed(self, project):
         run_dbt(['seed'], expect_pass=False)
