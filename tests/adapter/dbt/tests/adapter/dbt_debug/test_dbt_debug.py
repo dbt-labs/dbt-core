a74555741011c66396f088a08e14b35bb142d546
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-10-24 10:38:58 -0500

    init pr for dbt_debug test conversion

---
+++ tests/adapter/dbt/tests/adapter/dbt_debug/test_dbt_debug.py
@@ -0,0 +1,49 @@
+import pytest
+import re
+from dbt.tests.util import run_dbt
+
+MODELS__MODEL_SQL = """
+seled 1 as id
+"""
+
+
+class BaseDebug:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"model.sql": MODELS__MODEL_SQL}
+
+    @pytest.fixture(autouse=True)
+    def capsys(self, capsys):
+        self.capsys = capsys
+
+    def assertGotValue(self, linepat, result):
+        found = False
+        output = self.capsys.readouterr().out
+        for line in output.split('\n'):
+            if linepat.match(line):
+                found = True
+                assert result in line
+        if not found:
+            with pytest.raises(Exception) as exc:
+                msg = f'linepat {linepat} not found in stdout: {output}'
+                assert msg in str(exc.value)
+
+    def test_ok(self, project):
+        run_dbt(["debug"])
+        assert "ERROR" not in self.capsys.readouterr().out
+
+    def test_nopass(self, project):
+        run_dbt(['debug', '--target', 'nopass'], expect_pass=False)
+        self.assertGotValue(re.compile(r'\s+profiles\.yml file'), 'ERROR invalid')
+
+    def test_wronguser(self, project):
+        run_dbt(['debug', '--target', 'wronguser'], expect_pass=False)
+        self.assertGotValue(re.compile(r'\s+Connection test'), 'ERROR')
+
+    def test_empty_target(self, project):
+        run_dbt(['debug', '--target', 'none_target'], expect_pass=False)
+        self.assertGotValue(re.compile(r"\s+output 'none_target'"), 'misconfigured')
+
+
+class TestDebug(BaseDebug):
+    pass
