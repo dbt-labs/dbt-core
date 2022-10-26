fd6f12b976e33e9be3b0d56cf335b055ff1da09d
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-10-26 16:56:00 -0500

    reformatting test, new method for capsys and error messgae to check, todo fix badproject

--- tests/adapter/dbt/tests/adapter/dbt_debug/test_dbt_debug.py
+++ tests/adapter/dbt/tests/adapter/dbt_debug/test_dbt_debug.py
@@ -1,5 +1,7 @@
 import pytest
+import os
 import re
+import yaml
 from dbt.tests.util import run_dbt
 
 MODELS__MODEL_SQL = """
@@ -25,40 +27,85 @@ class BaseDebug:
                 assert result in line
         if not found:
             with pytest.raises(Exception) as exc:
-                msg = f'linepat {linepat} not found in stdout: {output}'
+                msg = f"linepat {linepat} not found in stdout: {output}"
                 assert msg in str(exc.value)
+
+    def check_project(self, splitout, msg="ERROR invalid"):
+        for line in splitout:
+            if line.strip().startswith("dbt_project.yml file"):
+                assert msg in line
+            elif line.strip().startswith("profiles.yml file"):
+                assert "ERROR invalid" not in line
 
 
 class BaseDebugProfileVariable(BaseDebug):
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {
-            'config-version': 2,
-            'profile': '{{ "te" ~ "st" }}'
+            "config-version": 2,
+            "profile": '{{ "te" ~ "st" }}'
         }
 
 
-class TestDebug(BaseDebug):
+class TestDebugPostgres(BaseDebug):
     def test_ok(self, project):
         run_dbt(["debug"])
         assert "ERROR" not in self.capsys.readouterr().out
 
     def test_nopass(self, project):
-        run_dbt(['debug', '--target', 'nopass'], expect_pass=False)
-        self.assertGotValue(re.compile(r'\s+profiles\.yml file'), 'ERROR invalid')
+        run_dbt(["debug", "--target", "nopass"], expect_pass=False)
+        self.assertGotValue(re.compile(r"\s+profiles\.yml file"), "ERROR invalid")
 
     def test_wronguser(self, project):
-        run_dbt(['debug', '--target', 'wronguser'], expect_pass=False)
-        self.assertGotValue(re.compile(r'\s+Connection test'), 'ERROR')
+        run_dbt(["debug", "--target", "wronguser"], expect_pass=False)
+        self.assertGotValue(re.compile(r"\s+Connection test"), "ERROR")
 
     def test_empty_target(self, project):
-        run_dbt(['debug', '--target', 'none_target'], expect_pass=False)
-        self.assertGotValue(re.compile(r"\s+output 'none_target'"), 'misconfigured')
+        run_dbt(["debug", "--target", "none_target"], expect_pass=False)
+        self.assertGotValue(re.compile(r"\s+output 'none_target'"), "misconfigured")
 
 
 class TestDebugProfileVariablePostgres(BaseDebugProfileVariable):
     pass
 
 
-# class TestDebugInvalidProjectPostgres(BaseDebug):
-#     pass
+class TestDebugInvalidProjectPostgres(BaseDebug):
+
+    def test_empty_project(self, project):
+        with open("dbt_project.yml", "w") as f:  # noqa: F841
+            pass
+
+        run_dbt(["debug", "--profile", "test"], expect_pass=False)
+        splitout = self.capsys.readouterr().out.split("\n")
+        self.check_project(splitout)
+
+    def test_not_found_project(self, project):
+        run_dbt(["debug", "--project-dir", "nopass"], expect_pass=False)
+        splitout = self.capsys.readouterr().out.split("\n")
+        self.check_project(splitout, msg="ERROR not found")
+
+    def test_invalid_project_outside_current_dir(self, project):
+        # create a dbt_project.yml
+        project_config = {
+            "invalid-key": "not a valid key in this project"
+        }
+        os.makedirs("custom", exist_ok=True)
+        with open("custom/dbt_project.yml", "w") as f:
+            yaml.safe_dump(project_config, f, default_flow_style=True)
+        run_dbt(["debug", "--project-dir", "custom"], expect_pass=False)
+        splitout = self.capsys.readouterr().out.split("\n")
+        self.check_project(splitout)
+
+
+# class TestDebugBadProjectPostgres(BaseDebug):
+#     @pytest.fixture(scope="class")
+#     def project_config_update(self):
+#         # load a special project that is an error
+#         return {
+#             "invalid-key": "not a valid key so this is bad project"
+#         }
+
+#     def test_badproject(self, project):
+#         run_dbt(["debug", "--profile", "test"], expect_pass=False)
+#         splitout = self.capsys.readouterr().out.split("\n")
+#         self.check_project(splitout)
