80aa5c4c300f89941905ce59056838b3d2fb5a8d
Ian Knox <ian.knox@dbtlabs.com>
2022-12-06 13:57:04 -0600

    removed multi-path-ness for single_threaded args

--- core/dbt/task/base.py
+++ core/dbt/task/base.py
@@ -78,11 +78,6 @@ class BaseTask(metaclass=ABCMeta):
     def __init__(self, args, config, project=None):
         self.args = args
         self.config = config
-
-        import argparse
-
-        if hasattr(config, "args") and type(self.config.args) is argparse.Namespace:
-            self.config.args.single_threaded = False
         self.project = config if isinstance(config, Project) else project
 
     @classmethod
