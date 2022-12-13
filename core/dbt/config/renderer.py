43d0a3cb917b2d62323644e71757eda375588aaa
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2022-12-13 15:56:27 -0700

    Check for non-empty set intersection
    Co-authored-by: Kshitij Aranke <kshitij.aranke@dbtlabs.com>

--- core/dbt/config/renderer.py
+++ core/dbt/config/renderer.py
@@ -159,8 +159,8 @@ class DbtProjectYamlRenderer(BaseRenderer):
         if first in {"seeds", "models", "snapshots", "tests"}:
             keypath_parts = {(k.lstrip("+ ") if isinstance(k, str) else k) for k in keypath}
             # model-level hooks
-            late_rendered_hooks = ["pre-hook", "post-hook", "pre_hook", "post_hook"]
-            if any(hook in keypath_parts for hook in late_rendered_hooks):
+            late_rendered_hooks = {"pre-hook", "post-hook", "pre_hook", "post_hook"}
+            if keypath_parts.intersection(late_rendered_hooks):
                 return False
 
         return True
