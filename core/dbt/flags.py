aa06c81b401a419c3ad75a1896b0168bbaa4967a
Gerda Shank <gerda@dbtlabs.com>
2023-01-04 16:03:47 -0500

    Cleanup unimplemented flags

--- core/dbt/flags.py
+++ core/dbt/flags.py
@@ -232,8 +232,6 @@ def get_flag_dict():
         "quiet": QUIET,
         "no_print": NO_PRINT,
         "cache_selected_only": CACHE_SELECTED_ONLY,
-        "target_path": TARGET_PATH,
-        "log_path": LOG_PATH,
     }
 
 
