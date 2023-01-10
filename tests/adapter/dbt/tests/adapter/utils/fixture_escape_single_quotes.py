cef6a90e1d7b957ed2216829215b778879226951
Doug Beatty <doug.beatty@dbtlabs.com>
2023-01-10 15:15:07 -0700

    Check length of escaped strings in the adapter test

--- tests/adapter/dbt/tests/adapter/utils/fixture_escape_single_quotes.py
+++ tests/adapter/dbt/tests/adapter/utils/fixture_escape_single_quotes.py
@@ -1,15 +1,15 @@
 # escape_single_quotes
 
 models__test_escape_single_quotes_quote_sql = """
-select '{{ escape_single_quotes("they're") }}' as actual, 'they''re' as expected union all
-select '{{ escape_single_quotes("they are") }}' as actual, 'they are' as expected
+select '{{ escape_single_quotes("they're") }}' as actual, {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length, 'they''re' as expected, 7 as expected_length union all
+select '{{ escape_single_quotes("they are") }}' as actual, {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length, 'they are' as expected, 8 as expected_length
 """
 
 
 # The expected literal is 'they\'re'. The second backslash is to escape it from Python.
 models__test_escape_single_quotes_backslash_sql = """
-select '{{ escape_single_quotes("they're") }}' as actual, 'they\\'re' as expected union all
-select '{{ escape_single_quotes("they are") }}' as actual, 'they are' as expected
+select '{{ escape_single_quotes("they're") }}' as actual, {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length, 'they\\'re' as expected, 7 as expected_length union all
+select '{{ escape_single_quotes("they are") }}' as actual, {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length, 'they are' as expected, 8 as expected_length
 """
 
 
@@ -21,4 +21,7 @@ models:
       - assert_equal:
           actual: actual
           expected: expected
+      - assert_equal:
+          actual: actual_length
+          expected: expected_length
 """
