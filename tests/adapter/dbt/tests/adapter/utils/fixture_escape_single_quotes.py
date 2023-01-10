1d57240e1a31d5f8a2bb084b3984834c6ae398cd
Doug Beatty <doug.beatty@dbtlabs.com>
2023-01-10 16:01:11 -0700

    One column per line to improve readability

--- tests/adapter/dbt/tests/adapter/utils/fixture_escape_single_quotes.py
+++ tests/adapter/dbt/tests/adapter/utils/fixture_escape_single_quotes.py
@@ -1,15 +1,37 @@
 # escape_single_quotes
 
 models__test_escape_single_quotes_quote_sql = """
-select '{{ escape_single_quotes("they're") }}' as actual, {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length, 'they''re' as expected, 7 as expected_length union all
-select '{{ escape_single_quotes("they are") }}' as actual, {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length, 'they are' as expected, 8 as expected_length
+select
+  '{{ escape_single_quotes("they're") }}' as actual,
+  'they''re' as expected,
+  {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length,
+  7 as expected_length
+
+union all
+
+select
+  '{{ escape_single_quotes("they are") }}' as actual,
+  'they are' as expected,
+  {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length,
+  8 as expected_length
 """
 
 
 # The expected literal is 'they\'re'. The second backslash is to escape it from Python.
 models__test_escape_single_quotes_backslash_sql = """
-select '{{ escape_single_quotes("they're") }}' as actual, {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length, 'they\\'re' as expected, 7 as expected_length union all
-select '{{ escape_single_quotes("they are") }}' as actual, {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length, 'they are' as expected, 8 as expected_length
+select
+  '{{ escape_single_quotes("they're") }}' as actual,
+  'they\\'re' as expected,
+  {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length,
+  7 as expected_length
+
+union all
+
+select
+  '{{ escape_single_quotes("they are") }}' as actual,
+  'they are' as expected,
+  {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length,
+  8 as expected_length
 """
 
 
