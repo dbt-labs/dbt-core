2df619d6fe370ff7f0438e99af30cdc043a9b61a
Emily Rockman <emily.rockman@dbtlabs.com>
2023-01-10 14:07:41 -0600

    fix failing test

--- tests/functional/schema_tests/test_schema_v2_tests.py
+++ tests/functional/schema_tests/test_schema_v2_tests.py
@@ -95,7 +95,7 @@ from tests.functional.schema_tests.fixtures import (
     alt_local_utils__macros__type_timestamp_sql,
     all_quotes_schema__schema_yml,
 )
-from dbt.exceptions import ParsingError, CompilationError, DuplicateResourceName
+from dbt.exceptions import ParsingError, CompilationError, DuplicateResourceNameError
 from dbt.contracts.results import TestStatus
 
 
@@ -904,7 +904,7 @@ class TestGenericTestsCollide:
         project,
     ):
         """These tests collide, since only the configs differ"""
-        with pytest.raises(DuplicateResourceName) as exc:
+        with pytest.raises(DuplicateResourceNameError) as exc:
             run_dbt()
         assert "dbt found two tests with the name" in str(exc.value)
 
