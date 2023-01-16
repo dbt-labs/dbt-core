aed39369f4a169a5d1f90d243f7aeb59470e810f
Jeremy Cohen <jeremy@dbtlabs.com>
2023-01-16 12:04:15 +0100

    Respect quoting for 'this' in dbt-py models #6619

--- core/dbt/include/global_project/macros/python_model/python.sql
+++ core/dbt/include/global_project/macros/python_model/python.sql
@@ -33,8 +33,8 @@ def source(*args, dbt_load_df_function):
     {% set config_dbt_used = zip(model.config.config_keys_used, model.config.config_keys_defaults) | list %}
     {%- for key, default in config_dbt_used -%}
         {# weird type testing with enum, would be much easier to write this logic in Python! #}
-        {%- if key == 'language' -%}
-          {%- set value = 'python' -%}
+        {%- if key == "language" -%}
+          {%- set value = "python" -%}
         {%- endif -%}
         {%- set value = model.config.get(key, default) -%}
         {%- do config_dict.update({key: value}) -%}
@@ -62,11 +62,12 @@ class config:
 
 class this:
     """dbt.this() or dbt.this.identifier"""
-    database = '{{ this.database }}'
-    schema = '{{ this.schema }}'
-    identifier = '{{ this.identifier }}'
+    database = "{{ this.database }}"
+    schema = "{{ this.schema }}"
+    identifier = "{{ this.identifier }}"
+    {% set this_relation_name = this | string | replace('"', '\\"') %}
     def __repr__(self):
-        return '{{ this }}'
+        return "{{ this_relation_name  }}"
 
 
 class dbtObj:
