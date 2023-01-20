5aa801aaaf2e8f7b162c4d8ebb682bca483c035e
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-01-20 15:55:44 -0600

    init commit for column_types test conversion

---
+++ tests/functional/column_types/test_alter_column_types.py
@@ -0,0 +1,30 @@
+import pytest
+from dbt.tests.util import run_dbt
+from tests.functional.column_types.fixtures import (
+    macro_test_alter_column_type,
+    model_sql,
+    schema_yml,
+)
+
+
+class TestAlterColumnTypes:
+    @pytest.fixture(scope="class")
+    def macros(self):
+        return {
+            "test_alter_column_type.sql": macro_test_alter_column_type,
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "model.sql": model_sql,
+            "schema.yml": schema_yml,
+        }
+
+    def test_run_alter_and_test(self, project):
+        kwargs = "model_name, column_name, new_column_type"
+        results = run_dbt(["run"])
+        assert len(results) == 1
+        run_dbt(["run-operation", "test_alter_column_type" "--args", kwargs])
+        results = run_dbt(["test"])
+        assert len(results) == 1
