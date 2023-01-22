c2a0e4d42aae59645d1f34a12ff95dcd888f7061
Sean McIntyre <sean.mcintyre@fishtownanalytics.com>
2023-01-22 16:24:12 +0100

    Add the yappi profiler for multithread profiling

--- core/dbt/profiler.py
+++ core/dbt/profiler.py
@@ -3,6 +3,8 @@ from cProfile import Profile
 from pstats import Stats
 from typing import Any, Generator
 
+import yappi
+
 
 @contextmanager
 def profiler(enable: bool, outfile: str) -> Generator[Any, None, None]:
@@ -10,6 +12,7 @@ def profiler(enable: bool, outfile: str) -> Generator[Any, None, None]:
         if enable:
             profiler = Profile()
             profiler.enable()
+            yappi.start()
 
         yield
     finally:
@@ -18,3 +21,16 @@ def profiler(enable: bool, outfile: str) -> Generator[Any, None, None]:
             stats = Stats(profiler)
             stats.sort_stats("tottime")
             stats.dump_stats(str(outfile))
+            yappi.stop()
+            threads = yappi.get_thread_stats()
+            all_stats = Stats()
+
+            for thread in threads:
+                stats = yappi.get_func_stats(ctx_id=thread.id)
+                filename = f"thread-{thread.id}.pstat"
+                stats.save(filename, "pstat")
+
+                pstats = yappi.convert2pstats(stats)
+                all_stats.add(pstats)
+
+            all_stats.dump_stats('threads-combined.pstat')
