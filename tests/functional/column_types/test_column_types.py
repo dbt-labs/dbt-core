a54dd517f97606cdd985f2456c4fb0a95aa87ea2
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-01-23 14:37:11 -0600

    remove alt tests, remove old tests, push up working conversion

--- tests/functional/column_types/test_column_types.py
+++ tests/functional/column_types/test_column_types.py
@@ -8,26 +8,17 @@ class BaseTestColumnTypes:
     def macros(self):
         return {"test_is_type.sql": macro_test_is_type_sql}
 
-    def run_and_test(self, project):
+    def run_and_test(self):
         results = run_dbt(["run"])
         assert len(results) == 1
         results = run_dbt(["test"])
         assert len(results) == 1
 
 
-class TestColumnTypes(BaseTestColumnTypes):
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {"shcema.yml": schema_yml}
-
-    def test_run_and_test(self, project):
-        self.run_and_test(project)
-
-
 class TestPosgresColumnTypes(BaseTestColumnTypes):
     @pytest.fixture(scope="class")
     def models(self):
-        return {"model.sql": model_sql}
+        return {"model.sql": model_sql, "schema.yml": schema_yml}
 
     def test_run_and_test(self, project):
-        self.run_and_test(project)
+        self.run_and_test()
