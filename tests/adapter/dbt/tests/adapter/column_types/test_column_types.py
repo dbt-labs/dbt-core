e63244c539f2cab6c7cbc7ec8768f140cba4a7f3
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-01-23 16:19:49 -0600

    rename base class, move to adapter zone so adapters can use

--- tests/adapter/dbt/tests/adapter/column_types/test_column_types.py
+++ tests/adapter/dbt/tests/adapter/column_types/test_column_types.py
@@ -1,9 +1,9 @@
 import pytest
 from dbt.tests.util import run_dbt
-from tests.functional.column_types.fixtures import macro_test_is_type_sql, model_sql, schema_yml
+from dbt.tests.adapter.column_types.fixtures import macro_test_is_type_sql, model_sql, schema_yml
 
 
-class BaseTestColumnTypes:
+class BaseColumnTypes:
     @pytest.fixture(scope="class")
     def macros(self):
         return {"test_is_type.sql": macro_test_is_type_sql}
@@ -15,7 +15,7 @@ class BaseTestColumnTypes:
         assert len(results) == 1
 
 
-class TestPosgresColumnTypes(BaseTestColumnTypes):
+class TestPosgresColumnTypes(BaseColumnTypes):
     @pytest.fixture(scope="class")
     def models(self):
         return {"model.sql": model_sql, "schema.yml": schema_yml}
