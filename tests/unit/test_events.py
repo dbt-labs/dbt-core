bbb4f3ce3f1a4492dd61b8cc01bb46549c3e7537
Gerda Shank <gerda@dbtlabs.com>
2023-01-24 15:42:41 -0500

    Add to test_events.py

--- tests/unit/test_events.py
+++ tests/unit/test_events.py
@@ -185,6 +185,7 @@ sample_values = [
     PartialParsingError(exc_info={}),
     PartialParsingSkipParsing(),
     UnableToPartialParse(reason="something went wrong"),
+    StateCheckVarsHash(vars="testing", target="testing", profile="testing"),
     PartialParsingNotEnabled(),
     ParsedFileLoadFailed(path="", exc="", exc_info=""),
     StaticParserCausedJinjaRendering(path=""),
