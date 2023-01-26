b5caf2b6969b8993b1811e797888a44b2bfccbce
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2023-01-26 09:14:24 -0800

    Address PR comments

--- tests/cli/test_cli_run.py
+++ tests/cli/test_cli_run.py
@@ -1,33 +1,48 @@
+import pytest
 from click.testing import CliRunner
 
 from dbt.cli.main import cli
+from tests.cli.fixtures import BaseConfigProject
 
 
-def test_build():
-    runner = CliRunner()
-    clean_result = runner.invoke(cli, ['clean'])
-    assert 'target' in clean_result.output
-    assert 'dbt_packages' in clean_result.output
-    assert 'logs' in clean_result.output
+class TestCli(BaseConfigProject):
+    @pytest.fixture(scope="class")
+    def runner(self):
+        return CliRunner()
 
-    deps_result = runner.invoke(cli, ['deps'])
-    assert 'dbt-labs/dbt_utils' in deps_result.output
-    assert '1.0.0' in deps_result.output
+    def test_clean(self, runner, project):
+        result = runner.invoke(cli, ['clean'])
+        assert 'target' in result.output
+        assert 'dbt_packages' in result.output
+        assert 'logs' in result.output
 
-    ls_result = runner.invoke(cli, ['ls'])
-    assert '1 seed' in ls_result.output
-    assert '1 model' in ls_result.output
-    assert '4 tests' in ls_result.output
-    assert '1 snapshot' in ls_result.output
+    def test_deps(self, runner, project):
+        result = runner.invoke(cli, ['deps'])
+        assert 'dbt-labs/dbt_utils' in result.output
+        assert '1.0.0' in result.output
 
-    build_result = runner.invoke(cli, ['build'])
-    # 1 seed, 1 model, 2 tests
-    assert 'PASS=4' in build_result.output
-    # 2 tests
-    assert 'ERROR=2' in build_result.output
-    # 1 snapshot
-    assert 'SKIP=1' in build_result.output
+    def test_ls(self, runner, project):
+        runner.invoke(cli, ['deps'])
+        ls_result = runner.invoke(cli, ['ls'])
+        assert '1 seed' in ls_result.output
+        assert '1 model' in ls_result.output
+        assert '5 tests' in ls_result.output
+        assert '1 snapshot' in ls_result.output
 
-    docs_generate_result = runner.invoke(cli, ['docs', 'generate'])
-    assert 'Building catalog' in docs_generate_result.output
-    assert 'Catalog written' in docs_generate_result.output
+    def test_build(self, runner, project):
+        runner.invoke(cli, ['deps'])
+        result = runner.invoke(cli, ['build'])
+        # 1 seed, 1 model, 2 tests
+        assert 'PASS=4' in result.output
+        # 2 tests
+        assert 'ERROR=2' in result.output
+        # Singular test
+        assert 'WARN=1' in result.output
+        # 1 snapshot
+        assert 'SKIP=1' in result.output
+
+    def test_docs_generate(self, runner, project):
+        runner.invoke(cli, ['deps'])
+        result = runner.invoke(cli, ['docs', 'generate'])
+        assert 'Building catalog' in result.output
+        assert 'Catalog written' in result.output
