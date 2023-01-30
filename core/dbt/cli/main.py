c74fc2428745e197f9ed85bc46288a6f8f5ae7e5
Stu Kilgore <stuart.kilgore@gmail.com>
2023-01-30 13:22:07 -0600

    Use version_option from click

--- core/dbt/cli/main.py
+++ core/dbt/cli/main.py
@@ -22,6 +22,7 @@ from dbt.task.run_operation import RunOperationTask
 from dbt.task.build import BuildTask
 from dbt.task.generate import GenerateTask
 from dbt.task.init import InitTask
+from dbt.version import get_version_information
 
 
 # CLI invocation
@@ -64,6 +65,7 @@ class dbtRunner:
     epilog="Specify one of these sub-commands and you can find more help from there.",
 )
 @click.pass_context
+@click.version_option(version=get_version_information())
 @p.anonymous_usage_stats
 @p.cache_selected_only
 @p.debug
@@ -82,7 +84,6 @@ class dbtRunner:
 @p.static_parser
 @p.use_colors
 @p.use_experimental_parser
-@p.version
 @p.version_check
 @p.warn_error
 @p.warn_error_options
