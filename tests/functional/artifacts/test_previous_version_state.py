6d0aa92eea8d5c2db273940f1aeecec3e652e6ce
Gerda Shank <gerda@dbtlabs.com>
2023-02-07 16:09:25 -0500

    Update v8 manifest.json, test_previous_version_state.py to have more nodes

--- tests/functional/artifacts/test_previous_version_state.py
+++ tests/functional/artifacts/test_previous_version_state.py
@@ -1,18 +1,73 @@
 import pytest
 import os
 import shutil
-from dbt.tests.util import run_dbt
+from dbt.tests.util import run_dbt, get_manifest
 from dbt.exceptions import IncompatibleSchemaError
 from dbt.contracts.graph.manifest import WritableManifest
 
-# This is a *very* simple project, with just one model in it.
+# This project must have one of each kind of node type, plus disabled versions, for
+# test coverage to be effective.
 models__my_model_sql = """
 select 1 as id
 """
 
+seeds__my_seed_csv = """
+id,value
+4,2
+"""
+
+docs__somedoc_md = """
+{% docs somedoc %}
+Testing, testing
+{% enddocs %}
+"""
+
+macros__dummy_test_sql = """
+{% test nothing(model) %}
+
+-- a silly test to make sure that table-level tests show up in the manifest
+-- without a column_name field
+select 0
+
+{% endtest %}
+"""
+
+snapshot__snapshot_seed_sql = """
+{% snapshot snapshot_seed %}
+{{
+    config(
+      unique_key='id',
+      strategy='check',
+      check_cols='all',
+      target_schema=schema,
+    )
+}}
+select * from {{ ref('my_seed') }}
+{% endsnapshot %}
+"""
+
+tests__just_my_sql = """
+{{ config(tags = ['data_test_tag']) }}
+
+select * from {{ ref('my_model') }}
+where false
+"""
+
+analyses__a_sql = """
+select 4 as id
+"""
+
 # Use old attribute names (v1.0-1.2) to test forward/backward compatibility with the rename in v1.3
-models__metric_yml = """
+models__schema_yml = """
 version: 2
+models:
+  - name: my_model
+    description: "Example model"
+    columns:
+     - name: id
+       tests:
+       - not_null
+
 metrics:
   - name: my_metric
     label: Count records
@@ -22,6 +77,22 @@ metrics:
     sql: "*"
     timestamp: updated_at
     time_grains: [day]
+sources:
+  - name: my_source
+    description: "My source"
+    loader: a_loader
+    tables:
+      - name: my_table
+        description: "My table"
+        identifier: my_seed
+exposures:
+  - name: simple_exposure
+    type: dashboard
+    depends_on:
+      - ref('my_model')
+      - source('my_source', 'my_table')
+    owner:
+      email: something@example.com
 """
 
 # SETUP: Using this project, we have run past minor versions of dbt
@@ -46,7 +117,42 @@ class TestPreviousVersionState:
 
     @pytest.fixture(scope="class")
     def models(self):
-        return {"my_model.sql": models__my_model_sql, "metric.yml": models__metric_yml}
+        return {
+            "my_model.sql": models__my_model_sql,
+            "schema.yml": models__schema_yml,
+            "somedoc.md": docs__somedoc_md,
+        }
+
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"my_seed.csv": seeds__my_seed_csv}
+
+    @pytest.fixture(scope="class")
+    def snapshots(self):
+        return {"snapshot_seed.sql": snapshot__snapshot_seed_sql}
+
+    @pytest.fixture(scope="class")
+    def tests(self):
+        return {"just_my.sql": tests__just_my_sql}
+
+    @pytest.fixture(scope="class")
+    def macros(self):
+        return {"dummy_test.sql": macros__dummy_test_sql}
+
+    @pytest.fixture(scope="class")
+    def analyses(self):
+        return {"a.sql": analyses__a_sql}
+
+    def test_project(self, project):
+        results = run_dbt(["run"])
+        assert len(results) == 1
+        manifest = get_manifest(project.project_root)
+        # model, snapshot, seed, singular test, generic test, analysis
+        assert len(manifest.nodes) == 6
+        assert len(manifest.sources) == 1
+        assert len(manifest.exposures) == 1
+        assert len(manifest.metrics) == 1
+        assert "macro.test.test_nothing" in manifest.macros
 
     # Use this method when generating a new manifest version for the first time.
     # Once generated, we shouldn't need to re-generate or modify the manifest.
@@ -82,7 +188,7 @@ class TestPreviousVersionState:
         ]
         if expect_pass:
             results = run_dbt(cli_args, expect_pass=expect_pass)
-            assert len(results) == 0
+            assert len(results) == 4
         else:
             with pytest.raises(IncompatibleSchemaError):
                 run_dbt(cli_args, expect_pass=expect_pass)
