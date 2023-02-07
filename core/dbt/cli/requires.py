eb4004f8ce3f9c909474e10a74f35ebfd4ff12ba
Michelle Ark <michelle.ark@dbtlabs.com>
2023-02-07 17:27:32 -0500

    use UnsetProfile in deps and clean commands

--- core/dbt/cli/requires.py
+++ core/dbt/cli/requires.py
@@ -2,7 +2,7 @@ from dbt.adapters.factory import adapter_management, register_adapter
 from dbt.flags import set_flags
 from dbt.cli.flags import Flags
 from dbt.config import RuntimeConfig
-from dbt.config.runtime import load_project, load_profile
+from dbt.config.runtime import load_project, load_profile, UnsetProfile
 from dbt.events.functions import setup_event_logger
 from dbt.exceptions import DbtProjectError
 from dbt.parser.manifest import ManifestLoader, write_manifest
@@ -49,24 +49,34 @@ def preflight(func):
     return update_wrapper(wrapper, func)
 
 
-def profile(func):
-    def wrapper(*args, **kwargs):
-        ctx = args[0]
-        assert isinstance(ctx, Context)
+def profile(*args0, unset=False):
+    def outer_wrapper(func):
+        def wrapper(*args, **kwargs):
+            ctx = args[0]
+            assert isinstance(ctx, Context)
 
-        if ctx.obj.get("profile") is None:
-            flags = ctx.obj["flags"]
-            # TODO: Generalize safe access to flags.THREADS:
-            # https://github.com/dbt-labs/dbt-core/issues/6259
-            threads = getattr(flags, "THREADS", None)
-            profile = load_profile(
-                flags.PROJECT_DIR, flags.VARS, flags.PROFILE, flags.TARGET, threads
-            )
-            ctx.obj["profile"] = profile
+            if ctx.obj.get("profile") is None:
+                flags = ctx.obj["flags"]
+                # TODO: Generalize safe access to flags.THREADS:
+                # https://github.com/dbt-labs/dbt-core/issues/6259
+                threads = getattr(flags, "THREADS", None)
+                if unset:
+                    profile = UnsetProfile()
+                else:
+                    profile = load_profile(
+                        flags.PROJECT_DIR, flags.VARS, flags.PROFILE, flags.TARGET, threads
+                    )
+                ctx.obj["profile"] = profile
 
-        return func(*args, **kwargs)
+            return func(*args, **kwargs)
 
-    return update_wrapper(wrapper, func)
+        return update_wrapper(wrapper, func)
+
+    # if there are no args, the decorator was used without params @decorator
+    # otherwise, the decorator was called with params @decorator(arg)
+    if len(args0) == 0:
+        return outer_wrapper
+    return outer_wrapper(args0[0])
 
 
 def project(func):
