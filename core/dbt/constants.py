1d2c27c7ab594d20b7f81f133aebb7bf09ecbfae
Andy Curtis <acurtis@evidation.com>
2023-03-03 14:35:12 -0500

    Make MAXIMUM_SEED_SIZE configurable

--- core/dbt/constants.py
+++ core/dbt/constants.py
@@ -1,8 +1,14 @@
+import os
+
 SECRET_ENV_PREFIX = "DBT_ENV_SECRET_"
 DEFAULT_ENV_PLACEHOLDER = "DBT_DEFAULT_PLACEHOLDER"
 METADATA_ENV_PREFIX = "DBT_ENV_CUSTOM_ENV_"
 
-MAXIMUM_SEED_SIZE = 1 * 1024 * 1024
+def get_max_seed_size():
+    mx = os.getenv('DBT_MAXIMUM_SEED_SIZE', '1')
+    return int(mx)
+
+MAXIMUM_SEED_SIZE = get_max_seed_size() * 1024 * 1024
 MAXIMUM_SEED_SIZE_NAME = "1MB"
 
 PIN_PACKAGE_URL = (
