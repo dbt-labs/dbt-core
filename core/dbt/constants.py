a4d29d791646b4bbd7970815a448bb4cc6fbbcca
Andy Curtis <acurtis@evidation.com>
2023-03-03 14:42:01 -0500

    Updated MAXIMUM_SEED_SIZE_NAME

--- core/dbt/constants.py
+++ core/dbt/constants.py
@@ -9,7 +9,7 @@ def get_max_seed_size():
     return int(mx)
 
 MAXIMUM_SEED_SIZE = get_max_seed_size() * 1024 * 1024
-MAXIMUM_SEED_SIZE_NAME = "1MB"
+MAXIMUM_SEED_SIZE_NAME = str(get_max_seed_size()) + "MB"
 
 PIN_PACKAGE_URL = (
     "https://docs.getdbt.com/docs/package-management#section-specifying-package-versions"
