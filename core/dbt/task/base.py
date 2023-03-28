c1c2f5cb7a6ece1504bfb97a550ef224eb9897f0
Ian Knox <ian.knox@dbtlabs.com>
2023-03-27 19:35:12 -0500

    remove errant breakpoint

--- core/dbt/task/base.py
+++ core/dbt/task/base.py
@@ -76,7 +76,7 @@ class BaseTask(metaclass=ABCMeta):
         self.args = args
         self.config = config
         self.project = config if isinstance(config, Project) else project
-        breakpoint()
+
         if dbt.tracking.active_user is not None:
             dbt.tracking.track_project_id({"project_id": self.project.hashed_name()})
             dbt.tracking.track_adapter_info(
