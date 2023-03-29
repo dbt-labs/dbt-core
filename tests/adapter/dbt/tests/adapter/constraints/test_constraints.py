f3bd5f36ca21a7300c53df259a49d538e30474ab
Peter Allen Webb <peter.webb@dbtlabs.com>
2023-03-29 10:28:06 -0400

    CT-1922: (Almost) complete support for model level constraints

--- tests/adapter/dbt/tests/adapter/constraints/test_constraints.py
+++ tests/adapter/dbt/tests/adapter/constraints/test_constraints.py
@@ -413,3 +413,64 @@ insert into <model_identifier> (
 
 class TestModelConstraintsRuntimeEnforcement(BaseModelConstraintsRuntimeEnforcement):
     pass
+
+
+_expected_model_constraint_sql = """
+create table {0} (
+    id integer not null,
+    color text,
+    date_day text,
+    check (id > 0),
+    primary key (id),
+    constraint strange_uniqueness_requirement unique (color, date_day)
+) ;
+insert into {0} (
+    id ,
+    color ,
+    date_day
+)
+(
+    select
+       id,
+       color,
+       date_day
+       from
+    (
+        select
+            'blue' as color,
+            1 as id,
+            '2019-01-01' as date_day
+    ) as model_subq
+);
+"""
+
+
+class BaseModelConstraintsRuntimeEnforcement:
+    """
+    These model-level constraints pass muster for dbt's preflight checks. Make sure they're
+    passed into the DDL statement. If they don't match up with the underlying data,
+    the data platform should raise an error at runtime.
+    """
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model.sql": my_model_sql,
+            "constraints_schema.yml": constrained_model_schema_yml,
+        }
+
+    @pytest.fixture(scope="class")
+    def expected_sql(self, project):
+        relation = relation_from_name(project.adapter, "my_model")
+        tmp_relation = relation.incorporate(path={"identifier": relation.identifier + "__dbt_tmp"})
+        return _expected_model_constraint_sql.format(tmp_relation)
+
+    def test__model_constraints_ddl(self, project, expected_sql):
+        results = run_dbt(["run", "-s", "my_model"])
+        assert len(results) == 1
+        generated_sql = read_file("target", "run", "test", "models", "my_model.sql")
+        assert _normalize_whitespace(expected_sql) == _normalize_whitespace(generated_sql)
+
+
+class TestModelConstraintsRuntimeEnforcement(BaseModelConstraintsRuntimeEnforcement):
+    pass
