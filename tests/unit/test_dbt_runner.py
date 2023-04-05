3928a87d7cb6e11c526df1f455589137eee80635
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2023-04-04 18:14:50 -0700

    Add kwargs support to dbtRunner Fixes #7070

--- tests/unit/test_dbt_runner.py
+++ tests/unit/test_dbt_runner.py
@@ -1,7 +1,9 @@
+from unittest import mock
+
 import pytest
 
 from dbt.cli.main import dbtRunner, dbtUsageException
-from unittest import mock
+from dbt.exceptions import DbtProjectError
 
 
 class TestDbtRunner:
@@ -35,3 +37,29 @@ class TestDbtRunner:
         # to have a project to run it and it will emit events
         dbt.invoke(["debug"])
         mock_callback.assert_called()
+
+    def test_invoke_override(self, project, dbt: dbtRunner):
+        results, success = dbt.invoke(
+            ["run"],
+            log_format="json",
+            log_path="some_random_path",
+            version_check=False,
+            profile_name="some_random_profile_name",
+            target_dir="some_random_target_dir",
+        )
+        assert results.args["log_format"] == "json"
+        assert results.args["log_path"] == "some_random_path"
+        assert results.args["version_check"] is False
+        assert results.args["profile_name"] == "some_random_profile_name"
+        assert results.args["target_dir"] == "some_random_target_dir"
+
+    def test_invoke_override_project_dir(self, project, dbt: dbtRunner):
+        with pytest.raises(
+            DbtProjectError,
+            match="No dbt_project.yml found at expected path some_random_project_dir/dbt_project.yml",
+        ):
+            results, success = dbt.invoke(["run"], project_dir="some_random_project_dir")
+
+    def test_invoke_override_profiles_dir(self, project, dbt: dbtRunner):
+        with pytest.raises(DbtProjectError, match="Could not find profile named 'test'"):
+            results, success = dbt.invoke(["run"], profiles_dir="some_random_profiles_dir")
