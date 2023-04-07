0942143fe5939e82e352a35fad590bcc1c040216
Ian Knox <ian.knox@dbtlabs.com>
2023-04-06 19:17:39 -0500

    Fixing up a test, adding a comment or two

--- tests/functional/simple_snapshot/test_comment_ending_snapshot.py
+++ tests/functional/simple_snapshot/test_comment_ending_snapshot.py
@@ -1,5 +1,6 @@
 import pytest
 from dbt.tests.util import run_dbt
+import os
 from tests.functional.simple_snapshot.fixtures import (
     models__schema_yml,
     models__ref_snapshot_sql,
@@ -15,7 +16,7 @@ snapshots_with_comment_at_end__snapshot_sql = """
             target_schema=schema,
             unique_key='id',
             strategy='check',
-            check_cols=['email'],
+            check_cols=['first_name'],
         )
     }}
     select * from {{target.database}}.{{schema}}.seed
@@ -24,29 +25,33 @@ snapshots_with_comment_at_end__snapshot_sql = """
 """
 
 
-@pytest.fixture(scope="class")
-def snapshots():
-    return {"snapshot.sql": snapshots_with_comment_at_end__snapshot_sql}
+class SnapshotsWithCommentAtEnd:
+    @pytest.fixture(scope="class")
+    def snapshots(self):
+        return {"snapshot.sql": snapshots_with_comment_at_end__snapshot_sql}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "schema.yml": models__schema_yml,
+            "ref_snapshot.sql": models__ref_snapshot_sql,
+        }
+
+    @pytest.fixture(scope="class")
+    def macros(self):
+        return {"test_no_overlaps.sql": macros__test_no_overlaps_sql}
+
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"seed.csv": seeds__seed_csv}
 
 
-@pytest.fixture(scope="class")
-def models():
-    return {
-        "schema.yml": models__schema_yml,
-        "ref_snapshot.sql": models__ref_snapshot_sql,
-    }
-
-
-@pytest.fixture(scope="class")
-def macros():
-    return {"test_no_overlaps.sql": macros__test_no_overlaps_sql}
-
-
-@pytest.fixture(scope="class")
-def seeds():
-    return {"seed.csv": seeds__seed_csv}
-
-
-def test_comment_ending(project):
-    results = run_dbt(["snapshot"])
-    assert len(results) == 1
+class TestSnapshotsWithCommentAtEnd(SnapshotsWithCommentAtEnd):
+    def test_comment_ending(self, project):
+        path = os.path.join(project.test_data_dir, "seed_pg.sql")
+        project.run_sql_file(path)
+        # N.B. Snapshot is run twice to ensure snapshot_check_all_get_existing_columns is fully run
+        # (it exits early if the table doesn't already exist)
+        results = run_dbt(["snapshot"])
+        results = run_dbt(["snapshot"])
+        assert len(results) == 1
