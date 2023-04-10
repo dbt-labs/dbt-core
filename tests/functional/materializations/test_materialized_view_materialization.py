7632aa5ce0e7fd1453932ccbf36f078217dc023a
Mike Alfare <mike.alfare@dbtlabs.com>
2023-04-10 12:32:32 -0400

    add clarifying comment as to why we have a single test that's expected to fail at the dbt-core layer

--- tests/functional/materializations/test_materialized_view_materialization.py
+++ tests/functional/materializations/test_materialized_view_materialization.py
@@ -16,4 +16,9 @@ def models():
 
 
 def test_basic(project):
+    """
+    DDL is not implemented at the abstract `dbt-core` layer; this is expected to fail.
+
+    See `tests/adapter/dbt/tests/adapter/materialized_view_tests/` for relevant tests
+    """
     run_dbt(["run"], expect_pass=False)
