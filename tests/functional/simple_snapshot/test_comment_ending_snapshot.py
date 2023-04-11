bd8400840c54acfbcd3c3427ebe73f6daa7cafec
Mila Page <67295367+VersusFacit@users.noreply.github.com>
2023-04-11 14:16:16 -0700

    Fix a little typo

--- tests/functional/simple_snapshot/test_comment_ending_snapshot.py
+++ tests/functional/simple_snapshot/test_comment_ending_snapshot.py
@@ -14,7 +14,7 @@ snapshots_with_comment_at_end__snapshot_sql = """
         )
     }}
     select * from {{target.database}}.{{schema}}.seed
-    -- Test comment to prevent reccurence of https://github.com/dbt-labs/dbt-core/issues/6781
+    -- Test comment to prevent recurrence of https://github.com/dbt-labs/dbt-core/issues/6781
 {% endsnapshot %}
 """
 
