4182d3016bb587f1f9e18c91b18fd2059f042061
Dave Connors <dave.connors@fishtownanalytics.com>
2023-04-11 09:14:48 -0500

    use hasattr

--- core/dbt/graph/selector_methods.py
+++ core/dbt/graph/selector_methods.py
@@ -312,10 +312,10 @@ class PathSelectorMethod(SelectorMethod):
             ofp = Path(real_node.original_file_path)
             if ofp in paths:
                 yield node
-            if real_node.to_dict().get('patch_path', None):
-                pfp = real_node.patch_path.split('://')[1]
-                ymlfp = Path(pfp)    
-                if ymlfp in paths:  
+            if hasattr(real_node, "patch_path") and real_node.patch_path:
+                pfp = real_node.patch_path.split("://")[1]
+                ymlfp = Path(pfp)
+                if ymlfp in paths:
                     yield node
             elif any(parent in paths for parent in ofp.parents):
                 yield node
