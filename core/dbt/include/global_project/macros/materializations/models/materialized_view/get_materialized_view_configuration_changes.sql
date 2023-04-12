8dfd93293518c8106f13cca602624f360d453d54
Mike Alfare <mike.alfare@dbtlabs.com>
2023-04-12 19:56:37 -0400

    fixed list being passed as string bug

--- core/dbt/include/global_project/macros/materializations/models/materialized_view/get_materialized_view_configuration_changes.sql
+++ core/dbt/include/global_project/macros/materializations/models/materialized_view/get_materialized_view_configuration_changes.sql
@@ -1,9 +1,8 @@
 {% macro get_materialized_view_configuration_changes(existing_relation, new_config) %}
-    {{- adapter.dispatch('get_materialized_view_configuration_changes', 'dbt')(existing_relation, new_config) -}}
+    {% do return(adapter.dispatch('get_materialized_view_configuration_changes', 'dbt')(existing_relation, new_config)) %}
 {% endmacro %}
 
 
 {% macro default__get_materialized_view_configuration_changes(existing_relation, new_config) %}
-    {% set updates = [] %}
-    {{ return(updates) }}
+    {% do return([]) %}
 {% endmacro %}
