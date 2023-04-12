c4faa37b49087e41d54733d51390ff6f23722503
Mike Alfare <mike.alfare@dbtlabs.com>
2023-04-11 20:10:07 -0400

    moved default macros in `dbt-core` into `dbt-postgres`

--- plugins/postgres/dbt/include/postgres/macros/materializations/materialized_view.sql
+++ plugins/postgres/dbt/include/postgres/macros/materializations/materialized_view.sql
@@ -1,9 +1,20 @@
-{% macro postgres__create_materialized_view_as(relation, sql) %}
-    {% set proxy_view = postgres_create_view_as(relation, sql) %}
-    {{ return(proxy_view) }}
+{% macro postgres__get_create_materialized_view_as_sql(relation, sql) %}
+    {{ return(get_create_view_as_sql(relation, sql)) }}
 {% endmacro %}
 
 
-{% macro postgres__refresh_materialized_view(relation) %}
-    {{ return({'relations': [relation]}) }}
+{% macro postgres__get_refresh_data_in_materialized_view_sql(relation) %}
+    select 1;
+{% endmacro %}
+
+
+{% macro postgres__get_replace_materialized_view_as_sql(relation, sql, existing_relation, backup_relation, intermediate_relation) %}
+    {{ get_create_view_as_sql(intermediate_relation, sql) }}
+
+    {% if existing_relation is not none %}
+        alter view {{ existing_relation }} rename to {{ backup_relation.include(database=False, schema=False) }};
+    {% endif %}
+
+    alter view {{ intermediate_relation }} rename to {{ relation.include(database=False, schema=False) }};
+
 {% endmacro %}
