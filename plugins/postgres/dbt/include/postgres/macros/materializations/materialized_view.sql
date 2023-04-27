c2e1cae5b5fa92c5af70c31d151fb4aa235c4617
Mike Alfare <mike.alfare@dbtlabs.com>
2023-04-27 19:01:27 -0400

    move the body of the default macros into the postgres implementation, throw errors if the default is used, indicating that materialized views have not been implemented for that adapter

--- plugins/postgres/dbt/include/postgres/macros/materializations/materialized_view.sql
+++ plugins/postgres/dbt/include/postgres/macros/materializations/materialized_view.sql
@@ -1,15 +1,22 @@
-{% macro postgres__get_create_materialized_view_as_sql(relation, sql) %}
-    {{ return(get_create_view_as_sql(relation, sql)) }}
+{% macro default__get_alter_materialized_view_as_sql(
+    relation,
+    configuration_changes,
+    sql,
+    existing_relation,
+    backup_relation,
+    intermediate_relation
+) %}
+    {{- return(get_replace_materialized_view_as_sql(relation, sql, existing_relation, backup_relation, intermediate_relation)) -}}
 {% endmacro %}
 
 
-{% macro postgres__get_refresh_data_in_materialized_view_sql(relation) %}
-    select 1;
+{% macro postgres__get_create_materialized_view_as_sql(relation, sql) %}
+    {{- return(get_create_view_as_sql(relation, sql)) -}}
 {% endmacro %}
 
 
 {% macro postgres__get_replace_materialized_view_as_sql(relation, sql, existing_relation, backup_relation, intermediate_relation) %}
-    {{ get_create_view_as_sql(intermediate_relation, sql) }}
+    {{- get_create_view_as_sql(intermediate_relation, sql) -}}
 
     {% if existing_relation is not none %}
         alter view {{ existing_relation }} rename to {{ backup_relation.include(database=False, schema=False) }};
@@ -17,4 +24,14 @@
 
     alter view {{ intermediate_relation }} rename to {{ relation.include(database=False, schema=False) }};
 
+{% endmacro %}
+
+
+{% macro default__get_materialized_view_configuration_changes(existing_relation, new_config) %}
+    {%- do return([]) -%}
+{% endmacro %}
+
+
+{% macro postgres__refresh_materialized_view(relation) %}
+    {{- '' -}}
 {% endmacro %}
