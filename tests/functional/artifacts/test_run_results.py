dc92aa11cfbb26a64addc3c89649aa624f85897a
Ian Knox <ian.knox@fishtownanalytics.com>
2023-05-08 15:59:23 -0500

    terminate process testing

--- tests/functional/artifacts/test_run_results.py
+++ tests/functional/artifacts/test_run_results.py
@@ -1,3 +1,6 @@
+import multiprocessing
+from pathlib import Path
+from time import sleep
 import pytest
 from dbt.tests.util import run_dbt
 
@@ -7,6 +10,11 @@ select 1 as id
 
 bad_model_sql = """
 something bad
+"""
+
+slow_model_sql = """
+{{ config(materialized='table') }}
+select id from {{ ref('good_model') }}, pg_sleep(5)
 """
 
 
@@ -30,3 +38,31 @@ class TestRunResultsTimingFailure:
         results = run_dbt(["run"], expect_pass=False)
         assert len(results.results) == 1
         assert len(results.results[0].timing) > 0
+
+
+class TestRunResultsWritesFileOnSignal:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"good_model.sql": good_model_sql, "slow_model.sql": slow_model_sql}
+
+    def test_run_results_are_written_on_signal(self, project):
+
+        # N.B. This test is... no great.
+        # Due to the way that multiprocessing handles termination this test
+        # will always take the entire amount of time designated in pg_sleep.
+        # Additionally playing these timing games is probably quite fragile.
+
+        external_process_dbt = multiprocessing.Process(
+            target=run_dbt, args=([["run"]]), kwargs={"expect_pass": False}
+        )
+        external_process_dbt.start()
+        assert external_process_dbt.is_alive()
+        # More than enough time for the first model to complete
+        # but not enough for the second to complete.
+        # A bit janky, I know.
+        sleep(2)
+        external_process_dbt.terminate()
+        while external_process_dbt.is_alive() is True:
+            pass
+        run_results_file = Path(project.project_root) / "target/run_results.json"
+        assert run_results_file.is_file()
