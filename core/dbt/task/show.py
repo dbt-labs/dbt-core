2100885fc1a8710e9db14891362f589825d4d1f3
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2023-05-08 04:54:49 -0700

    use hasattr

--- core/dbt/task/show.py
+++ core/dbt/task/show.py
@@ -77,7 +77,7 @@ class ShowTask(CompileTask):
 
             node_name = result.node.name
 
-            if result.node.version:
+            if hasattr(result.node, "version"):
                 node_name += f".v{result.node.version}"
 
             fire_event(
