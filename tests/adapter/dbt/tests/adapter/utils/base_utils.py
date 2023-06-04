7a1b3959da296725bc3fc7d6ca636d959a8b92ce
Doug Beatty <doug.beatty@dbtlabs.com>
2023-06-04 14:10:22 -0600

    Fix null-safe equals comparison

--- tests/adapter/dbt/tests/adapter/utils/base_utils.py
+++ tests/adapter/dbt/tests/adapter/utils/base_utils.py
@@ -2,9 +2,11 @@ import pytest
 from dbt.tests.util import run_dbt
 
 macros__equals_sql = """
-{% macro equals(actual, expected) %}
-{# -- actual is not distinct from expected #}
-(({{ actual }} = {{ expected }}) or ({{ actual }} is null and {{ expected }} is null))
+{% macro equals(expr1, expr2) -%}
+case when (({{ expr1 }} = {{ expr2 }}) or ({{ expr1 }} is null and {{ expr2 }} is null))
+    then 0
+    else 1
+end = 0
 {% endmacro %}
 """
 
