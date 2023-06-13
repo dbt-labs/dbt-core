1682c35e8380dadf03a241c5b39d8d72f4144d57
Gerda Shank <gerda@dbtlabs.com>
2023-06-13 15:40:46 -0400

    formatting

--- core/dbt/config/project.py
+++ core/dbt/config/project.py
@@ -116,7 +116,7 @@ def package_and_project_data_from_root(project_root):
     dependent_projects_dict = {}
     if "packages" in dependencies_yml_dict:
         packages_dict["packages"] = dependencies_yml_dict["packages"]
-    else:  # don't check for "packages" here so we capture invalid keys in packages.yml 
+    else:  # don't check for "packages" here so we capture invalid keys in packages.yml
         packages_dict = packages_yml_dict
     if "projects" in dependencies_yml_dict:
         dependent_projects_dict["projects"] = dependencies_yml_dict["projects"]
