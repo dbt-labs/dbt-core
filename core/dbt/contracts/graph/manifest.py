4c05555cba923437473e2c478a6091f1720e9d22
Gerda Shank <gerda@dbtlabs.com>
2023-06-13 12:15:57 -0400

    Remove test for project dependencies in manifest. Now untestable.

--- core/dbt/contracts/graph/manifest.py
+++ core/dbt/contracts/graph/manifest.py
@@ -22,7 +22,7 @@ from typing import (
 from typing_extensions import Protocol
 from uuid import UUID
 
-from dbt.contracts.publication import ProjectDependencies, PublicationConfig, PublicModel
+from dbt.contracts.publication import PublicationConfig, PublicModel
 
 from dbt.contracts.graph.nodes import (
     BaseNode,
