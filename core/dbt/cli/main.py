02f1c79ed30b4e7db2f3b7f99c3bab4ad02441c0
Rex Ledesma <rex@elementl.com>
2023-06-22 09:51:11 -0400

    feat: move `--profile` and `--target` to global config

--- core/dbt/cli/main.py
+++ core/dbt/cli/main.py
@@ -142,11 +142,13 @@ class dbtRunner:
 @p.populate_cache
 @p.print
 @p.printer_width
+@p.profile
 @p.quiet
 @p.record_timing_info
 @p.send_anonymous_usage_stats
 @p.single_threaded
 @p.static_parser
+@p.target
 @p.use_colors
 @p.use_colors_file
 @p.use_experimental_parser
