6aada3fe520a6d222f6e1cb5af4bc88f5d05b6ee
Emily Rockman <emily.rockman@dbtlabs.com>
2023-08-11 10:07:46 -0500

    add env vars to tox.ini

--- tests/functional/basic/test_basic.py
+++ tests/functional/basic/test_basic.py
@@ -17,4 +17,4 @@ def test_basic(project):
     results = run_dbt(["run"])
     assert len(results) == 1
     manifest = get_manifest(project.project_root)
-    assert "model.test.my_model" in manifest.nodes
+    assert "you won't find me" in manifest.nodes
