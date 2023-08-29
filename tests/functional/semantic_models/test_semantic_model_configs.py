929348c89f3fe5f7d97541688c385f307905c478
Emily Rockman <emily.rockman@dbtlabs.com>
2023-08-29 16:00:16 -0500

    get project level working

--- tests/functional/semantic_models/test_semantic_model_configs.py
+++ tests/functional/semantic_models/test_semantic_model_configs.py
@@ -52,7 +52,48 @@ class TestDisabledConfigYamlLevelEnabledMetric:
             run_dbt(["parse"])
 
 
-# Test disabling config in dbt_project.yml
+# Test disabling semantic model config but not metric config in dbt_project.yml
+class TestMismatchesConfigProjectLevel:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "people.sql": models_people_sql,
+            "metricflow_time_spine.sql": metricflow_time_spine_sql,
+            "semantic_models.yml": semantic_model_people_yml,
+            "people_metrics.yml": models_people_metrics_yml,
+        }
+
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "semantic-models": {
+                "test": {
+                    "enabled": True,
+                }
+            }
+        }
+
+    def test_project_level(self, project):
+        run_dbt(["parse"])
+        manifest = get_manifest(project.project_root)
+        assert "semantic_model.test.semantic_people" in manifest.semantic_models
+
+        new_enabled_config = {
+            "semantic-models": {
+                "test": {
+                    "enabled": False,
+                }
+            }
+        }
+        update_config_file(new_enabled_config, project.project_root, "dbt_project.yml")
+        with pytest.raises(
+            ParsingError,
+            match="A semantic model having a measure `people` is disabled but was referenced",
+        ):
+            run_dbt(["parse"])
+
+
+# Test disabling semantic model and metric configs in dbt_project.yml
 class TestConfigProjectLevel:
     @pytest.fixture(scope="class")
     def models(self):
@@ -67,10 +108,15 @@ class TestConfigProjectLevel:
     def project_config_update(self):
         return {
             "semantic-models": {
-                "semantic_people": {
+                "test": {
                     "enabled": True,
-                },
-            }
+                }
+            },
+            "metrics": {
+                "test": {
+                    "enabled": True,
+                }
+            },
         }
 
     def test_project_level(self, project):
@@ -81,17 +127,21 @@ class TestConfigProjectLevel:
         new_enabled_config = {
             "semantic-models": {
                 "test": {
-                    "semantic_people": {
-                        "enabled": False,
-                    },
+                    "enabled": False,
                 }
-            }
+            },
+            "metrics": {
+                "test": {
+                    "enabled": False,
+                }
+            },
         }
         update_config_file(new_enabled_config, project.project_root, "dbt_project.yml")
         run_dbt(["parse"])
         manifest = get_manifest(project.project_root)
 
         assert "semantic_model.test.semantic_people" not in manifest.semantic_models
+        assert "semantic_model.test.semantic_people" in manifest.disabled
 
 
 # Test inheritence - set configs at project and semantic_model level - expect semantic_model level to win
