9a9c163011624d15f0777b91c3027d7c4f79c50d
Mike Alfare <mike.alfare@dbtlabs.com>
2023-08-31 18:28:21 -0400

    add interface for applying create or replace

--- core/dbt/include/global_project/macros/relations/materialized_view/replace.sql
+++ core/dbt/include/global_project/macros/relations/materialized_view/replace.sql
@@ -1,9 +1,10 @@
-{% macro get_replace_materialized_view_as_sql(relation, sql) %}
-    {{- log('Applying REPLACE to: ' ~ relation) -}}
-    {{- adapter.dispatch('get_replace_materialized_view_as_sql', 'dbt')(relation, sql) -}}
+{% macro get_replace_materialized_view_sql(relation, sql) %}
+    {{- adapter.dispatch('get_replace_materialized_view_sql', 'dbt')(relation, sql) -}}
 {% endmacro %}
 
 
-{% macro default__get_replace_materialized_view_as_sql(relation, sql) %}
-    {{ exceptions.raise_compiler_error("`get_replace_materialized_view_as_sql` has not been implemented for this adapter.") }}
+{% macro default__get_replace_materialized_view_sql(relation, sql) %}
+    {{ exceptions.raise_compiler_error(
+        "`get_replace_materialized_view_sql` has not been implemented for this adapter."
+    ) }}
 {% endmacro %}
--- core/dbt/include/global_project/macros/relations/view/replace.sql
+++ core/dbt/include/global_project/macros/relations/view/replace.sql
@@ -1,3 +1,15 @@
+{% macro get_replace_view_sql(relation, sql) %}
+    {{- adapter.dispatch('get_replace_view_sql', 'dbt')(relation, sql) -}}
+{% endmacro %}
+
+
+{% macro default__get_replace_view_sql(relation, sql) %}
+    {{ exceptions.raise_compiler_error(
+        "`get_replace_view_sql` has not been implemented for this adapter."
+    ) }}
+{% endmacro %}
+
+
 /* {#
        Core materialization implementation. BigQuery and Snowflake are similar
        because both can use `create or replace view` where the resulting view's columns
