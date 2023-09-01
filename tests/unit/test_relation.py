957a21dd2a30f2d7cd87db626e21074c1fa4ae1a
Mike Alfare <mike.alfare@dbtlabs.com>
2023-08-31 21:55:28 -0400

    fixed tests for new defaults

--- tests/unit/test_relation.py
+++ tests/unit/test_relation.py
@@ -1,3 +1,5 @@
+from dataclasses import replace
+
 import pytest
 
 from dbt.adapters.base import BaseRelation
@@ -13,6 +15,7 @@ from dbt.contracts.relation import RelationType
 )
 def test_can_be_renamed(relation_type, result):
     my_relation = BaseRelation.create(type=relation_type)
+    my_relation = replace(my_relation, renameable_relations=[RelationType.View])
     assert my_relation.can_be_renamed is result
 
 
@@ -25,4 +28,5 @@ def test_can_be_renamed(relation_type, result):
 )
 def test_can_be_replaced(relation_type, result):
     my_relation = BaseRelation.create(type=relation_type)
+    my_relation = replace(my_relation, replaceable_relations=[RelationType.View])
     assert my_relation.can_be_replaced is result
