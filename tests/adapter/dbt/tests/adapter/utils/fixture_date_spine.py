ac54a12b5daf9972fc02a322f995b8fb7b9a60c1
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2023-09-14 14:44:33 -0700

    Improve test fixture for `date_spine` macro to work with multiple adapters

--- tests/adapter/dbt/tests/adapter/utils/fixture_date_spine.py
+++ tests/adapter/dbt/tests/adapter/utils/fixture_date_spine.py
@@ -2,25 +2,74 @@
 
 models__test_date_spine_sql = """
 with generated_dates as (
-    {{ date_spine("day", "'2023-09-01'::date", "'2023-09-10'::date") }}
+    {% if target.type == 'postgres' %}
+        {{ date_spine("day", "'2023-09-01'::date", "'2023-09-10'::date") }}
+
+    {% elif target.type == 'bigquery' %}
+        select cast(date_day as date) as date_day
+        from ({{ date_spine("day", "'2023-09-01'", "'2023-09-10'") }})
+
+    {% else %}
+        {{ date_spine("day", "'2023-09-01'", "'2023-09-10'") }}
+    {% endif %}
 ), expected_dates as (
-    select '2023-09-01'::date as expected
-    union all
-    select '2023-09-02'::date as expected
-    union all
-    select '2023-09-03'::date as expected
-    union all
-    select '2023-09-04'::date as expected
-    union all
-    select '2023-09-05'::date as expected
-    union all
-    select '2023-09-06'::date as expected
-    union all
-    select '2023-09-07'::date as expected
-    union all
-    select '2023-09-08'::date as expected
-    union all
-    select '2023-09-09'::date as expected
+    {% if target.type == 'postgres' %}
+        select '2023-09-01'::date as expected
+        union all
+        select '2023-09-02'::date as expected
+        union all
+        select '2023-09-03'::date as expected
+        union all
+        select '2023-09-04'::date as expected
+        union all
+        select '2023-09-05'::date as expected
+        union all
+        select '2023-09-06'::date as expected
+        union all
+        select '2023-09-07'::date as expected
+        union all
+        select '2023-09-08'::date as expected
+        union all
+        select '2023-09-09'::date as expected
+
+    {% elif target.type == 'bigquery' %}
+        select cast('2023-09-01' as date) as expected
+        union all
+        select cast('2023-09-02' as date) as expected
+        union all
+        select cast('2023-09-03' as date) as expected
+        union all
+        select cast('2023-09-04' as date) as expected
+        union all
+        select cast('2023-09-05' as date) as expected
+        union all
+        select cast('2023-09-06' as date) as expected
+        union all
+        select cast('2023-09-07' as date) as expected
+        union all
+        select cast('2023-09-08' as date) as expected
+        union all
+        select cast('2023-09-09' as date) as expected
+
+    {% else %}
+        select '2023-09-01' as expected
+        union all
+        select '2023-09-02' as expected
+        union all
+        select '2023-09-03' as expected
+        union all
+        select '2023-09-04' as expected
+        union all
+        select '2023-09-05' as expected
+        union all
+        select '2023-09-06' as expected
+        union all
+        select '2023-09-07' as expected
+        union all
+        select '2023-09-08' as expected
+        union all
+        select '2023-09-09' as expected
+    {% endif %}
 ), joined as (
     select
         generated_dates.date_day,
