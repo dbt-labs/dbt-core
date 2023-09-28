a1803237ff0e45ac99c753214a1256d12c39b859
Peter Allen Webb <peter.webb@dbtlabs.com>
2023-09-28 15:58:47 -0400

    Work around missing resource module on windows

--- core/dbt/cli/requires.py
+++ core/dbt/cli/requires.py
@@ -29,7 +29,7 @@ from dbt.plugins import set_up_plugin_manager, get_plugin_manager
 
 from click import Context
 from functools import update_wrapper
-import resource
+import importlib.util
 import time
 import traceback
 
@@ -99,20 +99,25 @@ def postflight(func):
             fire_event(MainStackTrace(stack_trace=traceback.format_exc()))
             raise ExceptionExit(e)
         finally:
-            rusage = resource.getrusage(resource.RUSAGE_SELF)
-            fire_event(
-                ResourceReport(
-                    command_name=ctx.command.name,
-                    command_success=success,
-                    command_wall_clock_time=time.perf_counter() - start_func,
-                    process_user_time=rusage.ru_utime,
-                    process_kernel_time=rusage.ru_stime,
-                    process_mem_max_rss=rusage.ru_maxrss,
-                    process_in_blocks=rusage.ru_inblock,
-                    process_out_blocks=rusage.ru_oublock,
-                ),
-                EventLevel.INFO if ctx.obj["flags"].SHOW_RESOURCE_REPORT else None,
-            )
+            # Fire ResourceReport, but only on systems which support the resource
+            # module. (Skip it on Windows).
+            if importlib.util.find_spec("resource") is not None:
+                import resource
+
+                rusage = resource.getrusage(resource.RUSAGE_SELF)
+                fire_event(
+                    ResourceReport(
+                        command_name=ctx.command.name,
+                        command_success=success,
+                        command_wall_clock_time=time.perf_counter() - start_func,
+                        process_user_time=rusage.ru_utime,
+                        process_kernel_time=rusage.ru_stime,
+                        process_mem_max_rss=rusage.ru_maxrss,
+                        process_in_blocks=rusage.ru_inblock,
+                        process_out_blocks=rusage.ru_oublock,
+                    ),
+                    EventLevel.INFO if ctx.obj["flags"].SHOW_RESOURCE_REPORT else None,
+                )
 
             fire_event(
                 CommandCompleted(
