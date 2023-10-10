c7c4d4a417d819a6b220140c06641c40c9a56d79
Emily Rockman <emily.rockman@dbtlabs.com>
2023-10-10 14:44:36 -0500

    add new test, move fixtures

--- tests/functional/defer_state/test_group_updates.py
+++ tests/functional/defer_state/test_group_updates.py
@@ -8,65 +8,14 @@ from dbt.exceptions import ParsingError
 
 from tests.functional.defer_state.fixtures import (
     seed_csv,
+    model_1_sql,
+    modified_model_1_sql,
+    model_2_sql,
+    modified_model_2_sql,
+    group_schema_yml,
+    group_modified_schema_yml,
+    group_modified_fail_schema_yml,
 )
-
-model_1_sql = """
-select * from {{ ref('seed') }}
-"""
-
-modified_model_1_sql = """
-select * from  {{ ref('seed') }}
-order by 1
-"""
-
-model_2_sql = """
-select id from  {{ ref('model_1') }}
-"""
-
-
-schema_yml = """
-groups:
-  - name: finance
-    owner:
-      email: finance@jaffleshop.com
-
-models:
-  - name: model_1
-    config:
-      group: finance
-  - name: model_2
-    config:
-      group: finance
-"""
-
-
-modified_schema_yml = """
-groups:
-  - name: accounting
-    owner:
-      email: finance@jaffleshop.com
-models:
-  - name: model_1
-    config:
-      group: accounting
-  - name: model_2
-    config:
-      group: accounting
-"""
-
-modified_fail_schema_yml = """
-groups:
-  - name: finance
-    owner:
-      email: finance@jaffleshop.com
-models:
-  - name: model_1
-    config:
-      group: accounting
-  - name: model_2
-    config:
-      group: finance
-"""
 
 
 class GroupSetup:
@@ -75,7 +24,7 @@ class GroupSetup:
         return {
             "model_1.sql": model_1_sql,
             "model_2.sql": model_2_sql,
-            "schema.yml": schema_yml,
+            "schema.yml": group_schema_yml,
         }
 
     @pytest.fixture(scope="class")
@@ -101,7 +50,7 @@ class GroupSetup:
         assert model_2_result.group == "finance"
 
 
-class TestModifiedGroups(GroupSetup):
+class TestFullyModifiedGroups(GroupSetup):
     def test_changed_groups(self, project):
         self.group_setup()
 
@@ -112,7 +61,35 @@ class TestModifiedGroups(GroupSetup):
 
         # update group name, modify model so it gets picked up
         write_file(modified_model_1_sql, "models", "model_1.sql")
-        write_file(modified_schema_yml, "models", "schema.yml")
+        write_file(modified_model_2_sql, "models", "model_2.sql")
+        write_file(group_modified_schema_yml, "models", "schema.yml")
+
+        # this test is flaky if you don't clean first before the build
+        run_dbt(["clean"])
+        # only thing in results should be model_1
+        results = run_dbt(["build", "-s", "state:modified", "--defer", "--state", "./state"])
+
+        assert len(results) == 2
+        model_1_result = results[0].node
+        assert model_1_result.unique_id == "model.test.model_1"
+        assert model_1_result.group == "accounting"  # new group name!
+        model_2_result = results[1].node
+        assert model_2_result.unique_id == "model.test.model_2"
+        assert model_2_result.group == "accounting"  # new group name!
+
+
+class TestPartiallyModifiedGroups(GroupSetup):
+    def test_changed_groups(self, project):
+        self.group_setup()
+
+        # copy manifest.json to "state" directory
+        os.makedirs("state")
+        target_path = os.path.join(project.project_root, "target")
+        copy_file(target_path, "manifest.json", project.project_root, ["state", "manifest.json"])
+
+        # update group name, modify model so it gets picked up
+        write_file(modified_model_1_sql, "models", "model_1.sql")
+        write_file(group_modified_schema_yml, "models", "schema.yml")
 
         # this test is flaky if you don't clean first before the build
         run_dbt(["clean"])
@@ -136,7 +113,7 @@ class TestBadGroups(GroupSetup):
 
         # update group with invalid name, modify model so it gets picked up
         write_file(modified_model_1_sql, "models", "model_1.sql")
-        write_file(modified_fail_schema_yml, "models", "schema.yml")
+        write_file(group_modified_fail_schema_yml, "models", "schema.yml")
 
         # this test is flaky if you don't clean first before the build
         run_dbt(["clean"])
