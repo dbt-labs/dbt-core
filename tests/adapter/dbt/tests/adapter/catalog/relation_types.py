7bb2d650f2800b753b4a795872b581d7f6894517
Mike Alfare <mike.alfare@dbtlabs.com>
2023-11-03 19:50:01 -0400

    add comment explaining how to configure this test case

--- tests/adapter/dbt/tests/adapter/catalog/relation_types.py
+++ tests/adapter/dbt/tests/adapter/catalog/relation_types.py
@@ -6,6 +6,46 @@ from dbt.tests.adapter.catalog import files
 
 
 class CatalogRelationTypes:
+    """
+    Many adapters can use this test as-is. However, if your adapter contains different
+    relation types or uses different strings to describe the node (e.g. 'table' instead of 'BASE TABLE'),
+    then you'll need to configure this test.
+
+    To configure this test, you'll most likely need to update either `models`
+    and/or `test_relation_types_populate_correctly`. For example, `dbt-snowflake`
+    supports dynamic tables and does not support materialized views. It's implementation
+    might look like this:
+
+    class TestCatalogRelationTypes:
+        @pytest.fixture(scope="class", autouse=True)
+        def models(self):
+            yield {
+                "my_table.sql": files.MY_TABLE,
+                "my_view.sql": files.MY_VIEW,
+                "my_dynamic_table.sql": files.MY_DYNAMIC_TABLE,
+            }
+
+        @pytest.mark.parametrize(
+            "node_name,relation_type",
+            [
+                ("seed.test.my_seed", "BASE TABLE"),
+                ("model.test.my_table", "BASE TABLE"),
+                ("model.test.my_view", "VIEW"),
+                ("model.test.my_dynamic_table", "DYNAMIC TABLE"),
+            ],
+        )
+        def test_relation_types_populate_correctly(
+            self, docs: CatalogArtifact, node_name: str, relation_type: str
+        ):
+            super().test_relation_types_populate_correctly(
+                docs, node_name, relation_type
+            )
+
+    Note that we're able to configure the test case using pytest parameterization
+    and call back to the original test. That way any updates to the test are incorporated
+    into your adapter.
+    """
+
     @pytest.fixture(scope="class", autouse=True)
     def seeds(self):
         return {"my_seed.csv": files.MY_SEED}
