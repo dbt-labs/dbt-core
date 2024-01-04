6421959c49298e4233b428b3bca77982b189103b
Michelle Ark <michelle.ark@dbtlabs.com>
2024-01-04 13:32:27 -0500

    add common jinja exceptions

--- core/dbt/common/exceptions/__init__.py
+++ core/dbt/common/exceptions/__init__.py
@@ -3,3 +3,5 @@ from dbt.common.exceptions.events import *  # noqa
 from dbt.common.exceptions.macros import *  # noqa
 from dbt.common.exceptions.contracts import *  # noqa
 from dbt.common.exceptions.connection import *  # noqa
+from dbt.common.exceptions.system import *  # noqa
+from dbt.common.exceptions.jinja import *  # noqa
