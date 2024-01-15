b89c527e0a2f3c363b663278a16bb49fc6ff7b6b
truls-p <20932917+truls-p@users.noreply.github.com>
2024-01-15 09:16:09 +0000

    Change metadata_vars `if not` to `if ... is None`
    Both None and {} evaluates to True when using if not metadata_vars. This caused us to loop over the environment variables unnecessarily. This provides a significant performance increase for large projects.

--- core/dbt/common/events/functions.py
+++ core/dbt/common/events/functions.py
@@ -148,7 +148,7 @@ def fire_event(e: BaseEvent, level: Optional[EventLevel] = None) -> None:
 
 def get_metadata_vars() -> Dict[str, str]:
     global metadata_vars
-    if not metadata_vars:
+    if metadata_vars is None:
         metadata_vars = {
             k[len(_METADATA_ENV_PREFIX) :]: v
             for k, v in os.environ.items()
