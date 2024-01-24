ae2b3626afd12ff0089c8b15d00c3f009bb9a457
Michelle Ark <michelle.ark@dbtlabs.com>
2024-01-24 13:33:05 -0500

    first pass: move Macro to dbt/artifacts

--- core/dbt/artifacts/resources/__init__.py
+++ core/dbt/artifacts/resources/__init__.py
@@ -2,3 +2,5 @@ from dbt.artifacts.resources.base import BaseArtifactNode
 
 # alias to latest resource definitions
 from dbt.artifacts.resources.v1.documentation import Documentation
+from dbt.artifacts.resources.v1.macro import Macro, MacroDependsOn, MacroArgument
+from dbt.artifacts.resources.v1.docs import Docs
