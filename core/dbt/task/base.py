4acf10e0416c5edea52535893a14b6d92386f9ec
Gerda Shank <gerda@dbtlabs.com>
2024-02-12 17:14:31 -0500

    black

--- core/dbt/task/base.py
+++ core/dbt/task/base.py
@@ -336,7 +336,11 @@ class BaseRunner(metaclass=ABCMeta):
         return str(e)
 
     def _handle_internal_exception(self, e, ctx):
-        fire_event(InternalErrorOnRun(build_path=self.node.build_path, exc=str(e), node_info=get_node_info()))
+        fire_event(
+            InternalErrorOnRun(
+                build_path=self.node.build_path, exc=str(e), node_info=get_node_info()
+            )
+        )
         return str(e)
 
     def _handle_generic_exception(self, e, ctx):
