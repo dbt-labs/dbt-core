b14ec651d61f20ae983ac5f1fcbf237e3cfb79ed
Andrew Sweet <andrewmsweet@gmail.com>
2024-03-23 20:23:18 -0400

    Fix none group case

--- core/dbt/graph/selector_methods.py
+++ core/dbt/graph/selector_methods.py
@@ -277,7 +277,8 @@ class GroupSelectorMethod(SelectorMethod):
     def search(self, included_nodes: Set[UniqueId], selector: str) -> Iterator[UniqueId]:
         """yields nodes from included in the specified group"""
         for node, real_node in self.groupable_nodes(included_nodes):
-            if fnmatch(selector, real_node.config.get("group")):
+            node_group = real_node.config.get("group")
+            if node_group and fnmatch(selector, node_group):
                 yield node
 
 
