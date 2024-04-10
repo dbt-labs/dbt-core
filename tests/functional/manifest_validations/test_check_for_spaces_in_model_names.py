b1c584f6f7d7119cfbfdd7e9e10bcdea69695184
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-04-09 18:30:11 -0700

    Refactor `EventCatcher` so that the event to catch is setable
    We want to be able to catch more than just `SpacesInModelNameDeprecation`
    events, and in the next commit we will alter our tests to do so. Thus
    instead of writing a new catcher for each event type, a slight modification
    to the existing `EventCatcher` makes this much easier.

--- tests/functional/manifest_validations/test_check_for_spaces_in_model_names.py
+++ tests/functional/manifest_validations/test_check_for_spaces_in_model_names.py
@@ -2,24 +2,24 @@ import pytest
 
 from dataclasses import dataclass, field
 from dbt.cli.main import dbtRunner
-from dbt_common.events.base_types import EventLevel, EventMsg
+from dbt_common.events.base_types import BaseEvent, EventLevel, EventMsg
 from dbt.events.types import SpacesInModelNameDeprecation
 from typing import Dict, List
 
 
 @dataclass
 class EventCatcher:
+    event_to_catch: BaseEvent
     caught_events: List[EventMsg] = field(default_factory=list)
-    SpacesInModelNameDeprecation.__name__
 
     def catch(self, event: EventMsg):
-        if event.info.name == SpacesInModelNameDeprecation.__name__:
+        if event.info.name == self.event_to_catch.__name__:
             self.caught_events.append(event)
 
 
 class TestSpacesInModelNamesHappyPath:
     def test_no_warnings_when_no_spaces_in_name(self, project) -> None:
-        event_catcher = EventCatcher()
+        event_catcher = EventCatcher(SpacesInModelNameDeprecation)
         runner = dbtRunner(callbacks=[event_catcher.catch])
         runner.invoke(["parse"])
         assert len(event_catcher.caught_events) == 0
@@ -33,7 +33,7 @@ class TestSpacesInModelNamesSadPath:
         }
 
     def tests_warning_when_spaces_in_name(self, project) -> None:
-        event_catcher = EventCatcher()
+        event_catcher = EventCatcher(SpacesInModelNameDeprecation)
         runner = dbtRunner(callbacks=[event_catcher.catch])
         runner.invoke(["parse"])
 
@@ -52,12 +52,12 @@ class TestSpaceInModelNamesWithDebug:
         }
 
     def tests_debug_when_spaces_in_name(self, project) -> None:
-        event_catcher = EventCatcher()
+        event_catcher = EventCatcher(SpacesInModelNameDeprecation)
         runner = dbtRunner(callbacks=[event_catcher.catch])
         runner.invoke(["parse"])
         assert len(event_catcher.caught_events) == 1
 
-        event_catcher = EventCatcher()
+        event_catcher = EventCatcher(SpacesInModelNameDeprecation)
         runner = dbtRunner(callbacks=[event_catcher.catch])
         runner.invoke(["parse", "--debug"])
         assert len(event_catcher.caught_events) == 2
