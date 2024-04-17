342d54b0576df1c060987e4f2424bb2a87ef89de
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2024-04-17 11:56:11 -0500

    remove current attempt of add cache_renamed

--- core/dbt/include/global_project/macros/relations/materialized_view/rename.sql
+++ core/dbt/include/global_project/macros/relations/materialized_view/rename.sql
@@ -1,9 +1,5 @@
 {% macro get_rename_materialized_view_sql(relation, new_name) %}
-    {%- set to_relation = adapter.get_relation(database=relation.database, schema=relation.schema, identifier=new_name) %}
     {{- adapter.dispatch('get_rename_materialized_view_sql', 'dbt')(relation, new_name) -}}
-     {%- if relation is not none and  to_relation is not none -%}
-      {{ adapter.cache_renamed(from_relation=relation, to_relation=to_relation) }}
-    {%- endif -%}
 {% endmacro %}
 
 
