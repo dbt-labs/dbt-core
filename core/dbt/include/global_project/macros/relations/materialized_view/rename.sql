39f88feb9ff977279e6ec82c25c2af9bfa3eb7de
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2024-04-19 14:32:32 -0500

    remove extra curly brace

--- core/dbt/include/global_project/macros/relations/materialized_view/rename.sql
+++ core/dbt/include/global_project/macros/relations/materialized_view/rename.sql
@@ -2,7 +2,7 @@
     {% if relation is not none %}
         {% set database = relation.database %}
         {% set schema = relation.schema %}
-        {% set to_relation = adapter.get_relation(database=database, schema=schema, identifier=new_name) %}}
+        {% set to_relation = adapter.get_relation(database=database, schema=schema, identifier=new_name) %}
         {% if to_relation is not none %}
             {{ adapter.rename_relation(from_relation=relation, to_relation=to_relation) }}
         {% endif %}
