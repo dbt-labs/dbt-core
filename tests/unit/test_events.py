yu-iskw/dbt

Move UserConfig to dbt_project.yml and rename to ProjectFlags (#9289)

tests/unit/test_events.py
@@ -151,6 +151,7 @@ sample_values = [
     core_types.ConfigLogPathDeprecation(deprecated_path=""),
     core_types.ConfigTargetPathDeprecation(deprecated_path=""),
     adapter_types.CollectFreshnessReturnSignature(),
+    core_types.ProjectFlagsMovedDeprecation(),
     # E - DB Adapter ======================
     adapter_types.AdapterEventDebug(),
     adapter_types.AdapterEventInfo(),

dbt-labs/dbt-core

tests/unit/test_events.py
@@ -145,6 +145,7 @@ sample_values = [
     types.ConfigLogPathDeprecation(deprecated_path=""),
     types.ConfigTargetPathDeprecation(deprecated_path=""),
     types.CollectFreshnessReturnSignature(),
+    types.ProjectFlagsMovedDeprecation(),
     types.PackageMaterializationOverrideDeprecation(
         package_name="my_package", materialization_name="view"
     ),