3efd434e80c1d5e16e1319df1283274aabd8908f
Kshitij Aranke <kshitij@aranke.org>
2024-04-26 12:56:23 +0200

    Update core/dbt/graph/selector_methods.py
    Co-authored-by: Jeremy Cohen <jtcohen6@gmail.com>

--- core/dbt/graph/selector_methods.py
+++ core/dbt/graph/selector_methods.py
@@ -437,8 +437,8 @@ class FileSelectorMethod(SelectorMethod):
 class PackageSelectorMethod(SelectorMethod):
     def search(self, included_nodes: Set[UniqueId], selector: str) -> Iterator[UniqueId]:
         """Yields nodes from included that have the specified package"""
-        # `.` is an alias for the current dbt project name
-        if selector == "." and self.manifest.metadata.project_name is not None:
+        # `this` is an alias for the current dbt project name
+        if selector == "this" and self.manifest.metadata.project_name is not None:
             selector = self.manifest.metadata.project_name
 
         for node, real_node in self.all_nodes(included_nodes):
