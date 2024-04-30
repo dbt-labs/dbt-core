42f5fff7fd24c4d77f2f7226c0744f51bfca3dd7
Michelle Ark <michelle.ark@dbtlabs.com>
2024-04-30 17:54:23 -0400

    test minor artifact changes for backward + forward compatability: removing field with default value

--- tests/unit/artifacts/test_base_resource.py
+++ tests/unit/artifacts/test_base_resource.py
@@ -34,12 +34,22 @@ class TestMinorSchemaChange:
             new_field_with_default=False,
         )
 
-    def test_serializing_minor_change_new_default_field_is_backward_compatabile(
+    def test_serializing_new_default_field_is_backward_compatabile(
         self, base_resource_new_default_field
     ):
         # old code (using old class) can create an instance of itself given new data (new class)
         BaseResource.from_dict(base_resource_new_default_field.to_dict())
 
-    def test_serializing_minor_change_new_default_field_is_forward_compatible(self, base_resource):
+    def test_serializing_new_default_field_is_forward_compatible(self, base_resource):
         # new code (using new class) can create an instance of itself given old data (old class)
         BaseResourceWithDefaultField.from_dict(base_resource.to_dict())
+
+    def test_serializing_removed_default_field_is_backward_compatabile(self, base_resource):
+        # old code (using old class with default field) can create an instance of itself given new data (class w/o default field)
+        BaseResourceWithDefaultField.from_dict(base_resource.to_dict())
+
+    def test_serializing_removed_default_field_is_forward_compatible(
+        self, base_resource_new_default_field
+    ):
+        # new code (using class without default field) can create an instance of itself given old data (class with old field)
+        BaseResource.from_dict(base_resource_new_default_field.to_dict())
