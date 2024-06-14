25888cefbb1e435be235f4d1fb864620a5def142
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-06-14 14:08:27 -0700

    Add unit test for `descendants` method of `Graph` class

--- tests/unit/graph/test_graph.py
+++ tests/unit/graph/test_graph.py
@@ -2,6 +2,7 @@ import pytest
 
 from dbt.compilation import Linker
 from dbt.contracts.graph.manifest import Manifest
+from dbt.contracts.graph.nodes import ModelNode
 from dbt.graph.graph import Graph
 
 
@@ -21,3 +22,18 @@ class TestGraph:
         # Assert that it is a set, thus no duplicates
         assert isinstance(graph_nodes, set)
         assert len(graph_nodes) == len(all_manifest_nodes)
+
+    def test_descendantcs(self, graph: Graph, manifest: Manifest) -> None:
+        model: ModelNode = manifest.nodes["model.pkg.ephemeral_model"]
+
+        # check result when not limiting the depth
+        descendants = graph.descendants(node=model.unique_id)
+        assert descendants == {
+            "test.pkg.view_test_nothing",
+            "model.pkg.view_model",
+            "model.pkg.table_model",
+        }
+
+        # check that result excludes nodes that are out of depth
+        descendants = graph.descendants(node=model.unique_id, max_depth=1)
+        assert descendants == {"model.pkg.table_model", "model.pkg.view_model"}
