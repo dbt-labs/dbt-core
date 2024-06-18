da4cff96a3740406471e10b0fb8dc5a62cc285b0
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-06-18 14:20:08 -0700

    Add unit test for `select_childrens_parents` method of `Graph` class

--- tests/unit/graph/test_graph.py
+++ tests/unit/graph/test_graph.py
@@ -4,6 +4,7 @@ from dbt.compilation import Linker
 from dbt.contracts.graph.manifest import Manifest
 from dbt.contracts.graph.nodes import ModelNode
 from dbt.graph.graph import Graph
+from tests.unit.utils.manifest import make_model
 
 
 class TestGraph:
@@ -12,6 +13,21 @@ class TestGraph:
         linker = Linker()
         linker.link_graph(manifest=manifest)
         return Graph(graph=linker.graph)
+
+    @pytest.fixture
+    def extra_parent_model(self) -> ModelNode:
+        return make_model(pkg="pkg", name="extra_parent_model", code="SELECT 'cats' as interests")
+
+    @pytest.fixture
+    def model_with_two_direct_parents(
+        self, extra_parent_model: ModelNode, ephemeral_model: ModelNode
+    ) -> ModelNode:
+        return make_model(
+            pkg="pkg",
+            name="model_with_two_direct_parents",
+            code='SELECT * FROM {{ ref("ephemeral_model") }} UNION ALL SELECT * FROM {{ ref("extra_parent_model") }}',
+            refs=[extra_parent_model, ephemeral_model],
+        )
 
     def test_nodes(self, graph: Graph, manifest: Manifest):
         graph_nodes = graph.nodes()
@@ -61,3 +77,36 @@ class TestGraph:
         # linker.add_test_edges(manifest=manifest)
         # graph = Graph(graph=linker.graph)
         pass
+
+    def test_select_childrens_parents(
+        self,
+        model_with_two_direct_parents: ModelNode,
+        extra_parent_model: ModelNode,
+        manifest: Manifest,
+        ephemeral_model: ModelNode,
+    ) -> None:
+        # add extra nodes to manifest
+        manifest.add_node_nofile(extra_parent_model)
+        manifest.add_node_nofile(model_with_two_direct_parents)
+
+        # create graph (we don't use the fixture because we want our extra nodes)
+        linker = Linker()
+        linker.link_graph(manifest=manifest)
+        graph = Graph(graph=linker.graph)
+
+        # the "selected" node we care about
+        model: ModelNode = manifest.nodes["model.pkg.extra_parent_model"]
+
+        # `select_childrens_parents` should return
+        # * all children of the selected node (without depth limit)
+        # * all parents of the children of the selected node (without depth limit)
+        childrens_parents = graph.select_childrens_parents(
+            selected={
+                model.unique_id,
+            }
+        )
+
+        assert model_with_two_direct_parents.unique_id in childrens_parents
+        assert extra_parent_model.unique_id in childrens_parents
+        assert ephemeral_model.unique_id in childrens_parents
+        assert len(childrens_parents) == 4
