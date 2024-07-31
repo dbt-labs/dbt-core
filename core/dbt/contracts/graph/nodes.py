91fbe7f1285b69a0eeaf53f04753db001ef07b75
Michelle Ark <michelle.ark@dbtlabs.com>
2024-07-30 20:41:30 -0400

    TestModifiedVarsSchemaYml + support for modified.vars in schema.yml files

--- core/dbt/contracts/graph/nodes.py
+++ core/dbt/contracts/graph/nodes.py
@@ -425,9 +425,6 @@ class CompiledNode(CompiledResource, ParsedNode):
     def depends_on_macros(self):
         return self.depends_on.macros
 
-    def same_contents(self, old, adapter_type) -> bool:
-        return super().same_contents(old, adapter_type)
-
 
 # ====================================
 # CompiledNode subclasses
@@ -1258,6 +1255,9 @@ class SourceDefinition(
             old.unrendered_config,
         )
 
+    def same_vars(self, other: "SourceDefinition") -> bool:
+        return self.vars == other.vars
+
     def same_contents(self, old: Optional["SourceDefinition"]) -> bool:
         # existing when it didn't before is a change!
         if old is None:
@@ -1278,6 +1278,7 @@ class SourceDefinition(
             and self.same_quoting(old)
             and self.same_freshness(old)
             and self.same_external(old)
+            and self.same_vars(old)
             and True
         )
 
@@ -1374,6 +1375,9 @@ class Exposure(GraphNode, ExposureResource):
             old.unrendered_config,
         )
 
+    def same_vars(self, old: "Exposure") -> bool:
+        return self.vars == old.vars
+
     def same_contents(self, old: Optional["Exposure"]) -> bool:
         # existing when it didn't before is a change!
         # metadata/tags changes are not "changes"
@@ -1390,6 +1394,7 @@ class Exposure(GraphNode, ExposureResource):
             and self.same_label(old)
             and self.same_depends_on(old)
             and self.same_config(old)
+            and self.same_vars(old)
             and True
         )
 
@@ -1641,6 +1646,7 @@ class ParsedNodePatch(ParsedPatch):
     latest_version: Optional[NodeVersion]
     constraints: List[Dict[str, Any]]
     deprecation_date: Optional[datetime]
+    vars: Dict[str, Any]
     time_spine: Optional[TimeSpine] = None
 
 
