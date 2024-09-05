57b1353daf8ed66c320332418df96f9fb3e06a5b
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-09-05 10:03:26 -0500

    Add unit test for resolve_event_time_filter

--- tests/unit/context/test_providers.py
+++ tests/unit/context/test_providers.py
@@ -1,11 +1,18 @@
+from datetime import datetime
+from typing import Optional
 from unittest import mock
 
 import pytest
+import pytz
+from freezegun import freeze_time
+from pytest_mock import MockerFixture
 
 from dbt.adapters.base import BaseRelation
-from dbt.artifacts.resources import Quoting
+from dbt.artifacts.resources import NodeConfig, Quoting
+from dbt.artifacts.resources.types import PartitionGrain
 from dbt.context.providers import (
     BaseResolver,
+    EventTimeFilter,
     RuntimeRefResolver,
     RuntimeSourceResolver,
 )
@@ -33,6 +40,133 @@ class TestBaseResolver:
         resolver.config.args.EMPTY = empty
 
         assert resolver.resolve_limit == expected_resolve_limit
+
+    @freeze_time("2024-09-05 08:56:00")
+    @pytest.mark.parametrize(
+        "is_incremental,materialized,incremental_strategy,event_time_end,event_time_start,expected_filter",
+        [
+            (True, "table", "microbatch", None, None, None),
+            (True, "incremental", "merge", None, None, None),
+            (
+                True,
+                "incremental",
+                "microbatch",
+                None,
+                None,
+                EventTimeFilter(
+                    field_name="created_at",
+                    end=datetime(2024, 9, 5, 8, 56, 0, 0, pytz.UTC),
+                    start=datetime(2024, 9, 5, 0, 0, 0, 0, pytz.UTC),
+                ),
+            ),
+            (
+                True,
+                "incremental",
+                "microbatch",
+                "2024-08-01 08:11:00",
+                None,
+                EventTimeFilter(
+                    field_name="created_at",
+                    end=datetime(2024, 8, 1, 8, 11, 0, 0, pytz.UTC),
+                    start=datetime(2024, 8, 1, 0, 0, 0, 0, pytz.UTC),
+                ),
+            ),
+            (
+                True,
+                "incremental",
+                "microbatch",
+                None,
+                "2024-08-01 00:00:00",
+                EventTimeFilter(
+                    field_name="created_at",
+                    end=datetime(2024, 9, 5, 8, 56, 0, 0, pytz.UTC),
+                    start=datetime(2024, 8, 1, 0, 0, 0, 0, pytz.UTC),
+                ),
+            ),
+            (
+                True,
+                "incremental",
+                "microbatch",
+                "2024-09-01 00:00:00",
+                "2024-08-01 00:00:00",
+                EventTimeFilter(
+                    field_name="created_at",
+                    end=datetime(2024, 9, 1, 0, 0, 0, 0, pytz.UTC),
+                    start=datetime(2024, 8, 1, 0, 0, 0, 0, pytz.UTC),
+                ),
+            ),
+            (False, "incremental", "microbatch", None, None, None),
+            (
+                False,
+                "incremental",
+                "microbatch",
+                "2024-08-01 08:11:00",
+                None,
+                EventTimeFilter(
+                    field_name="created_at",
+                    end=datetime(2024, 8, 1, 8, 11, 0, 0, pytz.UTC),
+                    start=None,
+                ),
+            ),
+            (
+                False,
+                "incremental",
+                "microbatch",
+                None,
+                "2024-08-01 00:00:00",
+                EventTimeFilter(
+                    field_name="created_at",
+                    end=None,
+                    start=datetime(2024, 8, 1, 0, 0, 0, 0, pytz.UTC),
+                ),
+            ),
+            (
+                False,
+                "incremental",
+                "microbatch",
+                "2024-09-01 00:00:00",
+                "2024-08-01 00:00:00",
+                EventTimeFilter(
+                    field_name="created_at",
+                    end=datetime(2024, 9, 1, 0, 0, 0, 0, pytz.UTC),
+                    start=datetime(2024, 8, 1, 0, 0, 0, 0, pytz.UTC),
+                ),
+            ),
+        ],
+    )
+    def test_resolve_event_time_filter(
+        self,
+        mocker: MockerFixture,
+        resolver: ResolverSubclass,
+        is_incremental: bool,
+        materialized: str,
+        incremental_strategy: str,
+        event_time_end: Optional[str],
+        event_time_start: Optional[str],
+        expected_filter: Optional[EventTimeFilter],
+    ) -> None:
+        mocker.patch("dbt.context.providers.BaseResolver._is_incremental").return_value = (
+            is_incremental
+        )
+        target = mock.Mock()
+        target.config = mock.MagicMock(NodeConfig)
+        target.config.event_time = "created_at"
+        resolver.model.config = mock.MagicMock(NodeConfig)
+        resolver.model.config.materialized = materialized
+        resolver.model.config.incremental_strategy = incremental_strategy
+        resolver.model.config.batch_size = PartitionGrain.day
+        resolver.model.config.lookback = 0
+        resolver.config.args.EVENT_TIME_END = event_time_end
+        resolver.config.args.EVENT_TIME_START = event_time_start
+        event_time_filter = resolver.resolve_event_time_filter(target=target)
+
+        if expected_filter is not None:
+            assert event_time_filter is not None
+            assert event_time_filter.field_name == expected_filter.field_name
+            assert event_time_filter.end == expected_filter.end
+            assert event_time_filter.start == expected_filter.start
+        else:
+            assert event_time_filter is None
 
 
 class TestRuntimeRefResolver:
