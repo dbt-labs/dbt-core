5d1b9804ff05af415979bb21c8a80a9a72ccea37
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2024-09-27 19:49:24 +0100

    [Round 2] Fix #9005: Allow singular tests to be documented in properties.yml

--- core/dbt/contracts/graph/nodes.py
+++ core/dbt/contracts/graph/nodes.py
@@ -1642,6 +1642,11 @@ class ParsedMacroPatch(ParsedPatch):
     arguments: List[MacroArgument] = field(default_factory=list)
 
 
+@dataclass
+class ParsedSingularTestPatch(ParsedPatch):
+    pass
+
+
 # ====================================
 # Node unions/categories
 # ====================================
