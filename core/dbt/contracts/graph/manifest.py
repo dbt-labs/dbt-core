a6209191ffe9d539b157a0a15b656e73d3f3fc70
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-11-04 18:21:03 -0600

    Add function to that determines whether the new microbatch functionality should be used
    The new microbatch functionality is, unfortunately, potentially dangerous. That is
    it adds a new materalization strategy `microbatch` which an end user could have
    defined as a custom strategy previously. Additionally we added config keys to nodes,
    and as `config` is just a Dict[str, Any], it could contain anything, thus meaning
    people could already be using the configs we're adding for different purposes. Thus
    we need some intellegent gating. Specifically something that adheres to the following:
    
    cms = Custom Microbatch Strategy
    abms = Adapter Builtin Microbatch Strategy
    bf = Behavior flag
    umb = Use Microbatch Batching
    t/f/e = True/False/Error
    
    | cms | abms | bf | umb |
    | t   | t    | t  | t   |
    | f   | t    | t  | t   |
    | t   | f    | t  | t   |
    | f   | f    | t  | e   |
    | t   | t    | f  | f   |
    | f   | t    | f  | t   |
    | t   | f    | f  | f   |
    | f   | f    | f  | e   |
    
    (The above table assumes that there is a microbatch model present in the project)
    
    In order to achieve this we need to check that either the microbatch behavior
    flag is set to true OR microbatch materializaion being used is the _root_ microbatch
    materialization (i.e. not custom). The function we added in this commit,
    `use_microbatch_batches`, does just that.

--- core/dbt/contracts/graph/manifest.py
+++ core/dbt/contracts/graph/manifest.py
@@ -1755,6 +1755,20 @@ class Manifest(MacroMethods, dbtClassMixin):
         )
         return self.__class__, args
 
+    def _microbatch_macro_is_root(self, project_name: str, adapter_type: str) -> bool:
+        microbatch_is_root = False
+        candidate = self.find_materialization_macro_candidate_by_name(
+            project_name, "microbatch", adapter_type
+        )
+        if candidate is not None and candidate.locality == Locality.Root:
+            microbatch_is_root = True
+        return microbatch_is_root
+
+    def use_microbatch_batches(self, project_name: str, adapter_type: str) -> bool:
+        return get_flags().require_builtin_microbatch_strategy or self._microbatch_macro_is_root(
+            project_name=project_name, adapter_type=adapter_type
+        )
+
 
 class MacroManifest(MacroMethods):
     def __init__(self, macros) -> None:
