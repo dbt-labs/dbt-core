4588e2324a274e098a4d63d68c15798072e796fb
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-11-27 11:27:58 -0600

    Stop testing for batchless compiled code for microbatch models
    In 45daec72f4953be4c157e6e2ab5671455a569396 we stopped an extra compilation
    that was happening per batch prior to the batch_context being loaded. Stopping
    this extra compilation means that compiled sql for the microbatch model without
    the event time filter / batch context is no longer produced. We have discussed
    this and _believe_ it is okay given that this is a new node type that has not
    hit GA yet.

--- tests/functional/microbatch/test_microbatch.py
+++ tests/functional/microbatch/test_microbatch.py
@@ -693,16 +693,6 @@ class TestMicrobatchCompiledRunPaths(BaseMicrobatchTest):
         with patch_microbatch_end_time("2020-01-03 13:57:00"):
             run_dbt(["run"])
 
-        # Compiled paths - compiled model without filter only
-        assert read_file(
-            project.project_root,
-            "target",
-            "compiled",
-            "test",
-            "models",
-            "microbatch_model.sql",
-        )
-
         # Compiled paths - batch compilations
         assert read_file(
             project.project_root,
