b6b56a9ada46e4d5a26eaa97fb5762b07c920bcf
Chenyu Li <chenyu.li@dbtlabs.com>
2024-12-18 17:06:16 -0800

    add model freshness for adaptive job

--- core/dbt/artifacts/resources/v1/model.py
+++ core/dbt/artifacts/resources/v1/model.py
@@ -1,12 +1,14 @@
+import enum
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Dict, List, Literal, Optional
 
-from dbt.artifacts.resources.types import AccessType, NodeType
+from dbt.artifacts.resources.types import AccessType, NodeType, TimePeriod
 from dbt.artifacts.resources.v1.components import (
     CompiledResource,
     DeferRelation,
     NodeVersion,
+    Time,
 )
 from dbt.artifacts.resources.v1.config import NodeConfig
 from dbt_common.contracts.config.base import MergeBehavior
@@ -34,6 +36,17 @@ class TimeSpine(dbtClassMixin):
     custom_granularities: List[CustomGranularity] = field(default_factory=list)
 
 
+class ModelFreshnessDependsOnOptions(enum.StrEnum):
+    all = "all"
+    any = "any"
+
+
+@dataclass
+class ModelFreshness(dbtClassMixin):
+    depends_on: ModelFreshnessDependsOnOptions = ModelFreshnessDependsOnOptions.any
+    build_after: Time = field(default_factory=lambda: Time(period=TimePeriod.hour, count=0))
+
+
 @dataclass
 class Model(CompiledResource):
     resource_type: Literal[NodeType.Model]
@@ -46,6 +59,7 @@ class Model(CompiledResource):
     defer_relation: Optional[DeferRelation] = None
     primary_key: List[str] = field(default_factory=list)
     time_spine: Optional[TimeSpine] = None
+    freshness: Optional[ModelFreshness] = None
 
     def __post_serialize__(self, dct: Dict, context: Optional[Dict] = None):
         dct = super().__post_serialize__(dct, context)
