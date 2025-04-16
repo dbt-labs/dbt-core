f93c647d651ef5d83474ec5db76d74ab64fbf607
Quigley Malcolm <QMalcolm@users.noreply.github.com>
2025-04-16 10:02:40 -0700

    Begin basic jsonschema validations of dbt_project.yml (#11505)
    * Add jsonschema for validation project file
    
    * Add utility for helping to load jsonschema resources
    
    Currently things are a bit hard coded. We should probably alter this
    to be a bit more flexible.
    
    * Begin validating the the `dbt_project.yml` via jsonschema
    
    * Begin emitting deprecation warnings for generic jsonschema violations in dbt_project.yml
    
    * Move from `DbtInternalError` to `DbtRuntimeError` to avoid circular imports
    
    * Add tests for basic jsonschema validation of `dbt_project.yml`
    
    * Add changie doc
    
    * Add seralization test for new deprecation events
    
    * Alter the project jsonschema to not require things that are optional
    
    * Add datafiles to package egg
    
    * Update inclusion of project jsonschema in setup.py to get files correctly
    
    Using the glob spec returns a list of found files. Our previous spec was
    raising the error
    `error: can't copy 'dbt/resources/input_schemas/project/*.json': doesn't exist or not a regular file`
    
    * Try another approach of adding jsonschema to egg
    
    * Add input_schemas dir to MANIFEST.in spec
    
    * Drop jsonschema inclusion spec from setup.py
    
    * Begin using importlib.resources.files for loading project jsonschema
    
    This doesn't currently work with editable installs :'(
    
    * Use relative paths for loading jsonchemas instead of importlib
    
    Using "importlib" is the blessed way to do this sort of thing. However,
    that is failing for us on editable installs. This commit switches us
    to using relative paths. Technically doing this has edge cases, however
    this is also what we do for the `start_project` used in `dbt init`. So
    we're going to do the same, for now. We should revisit this soon.
    
    * Drop requirment of `__additional_properties__` specified by project jsonschema
    
    * Drop requirement for `pre-hook` and `post-hook` specified by project jsonschema
    
    * Reset `active_deprecations` global at the end of tests using `project` fixture

---
+++ core/dbt/jsonschemas.py
@@ -0,0 +1,17 @@
+import json
+from pathlib import Path
+from typing import Any, Dict
+
+from dbt.include.jsonschemas import JSONSCHEMAS_PATH
+
+
+def load_json_from_package(jsonschema_type: str, filename: str) -> Dict[str, Any]:
+    """Loads a JSON file from within a package."""
+
+    path = Path(JSONSCHEMAS_PATH).joinpath(jsonschema_type, filename)
+    data = path.read_bytes()
+    return json.loads(data)
+
+
+def project_schema() -> Dict[str, Any]:
+    return load_json_from_package(jsonschema_type="project", filename="0.0.85.json")
