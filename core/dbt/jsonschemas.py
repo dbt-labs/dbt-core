4cd514160d2ff5ba611b6a91925170a7679d7079
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-04-17 11:47:03 -0700

    Begin checking schema files for custom keys found in configs

--- core/dbt/jsonschemas.py
+++ core/dbt/jsonschemas.py
@@ -61,12 +61,24 @@ def jsonschema_validate(schema: Dict[str, Any], json: Dict[str, Any], file_path:
     errors: Iterator[ValidationError] = validator.iter_errors(json)  # get all validation errors
 
     for error in errors:
+
         if error.validator == "additionalProperties" and len(error.path) == 0:
             key = re.search(r"'\S+'", error.message)
             deprecations.warn(
                 "custom-top-level-key-deprecation",
                 msg="Unexpected top-level key" + (" " + key.group() if key else ""),
                 file=file_path,
+            )
+        elif (
+            error.validator == "anyOf"
+            and (list(error.path))[-1] == "config"
+            and isinstance(error.instance, dict)
+        ):
+            deprecations.warn(
+                "custom-key-in-config-deprecation",
+                key=(list(error.instance.keys()))[0],
+                file=file_path,
+                key_path=error_path_to_string(error),
             )
         else:
             deprecations.warn(
