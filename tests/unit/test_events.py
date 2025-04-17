a3243cd927e9eb104716eb741ec23397d314d15f
Quigley Malcolm <QMalcolm@users.noreply.github.com>
2025-04-16 17:16:00 -0700

    Add `UnexpectedJinjaBlockDeprecationSummary` and add file context to `UnexpectedJinjaBlockDeprecation` (#11517)
    * Add summary event for UnexpectedJinjaBlockDeprecation
    
    * Begin including file information in UnexpectedJinjaBlockDeprecation event
    
    * Add UnexpectedJinjaBlockDeprecationSummary to test_events.py

--- tests/unit/test_events.py
+++ tests/unit/test_events.py
@@ -162,7 +162,8 @@ sample_values = [
     core_types.MicrobatchMacroOutsideOfBatchesDeprecation(),
     core_types.GenericJSONSchemaValidationDeprecation(violation="", key_path="", file=""),
     core_types.GenericJSONSchemaValidationDeprecationSummary(occurrences=2, show_debug_hint=True),
-    core_types.UnexpectedJinjaBlockDeprecation(msg=""),
+    core_types.UnexpectedJinjaBlockDeprecation(msg="", file=""),
+    core_types.UnexpectedJinjaBlockDeprecationSummary(occurrences=2, show_debug_hint=True),
     # E - DB Adapter ======================
     adapter_types.AdapterEventDebug(),
     adapter_types.AdapterEventInfo(),
