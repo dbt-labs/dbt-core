604b4738f2449b9cf5ac4c0fabcf9e76a6ad52ab
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-04-16 21:01:03 -0700

    Begin validating `dbt_project.yml` for duplicate key violations

--- core/dbt/config/project.py
+++ core/dbt/config/project.py
@@ -8,7 +8,7 @@ from typing_extensions import Protocol, runtime_checkable
 
 from dbt import deprecations
 from dbt.adapters.contracts.connection import QueryComment
-from dbt.clients.yaml_helper import load_yaml_text
+from dbt.clients.yaml_helper import checked_load, load_yaml_text
 from dbt.config.selectors import SelectorDict
 from dbt.config.utils import normalize_warn_error_options
 from dbt.constants import (
@@ -87,9 +87,25 @@ class IsFQNResource(Protocol):
     package_name: str
 
 
-def _load_yaml(path):
+def _load_yaml(path, validate: bool = False):
     contents = load_file_contents(path)
-    return load_yaml_text(contents)
+    if validate:
+        result, failures = checked_load(contents)
+
+        # Fire deprecation warnings for each failure
+        if failures:
+            for failure in failures:
+                if failure.failure_type == "duplicate_key":
+                    deprecations.warn(
+                        "duplicate-yaml-keys-deprecation",
+                        duplicate_description=failure.message,
+                        file=path,
+                    )
+
+        return result
+
+    else:
+        return load_yaml_text(contents)
 
 
 def load_yml_dict(file_path):
@@ -195,7 +211,7 @@ def load_raw_project(project_root: str, validate: bool = False) -> Dict[str, Any
             )
         )
 
-    project_dict = _load_yaml(project_yaml_filepath)
+    project_dict = _load_yaml(project_yaml_filepath, validate=validate)
 
     if validate:
         jsonschema_validate(
