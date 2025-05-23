537f52523c4c48d53bc955889887897c57cd3ca7
Jeremy Cohen <jeremy@dbtlabs.com>
2025-05-23 15:35:31 +0200

    Rm 'name' from written lock file

--- core/dbt/task/deps.py
+++ core/dbt/task/deps.py
@@ -191,7 +191,8 @@ class DepsTask(BaseTask):
         renderer = PackageRenderer(self.cli_vars)
         for package in resolved_deps:
             package_dict = package.to_dict()
-            package_dict["name"] = package.get_project_name(self.project, renderer)
+            # Do not add 'name' yet, this will be added in the next version (v1.10)
+            # package_dict["name"] = package.get_project_name(self.project, renderer)
             packages_installed["packages"].append(package_dict)
 
         packages_installed[PACKAGE_LOCK_HASH_KEY] = _create_sha1_hash(
