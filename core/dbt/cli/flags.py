28f127b15af18a556745bb83ac0b4f18a5cc9ef0
Michelle Ark <michelle.ark@dbtlabs.com>
2025-06-11 16:02:24 -0400

    changelog entry

--- core/dbt/cli/flags.py
+++ core/dbt/cli/flags.py
@@ -399,7 +399,7 @@ class Flags:
         # Handle firing deprecations of CLI aliases separately using argv
         # because click because it difficult to disambiguite which CLI option was used
         # and only preserves the 'canonical' representation.
-        for deprecated_flags, warning in DEPRECATED_FLAGS_TO_WARNINGS.item():
+        for deprecated_flags, warning in DEPRECATED_FLAGS_TO_WARNINGS.items():
             for deprecated_flag in deprecated_flags:
                 if deprecated_flag in sys.argv:
                     warn(warning)
