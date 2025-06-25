088e7c0dc3bf8c305f44bf76a99df6ce3d527f94
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-06-25 15:17:37 -0500

    Add tests for validate_model_config

--- tests/unit/test_jsonschemas.py
+++ tests/unit/test_jsonschemas.py
@@ -27,7 +27,15 @@ class TestValidateModelConfig:
     @mock.patch.dict(os.environ, {"DBT_ENV_PRIVATE_RUN_JSONSCHEMA_VALIDATIONS": "True"})
     def test_validate_model_config_error(self, caught_events: List):
         config = {
-            "non_existent_config": True,
+            "non_existent_config": True,  # this config key doesn't exist
+            "docs": {
+                "show": True,  # this is a valid config key
+                "color": "red",  # this is an invalid config key, as it should be `node_color`
+            },
         }
         validate_model_config(config, "test.yml")
-        assert len(caught_events) == 1
+        assert len(caught_events) == 2
+        assert caught_events[0].info.name == "CustomKeyInObjectDeprecation"
+        assert caught_events[0].data.key == "color"
+        assert caught_events[1].info.name == "CustomKeyInConfigDeprecation"
+        assert caught_events[1].data.key == "non_existent_config"
