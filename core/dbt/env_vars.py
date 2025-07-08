6abed0232d90873086d4e67edbb2b0807e8967fc
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-07-08 13:01:08 -0500

    Add some extra engine env vars found throughout the project to the known list

--- core/dbt/env_vars.py
+++ core/dbt/env_vars.py
@@ -1,10 +1,22 @@
 import os
 from dataclasses import dataclass
-from typing import Optional
+from typing import List, Optional
 
 from dbt.cli import params
 from dbt.deprecations import warn
 from dbt_common.constants import ENGINE_ENV_PREFIX
+
+# These are env vars that are not in the params module, but are still allowed to be set.
+# TODO: Should at least some of these become (undocumented) cli param options?
+_ADDITIONAL_ENGINE_ENV_VARS: List[str] = [
+    "DBT_INVOCATION_ENV",
+    "DBT_RECORDED_FILE_PATH",
+    "DBT_TEST_STATE_MODIFIED",  # TODO: This is testing related, should we do this differently?
+    "DBT_PACKAGE_HUB_URL",
+    "DBT_DOWNLOAD_DIR",
+    "DBT_PP_FILE_DIFF_TEST",  # TODO: This is testing related, should we do this differently?
+    "DBT_PP_TEST",  # TODO: This is testing related, should we do this differently?
+]
 
 
 @dataclass(frozen=True)
@@ -24,9 +36,11 @@ def _create_engine_env_var(name: str) -> EngineEnvVar:
         )
 
 
-# This will be needed later in the flags module to cross propagate old non-engine prefixed to engine prefixed env vars (and vice versa)
+# Here we are creating a set of all known engine env vars. This is used in this moduleto create an allow list of dbt
+# engine env vars.
 KNOWN_ENGINE_ENV_VARS: set[EngineEnvVar] = {
-    _create_engine_env_var(envvar) for envvar in params.KNOWN_ENV_VARS
+    _create_engine_env_var(envvar)
+    for envvar in [*params.KNOWN_ENV_VARS, *_ADDITIONAL_ENGINE_ENV_VARS]
 }
 _ALLOWED_ENV_VARS: set[str] = {envvar.name for envvar in KNOWN_ENGINE_ENV_VARS}
 
