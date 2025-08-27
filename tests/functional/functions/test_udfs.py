d30ee9d10ac7d9e8b20d8cb3996ddde0198e0e03
Quigley Malcolm <QMalcolm@users.noreply.github.com>
2025-08-27 14:10:13 -0500

    Allow for the defining of basic SQL UDFs (#11957)
    * Add initial definiton of the `Function` resource
    
    * Add FunctionNode definition to graph contracts
    
    * Add test which checks whether basic UDFs can be parsed
    
    This test fails right now, which is intentional. This is test driven
    development. Now I do work to maket the test pass :)
    
    * Add basic function sql parser for UDFs, and plumb it through parsing code paths
    
    * Begin populating `functions` in the ref lookup
    
    * Begin patching `function` nodes with their yaml definitions
    
    Of note, presently `arguments` and `return_type` aren't populating properly.
    It's likely that we'll have to do additional work to the FunctionPatchParser
    to get this _fully_ working.
    
    * Increase responsibility of FunctionPatchParser to handle entire `parse_patch` of function nodes
    
    * Fix testing suite to accomodate addition of new `function` node
    
    * Add changie doc for new `function` node type
    
    * Minor refactoring of `NodePatchParser.parse_patch` to reduce code duplication in `FunctionPatchParser`

---
+++ tests/functional/functions/test_udfs.py
@@ -0,0 +1,48 @@
+from typing import Dict
+
+import pytest
+
+from dbt.artifacts.resources import FunctionReturnType
+from dbt.contracts.graph.nodes import FunctionNode
+from dbt.tests.util import run_dbt
+
+area_of_circle_sql = """
+pi() * radius * radius
+"""
+
+area_of_circle_yml = """
+functions:
+  - name: area_of_circle
+    description: Calculates the area of a circle for a given radius
+    arguments:
+      - name: radius
+        type: float
+        description: A floating point number representing the radius of the circle
+    return_type:
+      type: float
+"""
+
+
+class TestBasicSQLUDF:
+    @pytest.fixture(scope="class")
+    def functions(self) -> Dict[str, str]:
+        return {
+            "area_of_circle.sql": area_of_circle_sql,
+            "area_of_circle.yml": area_of_circle_yml,
+        }
+
+    def test_basic_sql_udf_parsing(self, project):
+        manifest = run_dbt(["parse"])
+        assert len(manifest.nodes) == 1
+        assert "function.test.area_of_circle" in manifest.nodes
+        function_node = manifest.nodes["function.test.area_of_circle"]
+        assert isinstance(function_node, FunctionNode)
+        assert function_node.description == "Calculates the area of a circle for a given radius"
+        assert len(function_node.arguments) == 1
+        argument = function_node.arguments[0]
+        assert argument.name == "radius"
+        assert argument.type == "float"
+        assert (
+            argument.description == "A floating point number representing the radius of the circle"
+        )
+        assert function_node.return_type == FunctionReturnType(type="float")
