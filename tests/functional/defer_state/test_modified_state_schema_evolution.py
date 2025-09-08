37517ec470960694fa813a498fda515fdc453848
MichelleArk <michelle.ark@dbtlabs.com>
2025-09-08 15:54:03 -0400

    introduce TestModifiedStateSchemaEvolution, make state_modified_compare_more_unrendered_values safer

---
+++ tests/functional/defer_state/test_modified_state_schema_evolution.py
@@ -0,0 +1,32 @@
+import json
+import os
+import shutil
+
+from dbt.tests.util import run_dbt
+
+
+class TestModifiedStateSchemaEvolution:
+    def update_state(self, test_data_dir):
+        run_dbt(["parse"])
+
+        shutil.copyfile("target/manifest.json", os.path.join(test_data_dir, "manifest.json"))
+
+        # Set dbt_version to PREVIOUS to trigger manifest upgrades on state:modified
+        state_manifest_path = os.path.join(test_data_dir, "manifest.json")
+        with open(state_manifest_path, "r") as f:
+            manifest = json.load(f)
+            manifest["metadata"]["dbt_version"] = "PREVIOUS"
+
+        with open(state_manifest_path, "w") as f:
+            json.dump(manifest, f, indent=2)
+
+    def test_modified_state_schema_evolution(self, happy_path_project):
+        # Uncomment this line when happy_path_project is updated
+        # self.update_state(happy_path_project.test_data_dir)
+
+        results = run_dbt(
+            ["ls", "--select", "state:modified", "--state", happy_path_project.test_data_dir]
+        )
+
+        # No false positives when no project changes are made
+        assert len(results) == 0
