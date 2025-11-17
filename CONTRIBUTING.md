be47827b81705beaa9f110ef600522e571c0c952
Emily Rockman <emily.rockman@dbtlabs.com>
2025-11-17 16:28:47 -0500

    cleanup docs

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -81,6 +81,7 @@ These are the tools used in `dbt-core` development and testing:
 - [`mypy`](https://mypy.readthedocs.io/en/stable/) for static type checking
 - [`pre-commit`](https://pre-commit.com) to easily run those checks
 - [`changie`](https://changie.dev/) to create changelog entries, without merge conflicts
+- [`hatchling`](https://hatch.pypa.io/) as the build backend for creating distribution packages
 - [`make`](https://users.cs.duke.edu/~ola/courses/programming/Makefiles/Makefiles.html) to run multiple setup or test steps in combination. Don't worry too much, nobody _really_ understands how `make` works, and our Makefile aims to be super simple.
 - [GitHub Actions](https://github.com/features/actions) for automating tests and checks, once a PR is pushed to the `dbt-core` repository
 
@@ -126,6 +127,19 @@ pre-commit install
 ```
 
 When installed in this way, any changes you make to your local copy of the source code will be reflected immediately in your next `dbt` run.
+
+#### Building dbt-core
+
+dbt-core uses [Hatch](https://hatch.pypa.io/) (specifically `hatchling`) as its build backend. To build distribution packages:
+
+```sh
+cd core
+python -m build
+```
+
+This will create both wheel (`.whl`) and source distribution (`.tar.gz`) files in the `dist/` directory.
+
+The build configuration is defined in `core/pyproject.toml` with additional environment configurations in `core/hatch.toml`. Note that `hatch` itself is not required for development work - standard `pip install -e` works as usual.
 
 ### Running `dbt-core`
 
--- core/setup.py
+++ core/setup.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
 
-"""Legacy setuptools shim retained for compatibility with existing workflows.  Will be removed in a future version."""
+"""
+DEPRECATED: This setup.py is maintained for backwards compatibility only.
+
+dbt-core now uses hatchling as its build backend (defined in pyproject.toml).
+Please use `python -m build` or `pip install` directly instead of setup.py commands.
+
+This file will be maintained indefinitely for legacy tooling support but is no
+longer the primary build interface.
+"""
 
 from setuptools import setup
-
-# the user has a downlevel version of setuptools.
-# ----
-# dbt-core uses these packages deeply, throughout the codebase, and there have been breaking changes in past patch releases (even though these are major-version-one).
-# Pin to the patch or minor version, and bump in each new minor version of dbt-core.
-# ----
-# dbt-core uses these packages in standard ways. Pin to the major version, and check compatibility
-# with major versions in each new minor version of dbt-core.
-# ----
-# These packages are major-version-0. Keep upper bounds on upcoming minor versions (which could have breaking changes)
-# and check compatibility / bump in each new minor version of dbt-core.
-# ----
-# These are major-version-0 packages also maintained by dbt-labs.
-# Accept patches but avoid automatically updating past a set minor version range.
-# Minor versions for these are expected to be backwards-compatible
-# ----
-# Expect compatibility with all new versions of these packages, so lower bounds only.
-# ----
 
 if __name__ == "__main__":
     setup()
