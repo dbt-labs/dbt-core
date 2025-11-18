f6328a3325c1b09a2e68b3549c3fcf87f8eb640d
Trilok Ramakrishna <trilok.ramakrishna@dbtlabs.com>
2025-11-18 15:55:21 +0530

    removed event catcher from utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,18 +1,2 @@
-from dataclasses import dataclass, field
-from typing import Callable, List
-
-from dbt_common.events.base_types import BaseEvent, EventMsg
-
-
-@dataclass
-class EventCatcher:
-    event_to_catch: BaseEvent
-    caught_events: List[EventMsg] = field(default_factory=list)
-    predicate: Callable[[EventMsg], bool] = lambda event: True
-
-    def catch(self, event: EventMsg):
-        if event.info.name == self.event_to_catch.__name__ and self.predicate(event):
-            self.caught_events.append(event)
-
-    def flush(self) -> None:
-        self.caught_events = []
+# EventCatcher has been moved to dbt-common (dbt_common.events.event_catcher)
+# This file is kept for backwards compatibility in case any external code imports from it
