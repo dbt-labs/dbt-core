bff21a9a8811f1f7a59c28125a4d5ad898e15a12
MichelleArk <michelle.ark@dbtlabs.com>
2025-11-24 13:14:11 -0500

    add testing across packages

--- tests/functional/duplicates/test_duplicate_resource_names.py
+++ tests/functional/duplicates/test_duplicate_resource_names.py
@@ -4,6 +4,7 @@ import pytest
 
 import dbt.deprecations as deprecations
 from dbt.exceptions import DuplicateResourceNameError
+from dbt.tests.fixtures.project import write_project_files
 from dbt.tests.util import run_dbt
 
 # Test resources with duplicate names but different database aliases
@@ -22,6 +23,19 @@ macros_sql = """
 {% macro generate_alias_name(custom_alias_name, node) -%}
     {{ node.name }}_{{ node.resource_type }}
 {%- endmacro %}
+"""
+
+versioned_model_yml = """
+models:
+  - name: same_name
+    versions:
+      - v: 1
+"""
+
+local_dependency__dbt_project_yml = """
+
+name: 'local_dep'
+version: '1.0'
 """
 
 
@@ -51,7 +65,7 @@ class BaseTestDuplicateNames:
         }
 
 
-class TestDuplicateNamesRequireUniqueProjectResourceNamesTrue(BaseTestDuplicateNames):
+class TestDuplicateNamesRequireUniqueResourceNamesTrue(BaseTestDuplicateNames):
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {
@@ -66,7 +80,44 @@ class TestDuplicateNamesRequireUniqueProjectResourceNamesTrue(BaseTestDuplicateN
             run_dbt(["parse"])
 
 
-class TestDuplicateNamesRequireUniqueProjectResourceNamesFalse(BaseTestDuplicateNames):
+class TestDuplicateNamesRequireUniqueResourceNamesTrueDifferentPackages(BaseTestDuplicateNames):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "flags": {
+                "require_unique_project_resource_names": True,
+            }
+        }
+
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        # Seed in local dep instead
+        return {}
+
+    @pytest.fixture(scope="class")
+    def packages(self):
+        return {"packages": [{"local": "local_dependency"}]}
+
+    @pytest.fixture(scope="class", autouse=True)
+    def setUp(self, project_root):
+        local_dependency_files = {
+            "dbt_project.yml": local_dependency__dbt_project_yml,
+            "seeds": {"same_name.csv": seed_csv},
+        }
+        write_project_files(project_root, "local_dependency", local_dependency_files)
+
+    def test_duplicate_names_with_flag_enabled_different_packages(self, project):
+        run_dbt(["deps"])
+        # Behavior flag is true, however, the seed is in a different package.
+        # No error is raised when encountering duplicate names between different packages.
+        manifest = run_dbt(["parse"])
+
+        assert len(manifest.nodes) == 2
+        assert "model.test.same_name" in manifest.nodes
+        assert "seed.local_dep.same_name" in manifest.nodes
+
+
+class TestDuplicateNamesRequireUniqueResourceNamesFalse(BaseTestDuplicateNames):
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {
@@ -87,7 +138,22 @@ class TestDuplicateNamesRequireUniqueProjectResourceNamesFalse(BaseTestDuplicate
         assert "duplicate-name-distinct-node-types-deprecation" in deprecations.active_deprecations
 
 
-class TestDuplicateNamesDefaultBehavior(TestDuplicateNamesRequireUniqueProjectResourceNamesFalse):
+class TestDuplicateNamesDefaultBehavior(TestDuplicateNamesRequireUniqueResourceNamesFalse):
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {}
+
+
+class TestDuplicateNamesDifferentResourceTypesVersionedUnversioned(BaseTestDuplicateNames):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "same_name.sql": model_sql,
+            "schema.yml": versioned_model_yml,
+        }
+
+    def test_duplicate_names_versioned_unversioned(self, project):
+        # DuplicateVersionedUnversionedError is not raised because parsing fails upstream.
+        # However, parsing still fails with an AssertionError because versioning is attempted on a non-model node.
+        with pytest.raises(AssertionError):
+            run_dbt(["parse"])
