afd22f2c1574bf85d587e38b210847fe67742e04
Gerda Shank <gerda@dbtlabs.com>
2025-12-03 14:36:20 -0500

    Test for retrieving meta key using config.get

--- tests/functional/configs/test_get_default.py
+++ tests/functional/configs/test_get_default.py
@@ -8,6 +8,19 @@ select {{ config.get('made_up_nonexistent_key', 'default_value') }} as col_value
 
 """
 
+meta_model_sql = """
+-- models/meta_model.sql
+select {{ config.get('meta_key', 'meta_default_value') }} as col_value
+"""
+
+schema_yml = """
+models:
+ - name: meta_model
+   config:
+     meta:
+       meta_key: my_meta_value
+"""
+
 
 class TestConfigGetDefault:
     @pytest.fixture(scope="class")
@@ -24,3 +37,22 @@ class TestConfigGetDefault:
         assert len(results) == 1
         assert str(results[0].status) == "error"
         assert 'column "default_value" does not exist' in results[0].message
+
+
+class TestConfigGetMeta:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "meta_model.sql": meta_model_sql,
+            "schema.yml": schema_yml,
+        }
+
+    def test_config_with_meta_key(
+        self,
+        project,
+    ):
+        # This test runs a model with a config.get(key, default)
+        results = run_dbt(["run"], expect_pass=False)
+        assert len(results) == 1
+        assert str(results[0].status) == "error"
+        assert 'column "my_meta_value" does not exist' in results[0].message
