0a2b664b4629e7ecb2735366a432033c586c0214
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2026-01-09 12:37:55 -0600

    Begin tracking thread limit, and microbatch limit, on `DbtThreadPool`
    Keeping track of these things will allow us to not execute "too many"
    microbatch model runners in the upcoming commits

--- core/dbt/graph/thread_pool.py
+++ core/dbt/graph/thread_pool.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from math import floor
 from multiprocessing.pool import ThreadPool
 
 
@@ -9,6 +10,8 @@ class DbtThreadPool(ThreadPool):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.closed = False
+        self.max_threads = kwargs.get("processes") if kwargs.get("processes") else args[0]
+        self.max_microbatch_models = max(1, floor(self.max_threads / 2))
 
     def close(self):
         self.closed = True
