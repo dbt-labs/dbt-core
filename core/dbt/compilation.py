7e4cd0818638717e976c15a832d0679c7b6a7c91
Michelle Ark <MichelleArk@users.noreply.github.com>
2026-03-09 12:12:42 -0400

    set sqlparse.engine.grouping from CLI -sqlparse '{"MAX_GROUPING_DEPTH": "value", "MAX_GROUPING_TOKENS": "value"}' options, defaulting to None (#12558)

--- core/dbt/compilation.py
+++ core/dbt/compilation.py
@@ -45,6 +45,7 @@ from dbt_common.events.contextvars import get_node_info
 from dbt_common.events.format import pluralize
 from dbt_common.events.functions import fire_event
 from dbt_common.events.types import Note
+from dbt_common.exceptions import CompilationError
 from dbt_common.invocation import get_invocation_id
 
 graph_file_name = "graph.gpickle"
@@ -819,7 +820,22 @@ def inject_ctes_into_sql(sql: str, ctes: List[InjectedCTE]) -> str:
     if len(ctes) == 0:
         return sql
 
-    parsed_stmts = sqlparse.parse(sql)
+    try:
+        parsed_stmts = sqlparse.parse(sql)
+    except Exception as e:
+        compilation_exception_msg = str(e)
+        if "grouping depth exceeded" in compilation_exception_msg.lower():
+            compilation_exception_msg = (
+                f"{compilation_exception_msg} You may raise the limit via "
+                f'--sqlparse \'{{"MAX_GROUPING_DEPTH": "<value>"}}\'.'
+            )
+        if "number of tokens exceeded" in compilation_exception_msg.lower():
+            compilation_exception_msg = (
+                f"{compilation_exception_msg} You may raise the limit via "
+                f'--sqlparse \'{{"MAX_GROUPING_TOKENS": "<value>"}}\'.'
+            )
+        raise CompilationError(compilation_exception_msg)
+
     parsed = parsed_stmts[0]
 
     with_stmt = None
