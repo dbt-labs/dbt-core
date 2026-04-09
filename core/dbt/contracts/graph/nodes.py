2384e7c0aeadb8b4a7b15ad85d1a2d37650e517f
Tauhid Anjum <tauhidanjum@gmail.com>
2026-04-09 19:36:22 +0530

    Fix Self import for Python 3.10 compatibility (#12809)
    * Fix Self import for Python 3.10 compatibility
    
    * Update core/dbt/contracts/graph/nodes.py
    
    Co-authored-by: Quigley Malcolm <QMalcolm@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Quigley Malcolm <QMalcolm@users.noreply.github.com>

--- core/dbt/contracts/graph/nodes.py
+++ core/dbt/contracts/graph/nodes.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import sys
 import threading
 from dataclasses import dataclass, field
 from datetime import datetime
@@ -12,13 +13,18 @@ from typing import (
     Literal,
     NoReturn,
     Optional,
-    Self,
     Sequence,
     Tuple,
     Type,
     Union,
     get_args,
 )
+
+# TODO: Collapse into just using the >3.11 case once we drop support for python 3.10
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
 
 from mashumaro.types import SerializableType
 
