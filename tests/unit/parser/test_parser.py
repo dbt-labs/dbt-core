941cac70877d219043dcb4b333a1b9c9e6c330ef
Edosicca <edoardo.sichelli@gmail.com>
2026-05-13 00:05:19 +0200

    format tests

--- tests/unit/parser/test_parser.py
+++ tests/unit/parser/test_parser.py
@@ -1905,9 +1905,7 @@ class SnapshotParserTest(BaseParserTest):
             select 1 as id, now() as last_update"""
         full_file = """
         {{% snapshot foo %}}{}{{% endsnapshot %}}
-        """.format(
-            raw_code
-        )
+        """.format(raw_code)
         block = self.file_block_for(full_file, "nested/snap_1.sql")
         self.parser.manifest.files[block.file.file_id] = block.file
         self.parser.parse_file(block)
@@ -1977,9 +1975,7 @@ class SnapshotParserTest(BaseParserTest):
         full_file = """
         {{% snapshot foo %}}{}{{% endsnapshot %}}
         {{% snapshot bar %}}{}{{% endsnapshot %}}
-        """.format(
-            raw_1, raw_2
-        )
+        """.format(raw_1, raw_2)
         block = self.file_block_for(full_file, "nested/snap_1.sql")
         self.parser.manifest.files[block.file.file_id] = block.file
         self.parser.parse_file(block)
