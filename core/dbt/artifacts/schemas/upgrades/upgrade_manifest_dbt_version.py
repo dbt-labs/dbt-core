deeb2d099cd628b238c3449de75753e6ca0a479b
MichelleArk <michelle.ark@dbtlabs.com>
2026-06-25 15:31:39 -0400

    linting

--- core/dbt/artifacts/schemas/upgrades/upgrade_manifest_dbt_version.py
+++ core/dbt/artifacts/schemas/upgrades/upgrade_manifest_dbt_version.py
@@ -1,9 +1,10 @@
 from dbt.clients.jinja_static import statically_parse_unrendered_config
 from dbt.flags import get_flags
 
+
 def upgrade_manifest_json_dbt_version(manifest: dict) -> dict:
     upgrade_unrendered_config(manifest)
-    
+
     return manifest
 
 
@@ -15,7 +16,7 @@ def upgrade_unrendered_config(manifest: dict) -> dict:
     call from the intermediate Python representation, behind state_modified_compare_more_unrendered_values.
 
     Only upgrade the manifest if state_modified_compare_more_unrendered_values is set to True, as the change to statically_parse_unrendered_config
-    would only lead to false positives in that scenario as projects with state_modified_compare_more_unrendered_values set to false do not go through 
+    would only lead to false positives in that scenario as projects with state_modified_compare_more_unrendered_values set to false do not go through
     the statically_parse_unrendered_config call at all.
 
     Similarly, simple built-in generic tests (not_null, unique) are skipped because they have no raw_code config block.
@@ -26,10 +27,18 @@ def upgrade_unrendered_config(manifest: dict) -> dict:
             if _node_is_simple_builtin_generic_test(node):
                 continue
             if node.get("raw_code") and node.get("unrendered_config"):
-                new_unrendered_config_from_code = statically_parse_unrendered_config(node.get("raw_code", "")) or {}
-                manifest["nodes"][unique_id]["unrendered_config"].update(new_unrendered_config_from_code)
+                new_unrendered_config_from_code = (
+                    statically_parse_unrendered_config(node.get("raw_code", "")) or {}
+                )
+                manifest["nodes"][unique_id]["unrendered_config"].update(
+                    new_unrendered_config_from_code
+                )
 
     return manifest
 
+
 def _node_is_simple_builtin_generic_test(node: dict) -> bool:
-    return node.get("resource_type") == "test" and node.get("depends_on", {}).get("macros") in (["macro.dbt.test_not_null"], ["macro.dbt.test_unique"])
+    return node.get("resource_type") == "test" and node.get("depends_on", {}).get("macros") in (
+        ["macro.dbt.test_not_null"],
+        ["macro.dbt.test_unique"],
+    )
