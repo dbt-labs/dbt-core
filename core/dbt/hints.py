4f893e78564066907f5302187b506b8f2ac8ba59
Ashish Shukla <ashish.shukla@dbtlabs.com>
2026-07-15 00:07:35 +0530

    update resuse-relations hint (#15541)
    * update resuse-relations hint
    
    * add changelog
    
    (cherry picked from commit af7239bb6d5fda50ee81c7db9a478c155bece18b)

--- core/dbt/hints.py
+++ core/dbt/hints.py
@@ -22,7 +22,7 @@ HINT_TS_FILENAME = "hint_ts.json"
 
 # Hint message text shown to the user. Keep these actionable and point at docs.
 REUSE_RELATIONS_ON_TOO_MANY_MODELS = (
-    "You're rebuilding a lot from scratch. You can use state to save time and money by reusing or skipping existing objects: "
+    "You're rebuilding a lot from scratch. You can use state to skip models that haven't changed: "
     "https://docs.getdbt.com/docs/optimize-builds?utm_source=dbt-cli"
 )
 
