eda650ff28f40fb00fe731e1c58f9b469f2ef728
Ashish Shukla <ashish.shukla@dbtlabs.com>
2026-08-12 12:22:39 +0530

    add mechanism for version deprecation warning (#15897)
    * add mechanism for version deprecation warning ( backportable till 1.10 as is )
    
    * addressed review comments
    
    (cherry picked from commit ee7613665102c3f440b5643b51f7fbee64c592e5)

--- core/dbt/cli/params.py
+++ core/dbt/cli/params.py
@@ -5,6 +5,7 @@ import click
 from dbt.cli.option_types import YAML, ChoiceTuple, Package, WarnErrorOptionsType
 from dbt.cli.options import MultiOption
 from dbt.cli.resolvers import default_profiles_dir, default_project_dir
+from dbt.deprecated_version import check_deprecated_version
 from dbt.version import get_version_information
 
 add_package = click.option(
@@ -683,6 +684,7 @@ def _version_callback(ctx, _param, value):
     if not value or ctx.resilient_parsing:
         return
     click.echo(get_version_information())
+    check_deprecated_version(is_warn=True)
     ctx.exit()
 
 
